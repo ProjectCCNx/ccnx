3576baf0eb66f28e2276539169c5cb087106734c
Nick Briggs <briggs@parc.com>
2007-10-16 00:25:06 +0000

    New exception to be reported if the ContentName from String is malformed, which, for the moment, means that it doesn't start with a separator character.

---
+++ Java_CCN/com/parc/ccn/data/MalformedContentNameStringException.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.parc.ccn.data;
+
+/**
+ * @author briggs
+ *
+ */
+public class MalformedContentNameStringException extends Exception {
+
+	/**
+	 * 
+	 */
+	public MalformedContentNameStringException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public MalformedContentNameStringException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public MalformedContentNameStringException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public MalformedContentNameStringException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
