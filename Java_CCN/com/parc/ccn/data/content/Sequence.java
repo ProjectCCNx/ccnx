7e22c92633db777e7eaf9bb58810ecc53cf6ccc6
Nick Briggs <briggs@parc.com>
2007-10-19 18:42:08 +0000

    Add new code to deal with content objects representing objects that are a sequence of named objects. [nhb]

---
+++ Java_CCN/com/parc/ccn/data/content/Sequence.java
@@ -0,0 +1,99 @@
+
+package com.parc.ccn.data.content;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import com.parc.ccn.data.util.GenericXMLEncodable;
+import com.parc.ccn.data.util.XMLEncodable;
+import com.parc.ccn.data.util.XMLHelper;
+
+/**
+ * Mapping from a sequence to the underlying XML representation.
+ * A Sequence is a content object giving a compact description of a
+ * sequence of named blocks.
+ * @author briggs
+ *
+ */
+public class Sequence extends GenericXMLEncodable implements XMLEncodable {
+	
+	protected static final String SEQUENCE_ELEMENT = "Sequence";
+	protected static final String START_ELEMENT = "Start";
+	protected static final String COUNT_ELEMENT = "Count";
+	protected static final String BLOCKSIZE_ELEMENT = "BlockSize";
+	protected static final String LENGTH_ELEMENT = "Length";
+	
+	public static final int DEFAULT_BLOCKSIZE = 4096;
+	public static final int DEFAULT_START = 1;
+	
+	protected int _start;	// starting block number ( >= 0)
+	protected int _count;	// number of blocks in sequence (>= 0)
+	protected int _blockSize; // size in bytes(?) of a block (> 0)
+	protected int _length; // total length in bytes (? same unit as _blockSize) to account for partial last block (>= 0)
+
+	/**
+	 * Basic constructor for content sequences
+	 * @param start
+	 * @param count
+	 * @param blockSize
+	 * @param length
+	 */
+	public Sequence(int start, int count, int blockSize, int length) {
+		_start = start;
+		_count = count;
+		_blockSize = blockSize;
+		_length = length;
+	}
+
+	/**
+	 * 
+	 */
+	public Sequence(int length) {
+		this(DEFAULT_START, length / DEFAULT_BLOCKSIZE, DEFAULT_BLOCKSIZE, length);
+	}
+	/**
+	 * @param encoded
+	 * @throws XMLStreamException
+	 */
+	public Sequence(byte[] encoded) throws XMLStreamException {
+		super(encoded);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.parc.ccn.data.util.XMLEncodable#decode(javax.xml.stream.XMLEventReader)
+	 */
+	@Override
+	public void decode(XMLEventReader reader) throws XMLStreamException {
+		XMLHelper.readStartElement(reader, SEQUENCE_ELEMENT);
+		_start = Integer.valueOf(XMLHelper.readElementText(reader, START_ELEMENT));
+		_count = Integer.valueOf(XMLHelper.readElementText(reader, COUNT_ELEMENT));
+		_blockSize = Integer.valueOf(XMLHelper.readElementText(reader, BLOCKSIZE_ELEMENT));
+		_length = Integer.valueOf(XMLHelper.readElementText(reader, LENGTH_ELEMENT));
+		XMLHelper.readEndElement(reader);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.parc.ccn.data.util.XMLEncodable#encode(javax.xml.stream.XMLStreamWriter, boolean)
+	 */
+	@Override
+	public void encode(XMLStreamWriter writer, boolean isFirstElement)
+			throws XMLStreamException {
+		if (!validate()) {
+			throw new XMLStreamException("Cannot encode " + this.getClass().getName() + ": field values missing.");
+		}
+		XMLHelper.writeStartElement(writer, SEQUENCE_ELEMENT, isFirstElement);
+		XMLHelper.writeElement(writer, START_ELEMENT,	 Integer.toString(_start));
+		XMLHelper.writeElement(writer, COUNT_ELEMENT,	 Integer.toString(_count));
+		XMLHelper.writeElement(writer, BLOCKSIZE_ELEMENT,	 Integer.toString(_blockSize));
+		XMLHelper.writeElement(writer, LENGTH_ELEMENT,	Integer.toString(_length));
+		writer.writeEndElement();
+	}
+
+	@Override
+	public boolean validate() {
+		if (_start < 0 || _count < 0 || _blockSize <= 0 || _length < 0) return false;
+		return true;
+	}
+
+}
