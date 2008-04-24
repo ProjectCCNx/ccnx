cc16b0948677bdc835cf9f53cddd90a5d7e8ec4b
Nick Briggs <briggs@parc.com>
2008-04-24 20:06:48 +0000

    make dataresponsetest not express an interest in root, and use files containing interests to control what it does express

--- ccnd/lib/dataresponsetest.c
+++ ccnd/lib/dataresponsetest.c
@@ -9,6 +9,11 @@
 
 #include <ccn/ccn.h>
 
+static struct options {
+    int logging;
+    int nointerest;
+} options = {0, 0};
+
 struct handlerstate {
     int next;
     int count;
@@ -33,7 +38,8 @@ int
 main (int argc, char *argv[]) {
     struct ccn *ccn = NULL;
     struct ccn_closure *action;
-    struct ccn_charbuf *namebuf;
+    struct ccn_charbuf *namebuf = NULL;
+    struct ccn_charbuf *interestnamebuf = NULL;
     struct ccn_buf_decoder decoder;
     struct ccn_buf_decoder *d;
     struct handlerstate *state;
@@ -42,8 +48,6 @@ main (int argc, char *argv[]) {
     ssize_t rawlen;
     int i, n, res;
     int fd = -1;
-    int quiet = 0;
-    int nointerest = 0;
 
     ccn = ccn_create();
     if (ccn_connect(ccn, NULL) == -1) {
@@ -55,24 +59,48 @@ main (int argc, char *argv[]) {
     action = calloc(1, sizeof(struct ccn_closure));
     action->p = interest_handler;
 
+    namebuf = ccn_charbuf_create();
+    if (namebuf == NULL) {
+        fprintf(stderr, "ccn_charbuf_create\n");
+        exit(1);
+    }
+    res = ccn_name_init(namebuf);
+    if (res < 0) {
+        fprintf(stderr, "ccn_name_init\n");
+        exit(1);
+    }
+
+    interestnamebuf = ccn_charbuf_create();
+    if (interestnamebuf == NULL) {
+        fprintf(stderr, "ccn_charbuf_create\n");
+        exit(1);
+    }
+    res = ccn_name_init(interestnamebuf);
+    if (res < 0) {
+        fprintf(stderr, "ccn_name_init\n");
+        exit(1);
+    }
+
     n = 0;
     for (i = 1; i < argc; i++) {
         if (fd != -1) close(fd);
         filename = argv[i];
-        if (0 == strcmp(filename, "-quiet")) {
-            quiet = 1;
+        if (0 == strcmp(filename, "-d")) {
+            options.logging++;
             continue;
         }
         if (0 == strcmp(filename, "-nointerest")) {
-            nointerest = 1;
+            options.nointerest = 1;
             continue;
         }
-        if (!quiet) fprintf(stderr, "Processing %s ", filename);
+
+        if (options.logging > 0) fprintf(stderr, "Processing %s ", filename);
         fd = open(filename, O_RDONLY);
         if (fd == -1) {
             perror("- open");
             continue;
         }
+
         rawlen = read(fd, rawbuf, sizeof(rawbuf));
         if (rawlen <= 0) {
             perror("- read");
@@ -80,38 +108,43 @@ main (int argc, char *argv[]) {
         }
         
         d = ccn_buf_decoder_start(&decoder, (unsigned char *)rawbuf, rawlen);
+
         if (ccn_buf_match_dtag(d, CCN_DTAG_ContentObject)) {
             state->items = realloc(state->items, (n + 1) * sizeof(*(state->items)));
             if (state->items == NULL) {
-                perror("realloc failed");
+                perror(" - realloc failed");
                 exit(1);
             }
             memset(&(state->items[n]), 0, sizeof(*(state->items)));
             state->items[n].components = ccn_indexbuf_create();
             res = ccn_parse_ContentObject((unsigned char *)rawbuf, rawlen, &(state->items[n].x), state->items[n].components);
             if (res < 0) {
+                if (options.logging > 0) fprintf(stderr, "Processing %s ", filename);
                 fprintf(stderr, "- skipping: ContentObject error %d\n", res);
                 ccn_indexbuf_destroy(&state->items[n].components);
                 continue;
             }
-            if (!quiet) fprintf(stderr, "- ok\n");
+            if (options.logging > 0) fprintf(stderr, "- ok\n");
             state->items[n].filename = filename;
             state->items[n].contents = malloc(rawlen);
             state->items[n].size = rawlen;
             memcpy(state->items[n].contents, rawbuf, rawlen);
             n++;
         } else if (ccn_buf_match_dtag(d, CCN_DTAG_Interest)) {
-            fprintf(stderr, " - skipping: Interest\n");
+            struct ccn_parsed_interest interest = {0};
+            if (options.nointerest == 0) {
+                interestnamebuf->length = 0;
+                res = ccn_parse_interest((unsigned char *)rawbuf, rawlen, &interest, NULL);
+                ccn_charbuf_append(interestnamebuf, rawbuf + interest.name_start, interest.name_size);
+                res = ccn_express_interest(ccn, interestnamebuf, -1, action);
+            }
+        } else {
+            if (options.logging == 0) fprintf(stderr, "Processing %s ", filename);
+            fprintf(stderr, "- skipping: unknown type\n");
         }
     }
     state->count = n;
     action->data = state;
-
-    namebuf = ccn_charbuf_create();
-    if (namebuf == NULL) {
-        fprintf(stderr, "ccn_charbuf_create\n");
-        exit(1);
-    }        
 
     if (ccn_name_init(namebuf) == -1) {
         fprintf(stderr, "ccn_name_init\n");
@@ -119,7 +152,6 @@ main (int argc, char *argv[]) {
     }
 
     res = ccn_set_interest_filter(ccn, namebuf, action);
-    if (nointerest == 0) res = ccn_express_interest(ccn, namebuf, -1, action);
     res = ccn_run(ccn, -1);
     ccn_disconnect(ccn);
     ccn_destroy(&ccn);
