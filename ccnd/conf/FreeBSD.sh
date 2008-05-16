f862f2aecf7aecec6f71b22eb2346ab3062a7f14
Michael Plass <plass@parc.com>
2008-05-16 22:52:25 +0000

    Fixups for configure on other platforms

--- ccnd/conf/FreeBSD.sh
+++ ccnd/conf/FreeBSD.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Make some symlinks so that we can use the native expat.
-
-make cleanall
+set -x
+find include lib -type l -exec rm '{}' ';'
 ln -s /usr/include/bsdxml.h include/expat.h
 ln -s /usr/lib/libbsdxml.a lib/libexpat.a
--- ccnd/conf/SunOS.mk
+++ ccnd/conf/SunOS.mk
@@ -1,2 +1,3 @@
 MORE_CFLAGS = -mt
-MORE_LDLIBS = -lmtmalloc -lnsl -lsocket
+MORE_LDLIBS = -L/usr/apache2/lib -lmtmalloc -lnsl -lsocket
+CPREFLAGS = -I../include -I/usr/apache2/include -DXML_STATUS_OK=0 -I/usr/local/ssl/include
