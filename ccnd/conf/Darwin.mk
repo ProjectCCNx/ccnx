2c202244b066aec6b1935045103dbab714f5a83e
Bill Janssen <janssen@parc.com>
2008-05-20 19:33:48 +0000

    update to build shared library and python lib

--- ccnd/conf/Darwin.mk
+++ ccnd/conf/Darwin.mk
@@ -1,1 +1,6 @@
+SHEXT=dylib
+SHLIBNAME=libccn.1.$(SHEXT)
+SHLIBDEPS=/usr/lib/dylib1.o
+SHARED_LD_FLAGS = -dylib -arch `/usr/bin/arch` -install_name $(SHLIBNAME) -all_load /usr/lib/dylib1.o -lSystem
+PLATCFLAGS=-fno-common
 CWARNFLAGS = -Wall
--- ccnd/conf/Darwin.sh
+++ ccnd/conf/Darwin.sh
@@ -1,1 +1,16 @@
 echo "# Darwin.sh $@ was here" >> conf.mk
+# check for python that's advanced enough
+PYTHON=`which python`
+if test "$PYTHON" != ""; then 
+  PYTHONVERSION=`$PYTHON -c "import sys; print ((sys.version_info >= (2, 5, 0)) and "1" or "0")"`
+  if test "$PYTHONVERSION" == 1; then
+     echo "PYTHON=$PYTHON" >> conf.mk
+     echo "PYTHON_TARGETS=python" >> conf.mk
+  else
+     echo "PYTHON=" >> conf.mk
+     echo "PYTHON_TARGETS=" >> conf.mk
+  fi
+else
+   echo "PYTHON=" >> conf.mk
+   echo "PYTHON_TARGETS=" >> conf.mk
+fi
