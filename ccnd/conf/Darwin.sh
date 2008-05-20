1bd9fff9775ed1a6385b77a47871018b919cf78d
Bill Janssen <janssen@parc.com>
2008-05-20 20:01:40 +0000

    better test for ctypes

--- ccnd/conf/Darwin.sh
+++ ccnd/conf/Darwin.sh
@@ -2,7 +2,14 @@ echo "# Darwin.sh $@ was here" >> conf.mk
 # check for python that's advanced enough
 PYTHON=`which python`
 if test "$PYTHON" != ""; then 
-  PYTHONVERSION=`$PYTHON -c "import sys; print ((sys.version_info >= (2, 5, 0)) and "1" or "0")"`
+  PYTHONVERSION=`PYTHON <<EOF
+try:
+    import ctypes
+except:
+    print 0
+else:
+    print 1 
+EOF`
   if test "$PYTHONVERSION" == 1; then
      echo "PYTHON=$PYTHON" >> conf.mk
      echo "PYTHON_TARGETS=python" >> conf.mk
