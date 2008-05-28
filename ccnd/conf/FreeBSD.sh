7079c4b76c4cf3457bcfc344cbff29c0a7184743
Michael Plass <plass@parc.com>
2008-05-28 18:52:53 +0000

    Fix build

--- ccnd/conf/FreeBSD.sh
+++ ccnd/conf/FreeBSD.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Make some symlinks so that we can use the native expat.
 set -x
-find include lib -type l -exec rm '{}' ';'
+mkdir -p include
+find include lib -type l -name \*expat\* -exec rm '{}' ';'
 ln -s /usr/include/bsdxml.h include/expat.h
 ln -s /usr/lib/libbsdxml.a lib/libexpat.a
