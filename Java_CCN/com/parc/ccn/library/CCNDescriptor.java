ef1f69b0035f83652ead375e0e5779367e385e03
Diana Smetters <smetters@parc.com>
2008-06-03 21:27:42 +0000

    Checkpoint for Ying.

--- Java_CCN/com/parc/ccn/library/CCNDescriptor.java
+++ Java_CCN/com/parc/ccn/library/CCNDescriptor.java
@@ -1,7 +1,13 @@
 package com.parc.ccn.library;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.xml.stream.XMLStreamException;
 
+import com.parc.ccn.Library;
 import com.parc.ccn.data.ContentName;
 import com.parc.ccn.data.ContentObject;
 import com.parc.ccn.data.content.Header;
@@ -27,6 +33,8 @@ import com.parc.ccn.data.security.ContentAuthenticator;
 public class CCNDescriptor {
 	
 	public enum SeekWhence {SEEK_SET, SEEK_CUR, SEEK_END};
+	
+	CCNLibrary _library = null;
 	
 	/**
 	 * The base name of the object we are actually
@@ -81,7 +89,13 @@ public class CCNDescriptor {
 	 * @throws XMLStreamException if the object is not a valid Header
 	 */
 	public CCNDescriptor(ContentObject headerObject, 
-						 boolean verified) throws XMLStreamException {
+						 boolean verified,
+						 CCNLibrary library) throws XMLStreamException {
+		_library = library; 
+		if (null == _library) {
+			throw new IllegalArgumentException("Unexpected null library in CCNDescriptor constructor!");
+		}
+
 		_headerAuthenticator = headerObject.authenticator();
 		_baseName = StandardCCNLibrary.headerRoot(headerObject.name());
 		
@@ -90,32 +104,106 @@ public class CCNDescriptor {
 	}
 	
 	/**
-	 * Reads from this object into buf. Starts at the
-	 * beginning of the object and continues. 
+	 * Reads from this object into buf. Starts at the current position in
+	 * the object. By default, that starts at the beginning of the object (byte 0)
+	 * after open, and subsequent calls to read pick up where the last left off
+	 * (analogous to fread), but it can be moved by calling seek.
 	 * @param buf the buffer into which to write.
 	 * @param offset the offset into buf at which to write data
 	 * @param len the number of bytes to write
 	 * @return
 	 */
 	public long read(byte[] buf, long offset, long len) {
+		int result = 0;
 		
 		// is this the first block?
 		if (null == _currentBlock) {
-			
+			// Sets _currentBlock and _readOffset.
+			result = seek(0);
+			if (result != 0) {
+				return result; // errno analogy?
+			}
+		} 
+		
+		// Now we have a block in place. Read from it. If we run out of block before
+		// we've read len bytes, pull next block.
+		long lenToRead = len;
+		while (lenToRead > 0) {
+			if (_readOffset >= _currentBlock.content().length) {
+				// DKS make sure we don't miss a byte...
+				result = seek(StandardCCNLibrary.getFragment(_currentBlock.name())+1);
+			}
 		}
 		return 0;
 	}
 	
 	/**
 	 * Support ideas of seek, etc, even if fuse doesn't
-	 * require them.
+	 * require them. Seek actually does a get on the appropriate content block.
 	 */
 	public int seek(long offset, Enum<SeekWhence> whence) {
+		return 0;
+	}
+	
+	protected int seek(int blockNumber) {
+		_readOffset = 0;
 		return 0;
 	}
 	
 	public long tell() {
 		return 0;
 	}
+	
+	protected ContentObject getBlock(int number) throws IOException, InterruptedException {
+		
+		ContentName blockName = StandardCCNLibrary.fragmentName(_baseName, number);
 
+		ArrayList<ContentObject> blocks = _library.get(blockName, _headerAuthenticator, true);
+		
+		if ((null == blocks) || (blocks.size() == 0)) {
+			Library.logger().info("Cannot get block " + number + " of file " + _baseName + " expected block: " + blockName.toString());
+			throw new IOException("Cannot get block " + number + " of file " + _baseName + " expected block: " + blockName.toString());
+		}
+		// So for each header, we assume we have a potential document.
+		
+		// First we verify. (Or should get have done this for us?)
+		// We don't bother complaining unless we have more than one
+		// header that matches. Given that we would complain for
+		// that, we need an enumerate that operates at this level.)
+		Iterator<ContentObject> blockIt = blocks.iterator();
+		while (blockIt.hasNext()) {
+			ContentObject block = blockIt.next();
+			// TODO: DKS: should this be header.verify()?
+			// Need low-level verify as well as high-level verify...
+			// Low-level verify just checks that signer actually signed.
+			// High-level verify checks trust.
+			try {
+				if (!ContentObject.verifyContentDigest(block.authenticator().contentDigest(), block.content())) {
+					Library.logger().warning("Found block: " + block.name().toString() + " that fails to verify.");
+					blockIt.remove();
+				}
+				// Compare to see whether this block matches the root signature we previously verified, if
+				// not, verify and store the current signature.
+				// DKS TODO -- and move original verification inside constructor
+				//if ()
+			} catch (Exception e) {
+				Library.logger().warning("Got an " + e.getClass().getName() + " exception attempting to verify header: " + block.name().toString() + ", treat as failure to verify.");
+				Library.warningStackTrace(e);
+				blockIt.remove();
+			}
+		}
+		if (blocks.size() == 0) {
+			Library.logger().info("No available verifiable content named: " + blockName.toString());
+			throw new FileNotFoundException("No available verifiable content named: " + blockName.toString());
+		}
+		if (blocks.size() > 1) {
+			Library.logger().info("Found " + blocks.size() + " blocks matching the name: " + blockName.toString());
+			throw new IOException("CCNException: More than one (" + blocks.size() + ") valid block found for name: " + blockName.toString() + " in open!");
+		}
+		if (blocks.get(0) == null) {
+			Library.logger().info("Found only null blocks matching the name: " + blockName.toString());
+			throw new IOException("CCNException: No non-null blocks found for name: " + blockName.toString() + " in open!");
+		}
+		return null;
+	}
 }
