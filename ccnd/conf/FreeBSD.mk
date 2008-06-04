7ead7b34a9fa2dc69e6c15daad9c1270507d6d73
Bill Janssen <janssen@parc.com>
2008-06-04 02:34:59 +0000

    updated for shared libraries and Python on FreeBSD

--- ccnd/conf/FreeBSD.mk
+++ ccnd/conf/FreeBSD.mk
@@ -1,1 +1,6 @@
+SHEXT=so
+SHLIBNAME=libccn.$(SHEXT).1
+SHLIBDEPS=
+SHARED_LD_FLAGS= -shared -whole-archive -soname=$(SHLIBNAME) -lc
+PLATCFLAGS=-fPIC
 CWARNFLAGS = -Wall
--- ccnd/conf/FreeBSD.sh
+++ ccnd/conf/FreeBSD.sh
@@ -1,3 +1,4 @@
+echo "# FreeBSD.sh $@ was here" >> conf.mk
 #!/bin/sh
 # Make some symlinks so that we can use the native expat.
 set -x
@@ -5,3 +6,25 @@ mkdir -p include
 find include lib -type l -name \*expat\* -exec rm '{}' ';'
 ln -s /usr/include/bsdxml.h include/expat.h
 ln -s /usr/lib/libbsdxml.a lib/libexpat.a
+# check for python that's advanced enough
+PYTHON=`which python`
+if test "$PYTHON" != ""; then 
+  PYTHONVERSION=`$PYTHON <<EOF
+try:
+    import ctypes
+except:
+    print 0
+else:
+    print 1 
+EOF`
+  if test "$PYTHONVERSION" -eq 1; then
+     echo "PYTHON=$PYTHON" >> conf.mk
+     echo "PYTHON_TARGETS=python" >> conf.mk
+  else
+     echo "PYTHON=" >> conf.mk
+     echo "PYTHON_TARGETS=" >> conf.mk
+  fi
+else
+   echo "PYTHON=" >> conf.mk
+   echo "PYTHON_TARGETS=" >> conf.mk
+fi
