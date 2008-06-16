ddc0887352c3f9d68293b6bc126960ad01fc6a5f
Michael Plass <plass@parc.com>
2008-06-16 19:47:12 +0000

    Add ability to build ccnd for use with gcov

--- ccnd/subr.mk
+++ ccnd/subr.mk
@@ -9,6 +9,10 @@ $(OBJDIR)/Makefile: Makefile
 	test -f $(OBJDIR)/Makefile && mv $(OBJDIR)/Makefile $(OBJDIR)/Makefile~ ||:
 	cp -p Makefile $(OBJDIR)/Makefile
 
+coverage:
+	X () { test -f $$1 || return 0; gcov $$*; }; X *.gc??
+
+
 depend: Makefile $(CSRC)
 	echo $(CSRC) | xargs gcc -MM $(CPREFLAGS) > depend
 	tail -n `wc -l < depend` Makefile | diff - depend
