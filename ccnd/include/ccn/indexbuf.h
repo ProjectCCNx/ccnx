0e4fa796253f4fed260b0096aa702c3c9749d42d
Jim Thornton <jthornton@parc.com>
2008-06-17 15:39:16 +0000

    Added two convenience functions for reading path components from an encoded structure with indexbuf component markings

--- ccnd/include/ccn/indexbuf.h
+++ ccnd/include/ccn/indexbuf.h
@@ -25,4 +25,26 @@ size_t *ccn_indexbuf_reserve(struct ccn_indexbuf *c, size_t n);
 int ccn_indexbuf_append(struct ccn_indexbuf *c, const size_t *p, size_t n);
 int ccn_indexbuf_append_element(struct ccn_indexbuf *c, size_t v);
 
+/***********************************
+ * Convenience functions for processing individual components based on 
+ * indexbuf, especially as strings (though components need not be 
+ * strings)
+ */
+
+/*
+ * ccn_indexbuf_comp_strcmp: perform strcmp of given val against 
+ * component.  Returns -1, 0, or 1 if val is less than, equal to,
+ * or greater than the component at given index (counting from 0).
+ * Safe even on binary components, though the result may not be useful.
+ */
+int ccn_indexbuf_comp_strcmp(const char *data, const struct ccn_indexbuf* indexbuf, unsigned int index, const char *val);
+
+/*
+ * ccn_indexbuf_comp_strdup: return a copy of component at given index
+ * as a string, that is, it will be terminated by \0 even if the original
+ * component was not.  The first component is index 0.
+ * Caller is responsible to free returned buffer containing copy.
+ */
+char * ccn_indexbuf_comp_strdup(const char *data, const struct ccn_indexbuf *indexbuf, unsigned int index);
+
 #endif
--- ccnd/lib/ccn_indexbuf.c
+++ ccnd/lib/ccn_indexbuf.c
@@ -75,3 +75,34 @@ ccn_indexbuf_append_element(struct ccn_indexbuf *c, ELEMENT v)
     return(0);
 }
 
+int ccn_indexbuf_comp_strcmp(const char *data, const struct ccn_indexbuf* indexbuf, unsigned int index, const char *val) {
+    /* indexbuf should have an extra value marking end of last component,
+       so we need to use last 2 values */
+    if (index > indexbuf->n-2) {
+	/* There isn't a component at this index, so no match, call
+	   supplied value greater-than */
+	return 1;
+    }
+    return strncmp(val, data + indexbuf->buf[index], indexbuf->buf[index+1]-indexbuf->buf[index]);
+}
+
+char * ccn_indexbuf_comp_strdup(const char *data, const struct ccn_indexbuf *indexbuf, unsigned int index) {
+    char * result;
+    int len;
+    /* indexbuf should have an extra value marking end of last component,
+       so we need to use last 2 values */
+    if (index > indexbuf->n-2) {
+	/* There isn't a component at this index */
+	return NULL;
+    }
+    len = indexbuf->buf[index+1]-indexbuf->buf[index];
+    result = malloc(len + 1); /* add extra byte for safety \0 */
+    if (result == NULL) {
+	return NULL;
+    }
+    memcpy(result, data + indexbuf->buf[index], len);
+    /* Ensure null-terminated by adding \0 here */
+    result[len] = '\0';
+    return result;
+}
+
