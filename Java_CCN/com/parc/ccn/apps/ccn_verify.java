f189203ba8673e1dabd8b692482ebd0455202320
Diana Smetters <smetters@parc.com>
2008-07-16 18:47:26 +0000

    Simple app that takes content objects for key and data and attempts to verify the signature.

---
+++ Java_CCN/com/parc/ccn/apps/ccn_verify.java
@@ -0,0 +1,63 @@
+package com.parc.ccn.apps;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateEncodingException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.xml.stream.XMLStreamException;
+
+import com.parc.ccn.data.ContentObject;
+import com.parc.ccn.security.crypto.certificates.CryptoUtil;
+
+public class ccn_verify {
+	
+	public static void usage() {
+		System.out.println("ccn_verify key_file ccnb_input_file [input_file [input_file...]] ");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length < 2)  {
+			usage();
+			return;
+		}
+		
+		try {
+		
+			PublicKey pubKey = readKeyFile(args[0]);
+	
+			for (int i=1; i < args.length; ++i) {
+				ContentObject co = readObjectFile(args[i]);
+				
+				if (!co.verify(pubKey)) {
+					System.out.println("BAD: Object: " + co.name() + " in file: " + args[i] + " failed to verify.");
+				} else {
+					System.out.println("GOOD: Object: " + co.name() + " in file: " + args[i] + " verified.");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Exception in ccn_verify: " + e.getClass().getName() + ": " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+		
+	public static ContentObject readObjectFile(String filePath) throws FileNotFoundException, XMLStreamException {
+		FileInputStream fis = new FileInputStream(filePath);
+		
+		ContentObject co = new ContentObject();
+		co.decode(fis);
+		return co;
+		
+	}
+	
+	public static PublicKey readKeyFile(String filePath) throws FileNotFoundException, XMLStreamException, CertificateEncodingException, InvalidKeySpecException, NoSuchAlgorithmException {
+		ContentObject keyObject = readObjectFile(filePath);
+		return CryptoUtil.getPublicKey(keyObject.content());
+	}
+
+}
