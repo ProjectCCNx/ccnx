a1ced1a36835618b173d10c91c567df524350309
Nick Briggs <briggs@parc.com>
2008-07-25 17:57:03 +0000

    Add initial cut at signing in ccn_encode_ContentObject and access to PKCS12 keystore to obtain private key

---
+++ ccnd/include/ccn/keystore.h
@@ -0,0 +1,21 @@
+/*
+ * ccn/keystore.h
+ *
+ * KEYSTORE interface
+ *
+ * This is a veneer so that the ccn code can avoid exposure to the
+ * underlying keystore implementation types
+ */
+#ifndef CCN_KEYSTORE_DEFINED
+#define CCN_KEYSTORE_DEFINED
+
+#include <stddef.h>
+
+struct ccn_keystore;
+
+struct ccn_keystore *ccn_keystore_create();
+void ccn_keystore_destroy(struct ccn_keystore **p);
+int ccn_keystore_init(struct ccn_keystore *p, char *name, char *password);
+const void *ccn_keystore_private_key(struct ccn_keystore *p);
+
+#endif
