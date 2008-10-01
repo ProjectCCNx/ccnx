3d515c9fb2242663e6f4118c1499bb96d3514988
Paul Rasmussen <rasmusse@parc.com>
2008-10-01 00:23:27 +0000

    Rearrange endToEnd tests to allow other tests to be built off of these

--- Java_CCN/test/ccn/endtoend/BaseLibrarySink.java
+++ Java_CCN/test/ccn/endtoend/BaseLibrarySink.java
@@ -1,26 +1,19 @@
 package test.ccn.endtoend;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.parc.ccn.Library;
-import com.parc.ccn.data.CompleteName;
 import com.parc.ccn.data.ContentObject;
-import com.parc.ccn.data.query.CCNInterestListener;
-import com.parc.ccn.data.query.Interest;
 import com.parc.ccn.library.CCNLibrary;
 import com.parc.ccn.library.StandardCCNLibrary;
 
 // NOTE: This test requires ccnd to be running and complementary source process
 
-public class BaseLibrarySink implements CCNInterestListener {
+public class BaseLibrarySink {
 	CCNLibrary library = null;
 	Semaphore sema = new Semaphore(0);
 	int next = 0;
@@ -45,75 +38,5 @@ public class BaseLibrarySink implements CCNInterestListener {
 	 */
 	public void checkGetResults(ArrayList<ContentObject> getResults) {
 		System.out.println("Got result: " + getResults.get(0).name());
-	}
-
-	@Test
-	public void gets() throws Throwable {
-		System.out.println("Get sequence started");
-		Random rand = new Random();
-		for (int i = 0; i < BaseLibrarySource.count; i++) {
-			Thread.sleep(rand.nextInt(50));
-			ArrayList<ContentObject> contents = library.get("/BaseLibraryTest/gets/" + i);
-			assertEquals(1, contents.size());
-			int value = contents.get(0).content()[0];
-			// Note that we cannot be guaranteed to pick up every value:
-			// due to timing we may miss a value that arrives while we are not
-			// in the get()
-			assertEquals(true, value >= i);
-			i = value;
-			System.out.println("Got " + i);
-			checkGetResults(contents);
-		}
-		System.out.println("Get sequence finished");
-	}
-	
-	@Test
-	public void server() throws Throwable {
-		System.out.println("GetServer started");
-		Interest interest = new Interest("/BaseLibraryTest/server");
-		// Register interest
-		library.expressInterest(interest, this);
-		// Block on semaphore until enough data has been received
-		sema.acquire();
-		library.cancelInterest(interest, this);
-		if (null != error) {
-			throw error;
-		}
-	}
-	
-	public synchronized Interest handleContent(ArrayList<ContentObject> results) {
-		try {
-			for (ContentObject contentObject : results) {
-				String objString = contentObject.name().toString();
-				Interest interest = new Interest(objString.substring(0, objString.lastIndexOf("/")) + "/" + new Integer(next).toString());
-				// Register interest
-				library.expressInterest(interest, this);
-				next++;
-			}
-			checkGetResults(results);
-			if (next >= BaseLibrarySource.count) {
-				sema.release();
-			}
-		} catch (Throwable e) {
-			error = e;
-		}
-		return null;
-	}
-
-	public void addInterest(Interest interest) {
-	}
-
-	public void cancelInterests() {
-	}
-
-	public Interest[] getInterests() {
-		return null;
-	}
-
-	public void interestTimedOut(Interest interest) {
-	}
-
-	public boolean matchesInterest(CompleteName name) {
-		return false;
 	}
 }
--- Java_CCN/test/ccn/endtoend/BaseLibrarySource.java
+++ Java_CCN/test/ccn/endtoend/BaseLibrarySource.java
@@ -1,36 +1,29 @@
 package test.ccn.endtoend;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.parc.ccn.Library;
 import com.parc.ccn.data.CompleteName;
 import com.parc.ccn.data.ContentName;
-import com.parc.ccn.data.query.CCNFilterListener;
-import com.parc.ccn.data.query.Interest;
 import com.parc.ccn.data.util.BinaryXMLCodec;
 import com.parc.ccn.library.CCNLibrary;
 import com.parc.ccn.library.StandardCCNLibrary;
 
 //NOTE: This test requires ccnd to be running and complementary sink process 
 
-public class BaseLibrarySource implements CCNFilterListener {
+public class BaseLibrarySource {
 	public static int count = 43;
 	protected CCNLibrary library = null;
 	ContentName name = null;
 	int next = 0;
 	protected static Throwable error = null; // for errors in callback
 	Semaphore sema = new Semaphore(0);
-	static Random rand;
+	protected static Random rand;
 
 	public BaseLibrarySource() throws Throwable {
 		library = StandardCCNLibrary.open();
@@ -75,55 +68,5 @@ public class BaseLibrarySource implements CCNFilterListener {
 			
 		}
 		return result;
-	}
-	
-	@Test
-	public void puts() throws Throwable {
-		assert(count <= Byte.MAX_VALUE);
-		System.out.println("Put sequence started");
-		for (int i = 0; i < count; i++) {
-			Thread.sleep(rand.nextInt(50));
-			byte[] content = getRandomContent(i);
-			CompleteName putName = library.put(ContentName.fromNative("/BaseLibraryTest/gets/" + new Integer(i).toString()), content);
-			System.out.println("Put " + i + " done: " + content.length + " content bytes");
-			checkPutResults(putName);
-		}
-		System.out.println("Put sequence finished");
-	}
-	
-	@Test
-	public void server() throws Throwable {
-		System.out.println("PutServer started");
-		// Register filter
-		name = ContentName.fromNative("/BaseLibraryTest/");
-		library.setInterestFilter(name, this);
-		// Block on semaphore until enough data has been received
-		sema.acquire();
-		library.cancelInterestFilter(name, this);
-		if (null != error) {
-			throw error;
-		}
-	}
-
-	public synchronized int handleInterests(ArrayList<Interest> interests) {
-		try {
-			if (next >= count) {
-				return 0;
-			}
-			for (Interest interest : interests) {
-				assertTrue(name.isPrefixOf(interest.name()));
-				byte[] content = getRandomContent(next);
-				CompleteName putName = library.put(ContentName.fromNative("/BaseLibraryTest/server/" + new Integer(next).toString()), content);
-				System.out.println("Put " + next + " done: " + content.length + " content bytes");
-				checkPutResults(putName);
-				next++;
-			}
-			if (next >= count) {
-				sema.release();
-			}
-		} catch (Throwable e) {
-			error = e;
-		}
-		return 0;
 	}
 }
