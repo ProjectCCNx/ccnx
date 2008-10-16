6a3b1ee882b005ff992484c7f64b47394b6f1261
Michael Plass <plass@parc.com>
2008-10-16 20:44:31 +0000

    ccncatchunks: add -a option to allow stale data.

--- ccnd/lib/ccncatchunks.c
+++ ccnd/lib/ccncatchunks.c
@@ -4,14 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ccn/bloom.h>
 #include <ccn/ccn.h>
 #include <ccn/charbuf.h>
 #include <ccn/uri.h>
 
+static void
+usage(const char *progname)
+{
+    fprintf(stderr,
+            "%s [-a] ccn:/a/b\n"
+            "   Reads stuff written by ccnsendchunks under"
+            " the given uri and writes to stdout\n"
+            "   -a - allow stale data\n",
+            progname);
+    exit(1);
+}
+
 struct excludestuff;
 
 struct mydata {
+    int allow_stale;
     struct excludestuff *excl;
 };
 
@@ -78,6 +92,15 @@ make_template(struct mydata *md, struct ccn_upcall_info *info)
     ccn_charbuf_append_tt(templ, CCN_DTAG_Name, CCN_DTAG);
     ccn_charbuf_append_closer(templ); /* </Name> */
     // XXX - use pubid if possible
+    ccn_charbuf_append_tt(templ, CCN_DTAG_AdditionalNameComponents, CCN_DTAG);
+    ccn_charbuf_append_non_negative_integer(templ, 1);
+    ccn_charbuf_append_closer(templ); /* </AdditionalNameComponents> */
+    if (md->allow_stale) {
+        ccn_charbuf_append_tt(templ, CCN_DTAG_AnswerOriginKind, CCN_DTAG);
+        ccn_charbuf_append_non_negative_integer(templ,
+                                                CCN_AOK_DEFAULT | CCN_AOK_STALE);
+        ccn_charbuf_append_closer(templ); /* </AnswerOriginKind> */
+    }
     nexcl = count_excludestuff(md->excl);
     if (nexcl != 0) {
         long r = lrand48();
@@ -195,22 +218,33 @@ main(int argc, char **argv)
     struct ccn_charbuf *name = NULL;
     struct ccn_charbuf *templ = NULL;
     struct ccn_closure *incoming = NULL;
+    const char *arg = NULL;
     int res;
-    if (argv[1] == NULL) {
-        fprintf(stderr, "%s: Reads stuff written by ccnsendchunks under "
-                "the given uri and writes to stdout\n", argv[0]);
+    char ch;
+    struct mydata *mydata;
+    int allow_stale = 0;
+    
+    while ((ch = getopt(argc, argv, "ha")) != -1) {
+        switch (ch) {
+            case 'a':
+                allow_stale = 1;
+                break;
+            case 'h':
+            default:
+                usage(argv[0]);
+        }
+    }
+    arg = argv[optind];
+    if (arg == NULL)
+        usage(argv[0]);
+    name = ccn_charbuf_create();
+    res = ccn_name_from_uri(name, arg);
+    if (res < 0) {
+        fprintf(stderr, "%s: bad ccn URI: %s\n", argv[0], arg);
         exit(1);
     }
-    else {
-        name = ccn_charbuf_create();
-        res = ccn_name_from_uri(name, argv[1]);
-        if (res < 0) {
-            fprintf(stderr, "%s: bad ccn URI: %s\n", argv[0], argv[1]);
-            exit(1);
-        }
-        if (argv[2] != NULL)
-            fprintf(stderr, "%s warning: extra arguments ignored\n", argv[0]);
-    }
+    if (argv[optind + 1] != NULL)
+        fprintf(stderr, "%s warning: extra arguments ignored\n", argv[0]);
     ccn = ccn_create();
     if (ccn_connect(ccn, NULL) == -1) {
         perror("Could not connect to ccnd");
@@ -219,15 +253,18 @@ main(int argc, char **argv)
     ccn_name_append(name, "0", 1);
     incoming = calloc(1, sizeof(*incoming));
     incoming->p = &incoming_content;
-    incoming->data = calloc(1, sizeof(struct mydata));
-    
-    /* XXX - should use a template to tailor the interest more carefully. */
+    mydata = calloc(1, sizeof(*mydata));
+    mydata->allow_stale = allow_stale;
+    mydata->excl = NULL;
+    incoming->data = mydata;
+    templ = make_template(mydata, NULL);
     ccn_express_interest(ccn, name, -1, incoming, templ);
+    ccn_charbuf_destroy(&templ);
     ccn_charbuf_destroy(&name);
     /* Run a little while to see if there is anything there */
     res = ccn_run(ccn, 200);
     if (incoming->intdata == 0) {
-        fprintf(stderr, "%s: not found: %s\n", argv[0], argv[1]);
+        fprintf(stderr, "%s: not found: %s\n", argv[0], arg);
         exit(1);
     }
     /* We got something, run until end of data or somebody kills us */
