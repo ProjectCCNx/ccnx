542567a26a0c1fbe9b21f007ce08be30a9bb47fd
Nathan Good <ngood@parc.com>
2008-12-01 22:37:11 +0000

    Nathan's first test CCN app.

---
+++ apps/containerApp/src/com/parc/ccn/apps/container/ContainerGUItest.java
@@ -0,0 +1,86 @@
+package com.parc.ccn.apps.container;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ContainerGUItest extends JPanel implements ActionListener {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7198145969014262490L;
+	protected JTextField textField;
+    protected JTextArea textArea;
+    private final static String newline = "\n";
+
+    public ContainerGUItest() {
+        super(new GridBagLayout());
+
+        textField = new JTextField(20);
+        textField.addActionListener(this);
+
+        textArea = new JTextArea(5, 20);
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        //Add Components to this panel.
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridwidth = GridBagConstraints.REMAINDER;
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        add(textField, c);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        add(scrollPane, c);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        String text = textField.getText();
+        textArea.append(text + newline);
+        textField.selectAll();
+
+        //Make sure the new text is visible, even if there
+        //was a selection in the text area.
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Testing The Containers");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add contents to the window.
+        frame.add(new ContainerGUItest());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+
+
+}
