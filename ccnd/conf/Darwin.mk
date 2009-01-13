7bbcf54a45df59a12f60c6359a71b3ab0036b00a
Bill Janssen <janssen@parc.com>
2009-01-13 23:58:23 +0000

    mv def of libcrypto above shlib link line

--- ccnd/conf/Darwin.mk
+++ ccnd/conf/Darwin.mk
@@ -1,8 +1,8 @@
+OPENSSL_CFLAGS = -I/opt/local/include
+OPENSSL_LIBS = -L/opt/local/lib
 SHEXT=dylib
 SHLIBNAME=libccn.1.$(SHEXT)
 SHLIBDEPS=/usr/lib/dylib1.o
-SHARED_LD_FLAGS = -dylib -arch `/usr/bin/arch` -install_name $(SHLIBNAME) -all_load /usr/lib/dylib1.o -lSystem
+SHARED_LD_FLAGS = -dylib -arch `/usr/bin/arch` -install_name $(SHLIBNAME) $(OPENSSL_LIBS) -all_load /usr/lib/dylib1.o -lcrypto -lSystem
 PLATCFLAGS=-fno-common
 CWARNFLAGS = -Wall -Wpointer-arith -Wreturn-type -Wstrict-prototypes
-OPENSSL_CFLAGS = -I/opt/local/include
-OPENSSL_LIBS = -L/opt/local/lib
