153124ac8dd01a4762268b5ce5347f62d6483704
Nick Briggs <briggs@parc.com>
2009-01-21 01:33:48 +0000

    Add -a flag to allow dump of stale data

--- ccnd/lib/ccndump.c
+++ ccnd/lib/ccndump.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include <ccn/ccn.h>
 #include <ccn/uri.h>
 
@@ -14,12 +16,21 @@
 </Interest>
 **********/
 struct ccn_charbuf *
-local_scope_template(void)
+local_scope_template(int allow_stale)
 {
     struct ccn_charbuf *templ = ccn_charbuf_create();
     ccn_charbuf_append_tt(templ, CCN_DTAG_Interest, CCN_DTAG);
+    /* <Name> */
     ccn_charbuf_append_tt(templ, CCN_DTAG_Name, CCN_DTAG);
     ccn_charbuf_append_closer(templ); /* </Name> */
+    if (allow_stale) {
+        /* <AnswerOriginKind>5</AnswerOriginKind> */
+        ccn_charbuf_append_tt(templ, CCN_DTAG_AnswerOriginKind, CCN_DTAG);
+        ccn_charbuf_append_tt(templ, 1, CCN_UDATA);
+        ccn_charbuf_putf(templ, "%d", (int)(CCN_AOK_CS + CCN_AOK_STALE));
+        ccn_charbuf_append_closer(templ); /* </AnswerOriginKind> */
+    }
+    /* <Scope>0</Scope> */
     ccn_charbuf_append_tt(templ, CCN_DTAG_Scope, CCN_DTAG);
     ccn_charbuf_append_tt(templ, 1, CCN_UDATA);
     ccn_charbuf_append(templ, "0", 1);
@@ -74,14 +85,40 @@ static struct ccn_closure incoming_content_action = {
     .data = &mydata
 };
 
+static void
+usage(const char *progname)
+{
+    fprintf(stderr,
+            "%s [-a] [uri]\n"
+            "   Dumps everything quickly retrievable\n"
+            "   -a - allow stale data\n",
+            progname);
+    exit(1);
+}
+
 int
 main(int argc, char **argv)
 {
     struct ccn *ccn = NULL;
     struct ccn_charbuf *c = NULL;
     struct ccn_charbuf *templ = NULL;
+    int allow_stale = 0;
     int i;
+    int ch;
     int res;
+    extern int optind;
+
+    while ((ch = getopt(argc, argv, "ha")) != -1) {
+        switch (ch) {
+            case 'a':
+                allow_stale = 1;
+                break;
+            case 'h':
+            default:
+                usage(argv[0]);
+        }
+    }
+    
     ccn = ccn_create();
     if (ccn_connect(ccn, NULL) == -1) {
         perror("Could not connect to ccnd");
@@ -89,16 +126,16 @@ main(int argc, char **argv)
     }
     c = ccn_charbuf_create();
     /* set scope to only address ccnd */
-    templ = local_scope_template();
-    if (argv[1] == NULL)
+    templ = local_scope_template(allow_stale);
+    if (argv[optind] == NULL)
         ccn_name_init(c);
     else {
-        res = ccn_name_from_uri(c, argv[1]);
+        res = ccn_name_from_uri(c, argv[optind]);
         if (res < 0) {
-            fprintf(stderr, "%s: bad ccn URI: %s\n", argv[0], argv[1]);
+            fprintf(stderr, "%s: bad ccn URI: %s\n", argv[0], argv[optind]);
             exit(1);
         }
-        if (argv[2] != NULL)
+        if (argv[optind+1] != NULL)
             fprintf(stderr, "%s warning: extra arguments ignored\n", argv[0]);
     }
     ccn_express_interest(ccn, c, -1, &incoming_content_action, templ);
