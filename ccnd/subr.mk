f65a6060164269abd0b6c4aa7b66841467160a58
Michael Plass <plass@parc.com>
2009-02-11 18:43:13 +0000

    Added better "install" target for ccnd and friends; installs selected programs as well as libs and headers.  Also added "uninstall" target for removal.

--- ccnd/subr.mk
+++ ccnd/subr.mk
@@ -9,6 +9,24 @@ $(OBJDIR)/Makefile: Makefile
 	test -f $(OBJDIR)/Makefile && mv $(OBJDIR)/Makefile $(OBJDIR)/Makefile~ ||:
 	cp -p Makefile $(OBJDIR)/Makefile
 
+install_libs: $(LIBS)
+	test -d $(INSTALL_LIB)
+	for i in $(LIBS); do $(INSTALL) $$i $(INSTALL_LIB); done
+
+install_programs: $(INSTALLED_PROGRAMS)
+	test -d $(INSTALL_BIN)
+	for i in $(INSTALLED_PROGRAMS); do $(INSTALL) $$i $(INSTALL_BIN); done
+
+install: install_libs install_programs
+
+uninstall_libs: $(LIBS)
+	for i in $(LIBS); do $(RM) $(INSTALL_LIB)/$$i; done
+
+uninstall_programs: $(PROGRAMS)
+	for i in $(PROGRAMS); do $(RM) $(INSTALL_BIN)/$$i; done
+
+uninstall: uninstall_libs uninstall_programs
+
 coverage:
 	X () { test -f $$1 || return 0; gcov $$*; }; X *.gc??
 
