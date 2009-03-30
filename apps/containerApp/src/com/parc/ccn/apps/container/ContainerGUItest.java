ce3ec025d27d13365b2c67b00cbaeef3c3c0e5e3
Nathan Good <ngood@parc.com>
2009-03-30 23:02:59 +0000

    Changes to update containerGUItest with current code base. Added in file browser, not attached to CCN (as CCN isn't working yet)

--- apps/containerApp/src/com/parc/ccn/apps/container/ContainerGUItest.java
+++ apps/containerApp/src/com/parc/ccn/apps/container/ContainerGUItest.java
@@ -5,14 +5,23 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidKeyException;
 import java.security.SignatureException;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+import javax.xml.stream.XMLStreamException;
 
 import com.parc.ccn.Library;
 import com.parc.ccn.config.ConfigurationException;
@@ -34,6 +43,8 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
     protected static JTextArea textArea;
     final static String newline = "\n";
     protected CCNLibrary library;
+    private static JTree tree;
+
     
     /* Model */
     protected Collection currentDirectory;
@@ -70,18 +81,27 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
         c.weighty = 1.0;
         add(scrollPane, c);
         
+
+        
         
       //CCN Stuff
     	library = CCNLibrary.open();
     	directoryName = ContentName.fromNative(DIRECTORY_NAME);
     	contentPrefix = ContentName.fromNative(CONTENT_PREFIX);
     	
-    	ContentObject directoryObject = library.getLatest(directoryName, DEFAULT_TIMEOUT);
+    	//ContentObject directoryObject = library.getLatest(directoryName, DEFAULT_TIMEOUT);
     	   	
-     	if (null == directoryObject) {
-    		directoryObject = library.put(directoryName, new LinkReference[0]);
-    	}
-		currentDirectory = library.decodeCollection(directoryObject);
+     	//if (null == directoryObject) {
+    	//	directoryObject = library.put(directoryName, new LinkReference[0]);
+    	//}
+     	
+		try {
+			currentDirectory = library.getCollection(directoryName, DEFAULT_TIMEOUT);
+		} catch (XMLStreamException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+			//library.decodeCollection(directoryObject);
 
 		namedContent.clear();
 		for (LinkReference lr : currentDirectory.contents()) {
@@ -94,7 +114,7 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
    		// When I want to get some content, not quite sure how these interest thingy works 
 		try {
 			// DKS temporary hack to say "last (latest version) object *after* this one"
-			Interest interest = Interest.next(directoryObject, null);
+			Interest interest = Interest.next(currentDirectory, null);
 			interest.orderPreference(Interest.ORDER_PREFERENCE_RIGHT | Interest.ORDER_PREFERENCE_ORDER_NAME);
 			Library.logger().info("Expressing initial interest: " + interest.name());
 			library.expressInterest(interest, this);
@@ -104,6 +124,12 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
 		}
     }
 
+
+
+    
+    
+
+    
     public void actionPerformed(ActionEvent evt) {
         String text = textField.getText();
         
@@ -118,19 +144,21 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
         //put content into ccnd
 		
 		//Library.logger().info(name.toString());
-        currentDirectory.add(name);
+       // currentDirectory.add(name);
         try {
-			ContentObject newDirectory = library.addToCollection(directoryName, new ContentName[]{name}, 0);
-    		currentDirectory = library.decodeCollection(newDirectory);
+			//ContentObject newDirectory = library.addToCollection(directoryName, new ContentName[]{name}, 0);
+			currentDirectory = library.addToCollection(currentDirectory, new ContentName[]{name},DEFAULT_TIMEOUT);
+			
+			//currentDirectory = library.decodeCollection(newDirectory);
 			library.put(name, text.getBytes());
-			Library.logger().info("Put new directory (" + currentDirectory.size() + " items): " + newDirectory.name());
+			Library.logger().info("Put new directory (" + currentDirectory.size() + " items): " + name);
 		} catch (SignatureException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		} catch (InterruptedException e1) {
+		} /*catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
@@ -153,6 +181,13 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
         //namedContent.add(name);
         //repopulate the list with the 
         //populateList();
+ catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         textField.selectAll();
 
@@ -192,25 +227,155 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
         frame.setVisible(true);
     }
 
+    public static class HelloWorldFrame extends JFrame {
+
+    	public void main(String args[]) {
+    		new HelloWorldFrame();
+    	}
+    	HelloWorldFrame() {
+    		JLabel jlbHelloWorld = new JLabel("hello world");
+    		add(jlbHelloWorld);
+    		
+            //tree browser related
+            DefaultMutableTreeNode top =
+                new DefaultMutableTreeNode("CCN");
+            createNodes(top);
+            tree = new JTree(top);
+            
+            tree.getSelectionModel().setSelectionMode
+            (TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+            //Listen for when the selection changes.
+            //tree.addTreeSelectionListener((TreeSelectionListener) this);
+
+            JScrollPane treeView = new JScrollPane(tree);
+            add(treeView);
+    		
+    		
+    		this.setSize(100, 100);
+    		// pack();
+    		setVisible(true);
+    	}
+    	
+    	// Tree functions    
+        private void createNodes(DefaultMutableTreeNode top) {
+            DefaultMutableTreeNode category = null;
+            DefaultMutableTreeNode book = null;
+            
+            category = new DefaultMutableTreeNode("CCN STUFF");
+            top.add(category);
+            
+            
+            book = new DefaultMutableTreeNode(new BookInfo
+                ("More CCN Stuff",
+                "CCN/Location"));
+            category.add(book);
+            
+            
+            book = new DefaultMutableTreeNode(new BookInfo
+                ("Look at all this CCN Stuff",
+                "CCN/Location"));
+            category.add(book);
+            
+            
+            book = new DefaultMutableTreeNode(new BookInfo
+                ("Is this yet more CCN Stuff",
+                "CCN/location"));
+            category.add(book);
+
+            //...add more books for programmers...
+
+            category = new DefaultMutableTreeNode("CCN Stuff in another container");
+            top.add(category);
+
+            //VM
+            book = new DefaultMutableTreeNode(new BookInfo
+                ("CCN Stuff someplace else",
+                 "CCN/location"));
+            category.add(book);
+
+            //Language Spec
+            book = new DefaultMutableTreeNode(new BookInfo
+                ("Other CCN Stuff",
+                 "CCN/location"));
+            category.add(book);
+        }
+
+        //more tree relates stuff
+        public void valueChanged(TreeSelectionEvent e) {
+        	//Returns the last path element of the selection.
+        	//This method is useful only when the selection model allows a single selection.
+        	    DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+        	                       tree.getLastSelectedPathComponent();
+
+        	    if (node == null)
+        	    //Nothing is selected.	
+        	    return;
+
+        	    Object nodeInfo = node.getUserObject();
+        	    if (node.isLeaf()) {
+        	        BookInfo book = (BookInfo)nodeInfo;
+        	        //displayURL(book.bookURL);
+        	    } else {
+        	        //displayURL(helpURL); 
+        	    }
+        	}
+    // For the tree demo
+        private class BookInfo {
+            public String bookName;
+            public URL bookURL;
+
+            public BookInfo(String book, String filename) {
+                bookName = book;
+               /* bookURL = getClass().getResource(filename);
+                if (bookURL == null) {
+                    System.err.println("Couldn't find file: "
+                                       + filename);
+                }*/
+            }
+
+            public String toString() {
+                return bookName;
+            }
+        }
+
+        /*private void displayURL(URL url) {
+            try {
+                if (url != null) {
+                    htmlPane.setPage(url);
+                } else { //null url
+    		htmlPane.setText("File Not Found");
+                    if (DEBUG) {
+                        System.out.println("Attempted to display a null URL.");
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Attempted to read a bad URL: " + url);
+            }
+        }*/
+    }
+
     public static void main(String[] args) {
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+            	new HelloWorldFrame();
                 createAndShowGUI();
             }
         });
     
     }
 
-	@Override
+	//@Override
 	public Interest handleContent(ArrayList<ContentObject> results,
 			Interest interest) {
 		Collection newCollection = null;
 		ContentObject collectionObject = null;
 		for (ContentObject result : results) {
 			try {
-				newCollection = library.decodeCollection(result);
+				newCollection = Collection.contentToCollection(result);
+				//library.decodeCollection(result);
 				
 				Library.logger().info("New directory: " + result.name());
 				
@@ -218,8 +383,10 @@ public class ContainerGUItest extends JPanel implements ActionListener, CCNInter
 				
 				//String text = "New directory: " + result.name();
 				//textArea.append(text + newline);
-			} catch (IOException e) {
-				continue;
+			} 
+			 catch (XMLStreamException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 		if (null != newCollection) {
