bbf9ee0908636a24885e160424f89d70e87850af
Paul Rasmussen <rasmusse@parc.com>
2009-04-10 20:55:05 +0000

    Increase timeout more

--- Java_CCN/test/ccn/network/daemons/repo/RemoteRepoIOGetTest.java
+++ Java_CCN/test/ccn/network/daemons/repo/RemoteRepoIOGetTest.java
@@ -47,7 +47,7 @@ public class RemoteRepoIOGetTest extends RepoTestBase {
 	
 	private ContentObject checkContent(ContentName contentName, boolean expected) throws Exception {
 		Interest interest = new Interest(contentName);
-		ContentObject co = getLibrary.get(interest, 10000);
+		ContentObject co = getLibrary.get(interest, 20000);
 		if (expected)
 			Assert.assertTrue(co != null);
 		else
