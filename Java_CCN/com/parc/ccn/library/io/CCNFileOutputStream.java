0f22045e55182eebfecedb53c0cf9e741406dd76
Diana Smetters <smetters@parc.com>
2009-04-14 22:05:03 +0000

    Pull roots out of header so that header can move up. Header probably won't have roots in it at all, odds are it won't be the first block you get so it won't help much anyway.

--- Java_CCN/com/parc/ccn/library/io/CCNFileOutputStream.java
+++ Java_CCN/com/parc/ccn/library/io/CCNFileOutputStream.java
@@ -47,8 +47,7 @@ public class CCNFileOutputStream extends CCNVersionedOutputStream {
 
 	protected void writeHeader() throws InvalidKeyException, SignatureException, IOException, InterruptedException {
 		// What do we put in the header if we have multiple merkle trees?
-		putHeader(_baseName, (int)_totalLength, _blockSize, _dh.digest(), 
-				((_roots.size() > 0) ? _roots.get(0) : null),
+		putHeader(_baseName, (int)_totalLength, _blockSize, _dh.digest(), null,
 				_timestamp, _locator, _publisher);
 		Library.logger().info("Wrote header: " + SegmentationProfile.headerName(_baseName));
 	}
