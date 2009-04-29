f311fad7bc4f8fd88aeb29a45f1c8ae17b1797e1
Jim Thornton <jthornton@parc.com>
2009-04-28 19:21:36 -0700

    Reorganized flow control test into individual test cases with uniform reset at start of each, refs #23

--- Java_CCN/test/ccn/library/CCNFlowControlTest.java
+++ Java_CCN/test/ccn/library/CCNFlowControlTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import com.parc.ccn.config.ConfigurationException;
 import com.parc.ccn.data.ContentName;
 import com.parc.ccn.data.ContentObject;
+import com.parc.ccn.data.MalformedContentNameStringException;
 import com.parc.ccn.data.query.Interest;
 import com.parc.ccn.data.security.Signature;
 import com.parc.ccn.data.security.SignedInfo;
@@ -23,42 +24,54 @@ import com.parc.ccn.library.profiles.VersioningProfile;
 public class CCNFlowControlTest {
 	static private CCNLibraryTestHarness _library ;
 	
+	static ContentName name1;
+	static ContentName v1;
+	static ContentName v2;	
+
 	static {
 		try {
 			_library = new CCNLibraryTestHarness();
+			
+			name1 = ContentName.fromNative("/foo/bar");
+			v1 = VersioningProfile.versionName(name1);
+			// JDT TODO -- sleep is needed because no easy way yet to generate 
+			// separate version numbers if generating names fast.
+			Thread.sleep(2);
+			v2 = VersioningProfile.versionName(name1);	
+
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (MalformedContentNameStringException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// no-op
 		}
 	}
 	
+	ContentObject obj1 = new ContentObject(name1, new SignedInfo(), "test".getBytes(), (Signature)null);
+	ContentName v1s1 = SegmentationProfile.segmentName(v1, 1);
+	ContentObject objv1s1 = new ContentObject(v1s1, new SignedInfo(), "v1s1".getBytes(), (Signature)null);		
+	ContentName v1s2 = SegmentationProfile.segmentName(v1, 2);
+	ContentObject objv1s2 = new ContentObject(v1s2, new SignedInfo(), "v1s2".getBytes(), (Signature)null);	
+	ContentName v1s3 = SegmentationProfile.segmentName(v1, 3);
+	ContentObject objv1s3 = new ContentObject(v1s3, new SignedInfo(), "v1s3".getBytes(), (Signature)null);	
+	ContentName v1s4 = SegmentationProfile.segmentName(v1, 4);
+	ContentObject objv1s4 = new ContentObject(v1s4, new SignedInfo(), "v1s4".getBytes(), (Signature)null);	
+	ContentName v2s1 = SegmentationProfile.segmentName(v2, 1);
+	ContentObject objv2s1 = new ContentObject(v2s1, new SignedInfo(), "v2s1".getBytes(), (Signature)null);	
+	ContentName v2s2 = SegmentationProfile.segmentName(v2, 2);
+	ContentObject objv2s2 = new ContentObject(v2s2, new SignedInfo(), "v2s2".getBytes(), (Signature)null);
+	Queue<ContentObject> queue = _library.getOutputQueue();
+	ArrayList<Interest> interestList = new ArrayList<Interest>();
+	CCNFlowControl fc = new CCNFlowControl(_library);
+
 	@Test
-	public void testControlFlow() throws Throwable {
-		
-		ContentName name1 = ContentName.fromNative("/foo/bar");
-		ContentObject obj1 = new ContentObject(name1, new SignedInfo(), "test".getBytes(), (Signature)null);
-		ContentName v1 = VersioningProfile.versionName(name1);
-		Thread.sleep(20);
-		ContentName v2 = VersioningProfile.versionName(name1);	
-		ContentName v1s1 = SegmentationProfile.segmentName(v1, 1);
-		ContentObject objv1s1 = new ContentObject(v1s1, new SignedInfo(), "v1s1".getBytes(), (Signature)null);		
-		ContentName v1s2 = SegmentationProfile.segmentName(v1, 2);
-		ContentObject objv1s2 = new ContentObject(v1s2, new SignedInfo(), "v1s2".getBytes(), (Signature)null);	
-		ContentName v1s3 = SegmentationProfile.segmentName(v1, 3);
-		ContentObject objv1s3 = new ContentObject(v1s3, new SignedInfo(), "v1s3".getBytes(), (Signature)null);	
-		ContentName v1s4 = SegmentationProfile.segmentName(v1, 4);
-		ContentObject objv1s4 = new ContentObject(v1s4, new SignedInfo(), "v1s4".getBytes(), (Signature)null);	
-		ContentName v2s1 = SegmentationProfile.segmentName(v2, 1);
-		ContentObject objv2s1 = new ContentObject(v2s1, new SignedInfo(), "v2s1".getBytes(), (Signature)null);	
-		ContentName v2s2 = SegmentationProfile.segmentName(v2, 2);
-		ContentObject objv2s2 = new ContentObject(v2s2, new SignedInfo(), "v2s2".getBytes(), (Signature)null);
+	public void testBasicControlFlow() throws Throwable {	
 		
 		System.out.println("Testing basic control flow functionality and errors");
 		_library.reset();
-		Queue<ContentObject> queue = _library.getOutputQueue();
-		CCNFlowControl fc = new CCNFlowControl(_library);
-		ArrayList<Interest> interestList = new ArrayList<Interest>();
 		try {
 			fc.put(obj1);
 			Assert.fail("Put with no namespace succeeded");
@@ -72,10 +85,15 @@ public class CCNFlowControlTest {
 		try {
 			fc.put(obj1);
 		} catch (IOException e) {
-			Assert.fail("Put with bad namespace succeeded");
+			Assert.fail("Put with good namespace failed");
 		}
 		
-		_library.reset();
+	}
+	
+	@Test
+	public void testInterestFirst() throws Throwable {	
+		
+		normalReset(name1);
 		System.out.println("Testing interest arrives before a put");
 		interestList.add(new Interest("/bar"));
 		fc.handleInterests(interestList);
@@ -85,11 +103,13 @@ public class CCNFlowControlTest {
 		fc.handleInterests(interestList);
 		fc.put(obj1);
 		testExpected(queue.poll(), obj1);
-		
+	}
+	
+	@Test
+	public void testNextBeforePut() throws Throwable {	
+
 		System.out.println("Testing \"next\" interest arrives before a put");
-		_library.reset();
-		interestList.clear();
-		fc = new CCNFlowControl(name1, _library);
+		normalReset(name1);
 		interestList.add(Interest.next(v1s2));
 		fc.handleInterests(interestList);
 		fc.put(objv1s1);
@@ -97,10 +117,13 @@ public class CCNFlowControlTest {
 		fc.put(objv1s3);
 		testExpected(queue.poll(), objv1s3);
 		
+	}
+	
+	@Test
+	public void testLastBeforePut() throws Throwable {	
+
 		System.out.println("Testing \"last\" interest arrives before a put");
-		_library.reset();
-		interestList.clear();
-		fc = new CCNFlowControl(name1, _library);
+		normalReset(name1);
 		interestList.add(Interest.last(v1s2));
 		fc.handleInterests(interestList);
 		fc.put(objv1s1);
@@ -108,20 +131,26 @@ public class CCNFlowControlTest {
 		fc.put(objv1s3);
 		testExpected(queue.poll(), objv1s3);
 		
+	}
+	
+	@Test
+	public void testPutsOrdered() throws Throwable {	
+
 		System.out.println("Testing puts output in correct order");
-		_library.reset();
-		interestList.clear();
-		fc = new CCNFlowControl(name1, _library);
+		normalReset(name1);
 		interestList.add(new Interest("/foo"));
 		fc.handleInterests(interestList);
 		fc.put(obj1);
 		testExpected(queue.poll(), obj1);
 		
-		_library.reset();
-		interestList.clear();
-		fc = new CCNFlowControl(name1, _library);
+	} 
+	
+	@Test
+	public void testRandomOrderPuts() throws Throwable {	
+
+		normalReset(name1);
 		
-		// Put these in in slightly random order. It would be nice to truly randomize this but am
+		// Put these in slightly random order. It would be nice to truly randomize this but am
 		// not going to bother with that right now.
 		fc.put(objv2s1);
 		fc.put(objv1s4);
@@ -135,8 +164,12 @@ public class CCNFlowControlTest {
 		co = testNext(co, objv1s4);
 		co = testNext(co, objv2s1);
 		
-		_library.reset();
-		fc = new CCNFlowControl(name1, _library);
+	}
+
+	@Test
+	public void testWaitForPutDrain() throws Throwable {	
+
+		normalReset(name1);
 		fc.put(objv1s2);
 		fc.put(objv2s1);
 		fc.put(objv1s4);
@@ -161,6 +194,12 @@ public class CCNFlowControlTest {
 			fc.waitForPutDrain();
 			Assert.fail("WaitforPutDrain succeeded when it should have failed");
 		} catch (IOException ioe) {}
+	}
+	
+	private void normalReset(ContentName n) {
+		_library.reset();
+		interestList.clear();
+		fc = new CCNFlowControl(n, _library);
 	}
 	
 	private ContentObject testNext(ContentObject co, ContentObject expected) throws InvalidParameterException, IOException {
