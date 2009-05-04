cf021b0a4e23ca4cd93697f2708208637f2df3ca
Nick Briggs <briggs@parc.com>
2009-05-04 16:32:06 -0700

    start to flesh out the CCN packet dissector

--- ccnd/pcap/ccn/packet-ccn.c
+++ ccnd/pcap/ccn/packet-ccn.c
@@ -28,77 +28,155 @@
 #include <epan/packet.h>
 #include <epan/prefs.h>
 
+#include <string.h>
+#include <ccn/ccn.h>
+#include <ccn/coding.h>
+#include <ccn/uri.h>
+
 /* forward reference */
 void proto_register_ccn();
 void proto_reg_handoff_ccn();
-static void dissect_ccn(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
-static gboolean dissect_ccn_heur( tvbuff_t *tvb, packet_info *pinfo,
-                                  proto_tree *tree );
+static int dissect_ccn(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static gboolean dissect_ccn_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
 
 static int proto_ccn = -1;
+static gint ett_ccn = -1;
+static gint ett_type = -1;
 static int global_ccn_port = 4485;
-static dissector_handle_t ccn_handle;
-
-/* Try heuristic CCN decode */
-static gboolean global_ccn_heur = FALSE;
-
+static dissector_handle_t ccn_handle = NULL;
 
 void
 proto_register_ccn(void)
 {
-	if (proto_ccn == -1) {
-		proto_ccn = proto_register_protocol (
-			"CCN Protocol",	/* name */
-			"CCN",		/* short name */
-			"ccn"		/* abbrev */
-			);
-	}
+    module_t *ccn_module;
+    static gint *ett[] = {
+        &ett_ccn,
+        &ett_type,
+    };
+    proto_ccn = proto_register_protocol("Content-centric Networking Protocol", /* name */
+                                        "CCN",		/* short name */
+                                        "ccn");		/* abbrev */
+    proto_register_subtree_array(ett, array_length(ett));
+    ccn_module = prefs_register_protocol(proto_ccn, proto_reg_handoff_ccn);
 }
 
 void
 proto_reg_handoff_ccn(void)
 {
-	static gboolean initialized = FALSE;
+    static gboolean initialized = FALSE;
+    static int current_ccn_port = -1;
 
-	if (!initialized) {
-		ccn_handle = create_dissector_handle(dissect_ccn, proto_ccn);
-		dissector_add("udp.port", global_ccn_port, ccn_handle);
-        dissector_add_handle("udp.port", ccn_handle); 
+    if (!initialized) {
+        ccn_handle = new_create_dissector_handle(dissect_ccn, proto_ccn);
         heur_dissector_add("udp", dissect_ccn_heur, proto_ccn);
-		initialized = TRUE;
-	}
+        initialized = TRUE;
+    }
+    if (current_ccn_port != -1) {
+        dissector_delete("udp.port", current_ccn_port, ccn_handle);
+    }
+    dissector_add("udp.port", global_ccn_port, ccn_handle);
+    current_ccn_port = global_ccn_port;
 }
-   
-static void
+
+/*
+ * Dissector that returns:
+ *
+ *	The amount of data in the protocol's PDU, if it was able to
+ *	dissect all the data;
+ *
+ *	0, if the tvbuff doesn't contain a PDU for that protocol;
+ *
+ *	The negative of the amount of additional data needed, if
+ *	we need more data (e.g., from subsequent TCP segments) to
+ *	dissect the entire PDU.
+ */ 
+static int
 dissect_ccn(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
-	if (check_col(pinfo->cinfo, COL_PROTOCOL)) {
-		col_set_str(pinfo->cinfo, COL_PROTOCOL, "CCN");
-	}
-	/* Clear out stuff in the info column */
-	if (check_col(pinfo->cinfo,COL_INFO)) {
-		col_clear(pinfo->cinfo,COL_INFO);
-	}
+    static const value_string packet_type_names[] = {
+        {CCN_DTAG_Interest, "Interest"},
+        {CCN_DTAG_ContentObject, "Content"},
+        {CCN_DTAG_ContentObjectV20080711, "ContentV20080711"},
+        {0, ""}
+    };
+
+    guint tvb_size = 0;
+    proto_tree *ccn_tree;
+    proto_item *ti = NULL;
+    proto_tree *type_tree;
+    proto_item *type_item;
+    const unsigned char *data;
+    struct ccn_skeleton_decoder skel_decoder;
+    struct ccn_skeleton_decoder *sd;
+    struct ccn_buf_decoder decoder;
+    struct ccn_buf_decoder *d;
+    int packet_type = 0;
+    size_t s;
+    
+    /* if it passes through a skeleton decoder it's one of ours for sure */
+    memset(&skel_decoder, 0, sizeof(skel_decoder));
+    sd = &skel_decoder;
+    tvb_size = tvb_length(tvb);
+    data = ep_tvb_memdup(tvb, 0, tvb_size);
+    s = ccn_skeleton_decode(sd, data, tvb_size);
+    if (sd->state < 0)
+        return (0);
+
+    /* Make it visible that we're taking this packet */
+    if (check_col(pinfo->cinfo, COL_PROTOCOL)) {
+        col_set_str(pinfo->cinfo, COL_PROTOCOL, "CCN");
+    }
+
+    /* Clear out stuff in the info column */
+    if (check_col(pinfo->cinfo, COL_INFO)) {
+        col_clear(pinfo->cinfo, COL_INFO);
+    }
+
+    /* decide whether it is an Interest or ContentObject */
+    memset(&decoder, 0, sizeof(decoder));
+    d = ccn_buf_decoder_start(&decoder, data, tvb_size);
+    if (d->decoder.state >= 0 && CCN_GET_TT_FROM_DSTATE(d->decoder.state) == CCN_DTAG) {
+        packet_type = d->decoder.numval;
+    }
+
+    
+    /* Add the packet type and CCN URI to the info column */
+    if (check_col(pinfo->cinfo, COL_INFO)) {
+        struct ccn_charbuf *c;
+        col_add_str(pinfo->cinfo, COL_INFO,
+                    val_to_str(packet_type, packet_type_names, "Unknown (0x%02x"));
+        c = ccn_charbuf_create();
+        ccn_uri_append(c, data, tvb_size, 1);
+        col_append_str(pinfo->cinfo, COL_INFO, " ");
+        col_append_str(pinfo->cinfo, COL_INFO, ccn_charbuf_as_string(c));
+        ccn_charbuf_destroy(&c);
+    }
+    
+    if (tree == NULL)
+        return (s);
+
+    
+    ti = proto_tree_add_item(tree, proto_ccn, tvb, 0, -1, FALSE);
+    ccn_tree = proto_item_add_subtree(ti, ett_ccn);
+    /* proto_tree_add_item(ccn_tree, hf_ccn_xxx, tvb, offset, len, FALSE); */
+        
+    type_item = proto_tree_add_text(ccn_tree, tvb, 0, 3,
+                                    val_to_str(packet_type, packet_type_names, "Unknown (0x%02x"));
+    type_tree = proto_item_add_subtree(type_item, ett_type);
+    return (s);
 }
 
 static gboolean
 dissect_ccn_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
-    int is_ccn = 0;  /* eventually, have to guess... */
+    
+    /* This is a heuristic dissector, which means we get all the UDP
+     * traffic not sent to a known dissector and not claimed by
+     * a heuristic dissector called before us!
+     */
 
-    if (!global_ccn_heur)
-        return FALSE;
-
-	/* This is a heuristic dissector, which means we get all the UDP
-	 * traffic not sent to a known dissector and not claimed by
-	 * a heuristic dissector called before us!
-	 */
-    if (is_ccn) {
-        dissect_ccn(tvb, pinfo, tree);
-        return TRUE;
-    }
-
-    return FALSE;
+    if (dissect_ccn(tvb, pinfo, tree) > 0)
+        return (TRUE);
+    else
+        return (FALSE);
 }
-
-   
