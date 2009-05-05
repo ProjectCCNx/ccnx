68a032149f323e89613cfd6e79998eaa65da0083
Nathan Good <ngood@parc.com>
2009-05-05 11:02:33 -0700

    ACL Manager GUI with fake data and transfer between groups working- apply/revert/cancel buttons need to not completed refs #168

--- apps/containerApp/src/com/parc/ccn/apps/containerApp/ACLManager.java
+++ apps/containerApp/src/com/parc/ccn/apps/containerApp/ACLManager.java
@@ -7,14 +7,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.BevelBorder;
+
+import com.parc.ccn.data.ContentName;
 
 public class ACLManager extends JDialog implements ActionListener{
 
@@ -22,30 +28,61 @@ public class ACLManager extends JDialog implements ActionListener{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JList list_2;
-	private JList list_1;
-	private JList list;
+	private JList userList;
+	private JList readWriteList;
+	private JList readOnlyList;
 
+	private JButton buttonApply;
+	private JButton buttonDefault;
+	private JButton buttonCancel;
+	private JButton buttonAssignReadOnly;
+	private JButton buttonRemoveReadOnly;
+	private JButton buttonAssingReadWrite;
+	private JButton buttonRemoveReadWrite;
+	
+	
 	private JFrame frame;
 	private String path;
-	/**
+	
+	//private ArrayList<String> readOnlyPrincipals;
+	//private ArrayList<String> readWritePrincipals;
+
+private SortedListModel readOnlyPrincipals = null;
+private SortedListModel readWritePrincipals = null;
+private SortedListModel userPool = null;
+
+/**
 	 * Launch the application
+ * @param path2 
 	 * @param args
 	 */
 	
 	//Get the list of users
-	public void getAvailUserList()
+	public void getAvailUserList(String path2)
 	{
-		
+	// grab the list of available users from somewhere
+		String principals[] = {"Paulo","Noel","Eliza","Chico","Alex","Aaron","Kelly"};
+		userPool.addAll(principals);		
 	}
 
-	public void getReadOnlyUsersList()
+	public void getReadOnlyUsersList(String path2)
 	{
+		// grab the content object
+		// get the list of users that are read only
+		// populate the list with the array of users
+		String principals[] = {"Mary","Cathy","Janet","Tracy","Natasha","Natalie","Tanya"};
+		readOnlyPrincipals.addAll(principals);
 		
 	}
 	
-	public void getReadWriteUsersList()
+	public void getReadWriteUsersList(String path2)
 	{
+		// grab the content object
+		// get the list of users that are read only
+		// populate the list with the array of users
+		
+		String principals[] = {"Jim","Fred","Bob","Frank","Matsumoto","Jorge","Frederick","Jane"};
+		readWritePrincipals.addAll(principals);
 		
 	}
 
@@ -53,6 +90,8 @@ public class ACLManager extends JDialog implements ActionListener{
 	//Applies new permissions to User Items
 	public void setReadOnlyUsersList()
 	{
+		// grab the content object
+		// apply the changes to the object
 		
 	}
 	
@@ -60,7 +99,8 @@ public class ACLManager extends JDialog implements ActionListener{
 	//Applies new permissions to User Items
 	public void setReadWriteUsersList()
 	{
-		
+		// grab the content object
+		// apply the changes to the object		
 	}
 	
 	/**
@@ -69,65 +109,90 @@ public class ACLManager extends JDialog implements ActionListener{
 	 * @param path 
 	 */
 	public ACLManager(String path) {
+
 		super();
-		setBounds(100, 100, 679, 452);
+		
+		readOnlyPrincipals = new SortedListModel();
+		readWritePrincipals = new SortedListModel();
+		userPool = new SortedListModel();
+		
+		getAvailUserList(path);
+		getReadOnlyUsersList(path);
+		getReadWriteUsersList(path);
+		
+		setBounds(100, 100, 615, 442);
 		setTitle("Manage Access Controls for "+path);
 		getContentPane().setLayout(null);
 				
-		final JButton button = new JButton();
-		button.setBounds(73, 367, 141, 25);
-		button.setText("Apply Changes");
-		getContentPane().add(button);
+		buttonApply = new JButton();
+		buttonApply.addActionListener(this);
+		buttonApply.setBounds(73, 367, 141, 25);
+		buttonApply.setText("Apply Changes");
+		getContentPane().add(buttonApply);
 
-		final JButton button_1 = new JButton();
-		button_1.setBounds(261, 367, 123, 25);
-		button_1.setText("Revert Default");
-		getContentPane().add(button_1);
+		buttonDefault = new JButton();
+		buttonDefault.addActionListener(this);
+		buttonDefault.setBounds(261, 367, 123, 25);
+		buttonDefault.setText("Revert Default");
+		getContentPane().add(buttonDefault);
 
-		final JButton button_2 = new JButton();
-		button_2.setBounds(421, 367, 133, 25);
-		button_2.setText("Cancel Changes");
-		getContentPane().add(button_2);
+		buttonCancel = new JButton();
+		buttonCancel.addActionListener(this);
+		buttonCancel.setBounds(421, 367, 133, 25);
+		buttonCancel.setText("Cancel Changes");
+		getContentPane().add(buttonCancel);
 
-		list = new JList();
-		list.setBorder(new BevelBorder(BevelBorder.LOWERED));
-		list.setBounds(10, 70, 120, 240);
-		getContentPane().add(list);
+		final JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(462, 70, 120, 245);
+		getContentPane().add(scrollPane_1);
+		
+		readWriteList = new JList(readWritePrincipals);
+		scrollPane_1.setViewportView(readWriteList);
+		readWriteList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		readWriteList.setBorder(new BevelBorder(BevelBorder.LOWERED));
+		//readWriteList.setBounds(462, 70, 120, 240);		
+		//getContentPane().add(readWriteList);
 
-		list_1 = new JList();
-		list_1.setBorder(new BevelBorder(BevelBorder.LOWERED));
-		list_1.setBounds(462, 70, 120, 240);
-		getContentPane().add(list_1);
+		
+		final JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(233, 70, 120, 245);
+		getContentPane().add(scrollPane_2);
+		
+		userList = new JList(userPool);
+		scrollPane_2.setViewportView(userList);
+		userList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		userList.setBorder(new BevelBorder(BevelBorder.LOWERED));
+//		userList.setBounds(233, 70, 120, 240);
+//		getContentPane().add(userList);
 
-		list_2 = new JList();
-		list_2.setBorder(new BevelBorder(BevelBorder.LOWERED));
-		list_2.setBounds(233, 70, 120, 240);
-		getContentPane().add(list_2);
+		buttonAssignReadOnly = new JButton();
+		buttonAssignReadOnly.addActionListener(this);
+		buttonAssignReadOnly.setMargin(new Insets(2, 2, 2, 2));
+		buttonAssignReadOnly.setIconTextGap(2);
+		buttonAssignReadOnly.setBounds(136, 66, 84, 25);
+		buttonAssignReadOnly.setText("<- assign");
+		getContentPane().add(buttonAssignReadOnly);
 
-		final JButton button_3 = new JButton();
-		button_3.setMargin(new Insets(2, 2, 2, 2));
-		button_3.setIconTextGap(2);
-		button_3.setBounds(136, 66, 84, 25);
-		button_3.setText("<- assign");
-		getContentPane().add(button_3);
+		buttonRemoveReadOnly = new JButton();
+		buttonRemoveReadOnly.addActionListener(this);
+		buttonRemoveReadOnly.setMargin(new Insets(2, 2, 2, 2));
+		buttonRemoveReadOnly.setBounds(135, 290, 79, 25);
+		buttonRemoveReadOnly.setText("remove ->");
+		getContentPane().add(buttonRemoveReadOnly);
 
-		final JButton button_4 = new JButton();
-		button_4.setMargin(new Insets(2, 2, 2, 2));
-		button_4.setBounds(135, 290, 79, 25);
-		button_4.setText("remove ->");
-		getContentPane().add(button_4);
+		buttonAssingReadWrite = new JButton();
+		buttonAssingReadWrite.addActionListener(this);
+		buttonAssingReadWrite.setMargin(new Insets(2, 2, 2, 2));
+		buttonAssingReadWrite.setBounds(370, 65, 79, 25);
+		buttonAssingReadWrite.setText("assign ->");
+		getContentPane().add(buttonAssingReadWrite);
 
-		final JButton button_5 = new JButton();
-		button_5.setMargin(new Insets(2, 2, 2, 2));
-		button_5.setBounds(370, 65, 79, 25);
-		button_5.setText("assign ->");
-		getContentPane().add(button_5);
-
-		final JButton button_6 = new JButton();
-		button_6.setMargin(new Insets(2, 2, 2, 2));
-		button_6.setBounds(370, 290, 79, 25);
-		button_6.setText("<- remove");
-		getContentPane().add(button_6);
+		buttonRemoveReadWrite = new JButton();
+		buttonRemoveReadWrite.addActionListener(this);
+		buttonRemoveReadWrite.setMargin(new Insets(2, 2, 2, 2));
+		buttonRemoveReadWrite.setBounds(370, 290, 79, 25);
+		buttonRemoveReadWrite.setText("<- remove");
+		getContentPane().add(buttonRemoveReadWrite);
 
 		final JLabel userAndGroupLabel = new JLabel();
 		userAndGroupLabel.setBounds(0, 0, 600, 15);
@@ -152,10 +217,74 @@ public class ACLManager extends JDialog implements ActionListener{
 		final JSeparator separator_3 = new JSeparator();
 		separator_3.setBounds(0, 20, 576, 20);
 		getContentPane().add(separator_3);
+
+		final JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(8, 71, 120, 245);
+		getContentPane().add(scrollPane);
+
+		readOnlyList = new JList(readOnlyPrincipals);
+		scrollPane.setViewportView(readOnlyList);
+		readOnlyList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		readOnlyList.setBorder(new BevelBorder(BevelBorder.LOWERED));
+				
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if(buttonApply == e.getSource()) {
+			
+		}else if(buttonDefault == e.getSource()){
+			
+		}else if(buttonCancel == e.getSource()){
+			
+		}else if(buttonAssignReadOnly == e.getSource()){
+			moveListItems(userList.getSelectedIndices(),userList,readOnlyList);
+			
+		}else if(buttonRemoveReadOnly == e.getSource()){			
+			moveListItems(readOnlyList.getSelectedIndices(),readOnlyList,userList);			
+			
+		}else if(buttonAssingReadWrite == e.getSource()){
+			moveListItems(userList.getSelectedIndices(),userList,readWriteList);
+			
+		}else if(buttonRemoveReadWrite == e.getSource()){
+			moveListItems(readWriteList.getSelectedIndices(),readWriteList,userList);
+			
+		}
+		
+	}
+	
+	private void moveListItems(int selectedIndices[],JList fromList,JList toList)
+	{
+		//ArrayList of selected items
+		ArrayList<Object> itemsSelected = new ArrayList<Object>();
+		
+		//Items selected that are to be removed from the toList
+		//ArrayList<Object> itemsToBeRemoved = new ArrayList<Object>();
+		
+		for(int i=0;i<selectedIndices.length;i++)
+		{
+			//remove item from fromList and move to toList
+			System.out.println("Index is "+ selectedIndices[i]);
+			Object selectedItem = fromList.getModel().getElementAt(selectedIndices[i]);
+			itemsSelected.add(selectedItem);			
+			
+		}
+		
+		//Bulk adding and removal of items
+		
+		for(Object item : itemsSelected)
+		{
+			((SortedListModel)toList.getModel()).add(item);
+			((SortedListModel)fromList.getModel()).removeElement(item);
+			
+		}
+//		private JList userList;
+//		private JList readWriteList;
+//		private JList readOnlyList;
+		
+//		private SortedListModel readOnlyPrincipals = null;
+//		private SortedListModel readWritePrincipals = null;
+//		private SortedListModel userPool = null;
 		
 	}
 
