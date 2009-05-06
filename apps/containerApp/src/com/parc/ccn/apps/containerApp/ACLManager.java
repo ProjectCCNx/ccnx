f21e453defcc40af0540fdd013f38a8c4e2f5e6a
Nathan Good <ngood@parc.com>
2009-05-05 18:21:13 -0700

    refs #168 Access Control UI with fake data setup with new UI.

--- apps/containerApp/src/com/parc/ccn/apps/containerApp/ACLManager.java
+++ apps/containerApp/src/com/parc/ccn/apps/containerApp/ACLManager.java
@@ -40,7 +40,8 @@ public class ACLManager extends JDialog implements ActionListener{
 	private JButton buttonAssingReadWrite;
 	private JButton buttonRemoveReadWrite;
 	
-	
+	private JButton buttonModify2View;
+	private JButton buttonView2Modify;	
 	private JFrame frame;
 	private String path;
 	
@@ -51,6 +52,11 @@ private SortedListModel readOnlyPrincipals = null;
 private SortedListModel readWritePrincipals = null;
 private SortedListModel userPool = null;
 
+//Default Items
+private SortedListModel readOnlyPrincipalsDefault = null;
+private SortedListModel readWritePrincipalsDefault = null;
+private SortedListModel userPoolDefault = null;
+
 /**
 	 * Launch the application
  * @param path2 
@@ -58,36 +64,43 @@ private SortedListModel userPool = null;
 	 */
 	
 	//Get the list of users
-	public void getAvailUserList(String path2)
+	public String[] getUserList(String path2,String permissions)
 	{
-	// grab the list of available users from somewhere
-		String principals[] = {"Paulo","Noel","Eliza","Chico","Alex","Aaron","Kelly"};
-		userPool.addAll(principals);		
+		if(permissions ==null){
+		
+			//get all users/groups
+			String principals[] = {"Paulo","Noel","Eliza","Chico","Alex","Aaron","Kelly"};
+			return principals;			
+		}
+		else {
+			//Use the path to look up the permissions
+			if(permissions.equalsIgnoreCase("read"))
+			{
+				String principals[] = {"Mary","Cathy","Janet","Tracy","Natasha","Natalie","Tanya"};
+				return principals;
+			}
+			else if (permissions.equalsIgnoreCase("write"))
+			{
+				String principals[] = {"Jim","Fred","Bob","Frank","Matsumoto","Jorge","Frederick","Jane"};
+				return principals;
+			}
+		
+		}
+		String empty[] ={""};
+		return empty;
 	}
 
-	public void getReadOnlyUsersList(String path2)
-	{
-		// grab the content object
-		// get the list of users that are read only
-		// populate the list with the array of users
-		String principals[] = {"Mary","Cathy","Janet","Tracy","Natasha","Natalie","Tanya"};
-		readOnlyPrincipals.addAll(principals);
-		
-	}
-	
-	public void getReadWriteUsersList(String path2)
-	{
-		// grab the content object
-		// get the list of users that are read only
-		// populate the list with the array of users
-		
-		String principals[] = {"Jim","Fred","Bob","Frank","Matsumoto","Jorge","Frederick","Jane"};
-		readWritePrincipals.addAll(principals);
-		
-	}
-
+//TODO Pending Changes Modal Dialog on close
+//TODO Add or remove between lists across the screen (from read only to read/write and not just users)
+//TODO have only one thing selected at a time
+	//TODO Buttons (Apply, Cancel, Default)
 	//On Close or Apply do this
 	//Applies new permissions to User Items
+	
+	public void setACLChanges()
+	{
+		
+	}
 	public void setReadOnlyUsersList()
 	{
 		// grab the content object
@@ -105,20 +118,30 @@ private SortedListModel userPool = null;
 	
 	/**
 	 * Create the dialog
-	 * @param frame 
 	 * @param path 
 	 */
 	public ACLManager(String path) {
 
 		super();
+		this.path = path;
 		
 		readOnlyPrincipals = new SortedListModel();
 		readWritePrincipals = new SortedListModel();
 		userPool = new SortedListModel();
 		
-		getAvailUserList(path);
-		getReadOnlyUsersList(path);
-		getReadWriteUsersList(path);
+		readOnlyPrincipalsDefault = new SortedListModel();
+		readWritePrincipalsDefault = new SortedListModel();
+		userPoolDefault = new SortedListModel();
+		
+		//List Models for the List Objects
+		userPool.addAll(getUserList(path,null));
+		readOnlyPrincipals.addAll(getUserList(path,"read"));
+		readWritePrincipals.addAll(getUserList(path,"write"));
+		
+		//Default List Items
+		userPoolDefault.addAll(getUserList(path,null));
+		readOnlyPrincipalsDefault.addAll(getUserList(path,"read"));
+		readWritePrincipalsDefault.addAll(getUserList(path,"write"));
 		
 		setBounds(100, 100, 615, 442);
 		setTitle("Manage Access Controls for "+path);
@@ -143,7 +166,7 @@ private SortedListModel userPool = null;
 		getContentPane().add(buttonCancel);
 
 		final JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(462, 70, 120, 245);
+		scrollPane_1.setBounds(304, 209, 278, 106);
 		getContentPane().add(scrollPane_1);
 		
 		readWriteList = new JList(readWritePrincipals);
@@ -155,7 +178,7 @@ private SortedListModel userPool = null;
 
 		
 		final JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBounds(233, 70, 120, 245);
+		scrollPane_2.setBounds(8, 70, 169, 255);
 		getContentPane().add(scrollPane_2);
 		
 		userList = new JList(userPool);
@@ -169,28 +192,28 @@ private SortedListModel userPool = null;
 		buttonAssignReadOnly.addActionListener(this);
 		buttonAssignReadOnly.setMargin(new Insets(2, 2, 2, 2));
 		buttonAssignReadOnly.setIconTextGap(2);
-		buttonAssignReadOnly.setBounds(136, 66, 84, 25);
-		buttonAssignReadOnly.setText("<- assign");
+		buttonAssignReadOnly.setBounds(194, 69, 79, 25);
+		buttonAssignReadOnly.setText("assign ->");
 		getContentPane().add(buttonAssignReadOnly);
 
 		buttonRemoveReadOnly = new JButton();
 		buttonRemoveReadOnly.addActionListener(this);
 		buttonRemoveReadOnly.setMargin(new Insets(2, 2, 2, 2));
-		buttonRemoveReadOnly.setBounds(135, 290, 79, 25);
-		buttonRemoveReadOnly.setText("remove ->");
+		buttonRemoveReadOnly.setBounds(194, 116, 79, 25);
+		buttonRemoveReadOnly.setText("<- remove");
 		getContentPane().add(buttonRemoveReadOnly);
 
 		buttonAssingReadWrite = new JButton();
 		buttonAssingReadWrite.addActionListener(this);
 		buttonAssingReadWrite.setMargin(new Insets(2, 2, 2, 2));
-		buttonAssingReadWrite.setBounds(370, 65, 79, 25);
+		buttonAssingReadWrite.setBounds(194, 225, 79, 25);
 		buttonAssingReadWrite.setText("assign ->");
 		getContentPane().add(buttonAssingReadWrite);
 
 		buttonRemoveReadWrite = new JButton();
 		buttonRemoveReadWrite.addActionListener(this);
 		buttonRemoveReadWrite.setMargin(new Insets(2, 2, 2, 2));
-		buttonRemoveReadWrite.setBounds(370, 290, 79, 25);
+		buttonRemoveReadWrite.setBounds(194, 291, 79, 25);
 		buttonRemoveReadWrite.setText("<- remove");
 		getContentPane().add(buttonRemoveReadWrite);
 
@@ -199,18 +222,13 @@ private SortedListModel userPool = null;
 		userAndGroupLabel.setText("User and Group Permissions for " + path);
 		getContentPane().add(userAndGroupLabel);
 
-		final JLabel viewPermissionsLabel = new JLabel();
-		viewPermissionsLabel.setBounds(8, 44, 122, 20);
-		viewPermissionsLabel.setText("View Permissions");
-		getContentPane().add(viewPermissionsLabel);
-
 		final JLabel usersLabel = new JLabel();
-		usersLabel.setBounds(267, 44, 69, 20);
-		usersLabel.setText("Users");
+		usersLabel.setBounds(8, 44, 123, 20);
+		usersLabel.setText("Users/Groups");
 		getContentPane().add(usersLabel);
 
 		final JLabel modifyPermissionsLabel = new JLabel();
-		modifyPermissionsLabel.setBounds(462, 44, 132, 20);
+		modifyPermissionsLabel.setBounds(304, 321, 132, 20);
 		modifyPermissionsLabel.setText("Modify Permissions");
 		getContentPane().add(modifyPermissionsLabel);
 
@@ -218,14 +236,31 @@ private SortedListModel userPool = null;
 		separator_3.setBounds(0, 20, 576, 20);
 		getContentPane().add(separator_3);
 
+		final JLabel viewPermissionsLabel = new JLabel();
+		viewPermissionsLabel.setBounds(304, 44, 122, 20);
+		viewPermissionsLabel.setText("View Permissions");
+		getContentPane().add(viewPermissionsLabel);
+		
 		final JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(8, 71, 120, 245);
+		scrollPane.setBounds(304, 70, 278, 106);
 		getContentPane().add(scrollPane);
 
 		readOnlyList = new JList(readOnlyPrincipals);
 		scrollPane.setViewportView(readOnlyList);
 		readOnlyList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		readOnlyList.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+		buttonModify2View = new JButton();
+		buttonModify2View.addActionListener(this);
+		buttonModify2View.setText("^");
+		buttonModify2View.setBounds(349, 178, 55, 25);
+		getContentPane().add(buttonModify2View);
+
+		buttonView2Modify = new JButton();
+		buttonView2Modify.addActionListener(this);
+		buttonView2Modify.setText("v");
+		buttonView2Modify.setBounds(479, 178, 49, 25);
+		getContentPane().add(buttonView2Modify);
 				
 	}
 
@@ -233,9 +268,15 @@ private SortedListModel userPool = null;
 		// TODO Auto-generated method stub
 		if(buttonApply == e.getSource()) {
 			
+			applyChanges();
 		}else if(buttonDefault == e.getSource()){
 			
+			restoreDefaults();
+			
+			
 		}else if(buttonCancel == e.getSource()){
+			
+			cancelChanges();
 			
 		}else if(buttonAssignReadOnly == e.getSource()){
 			moveListItems(userList.getSelectedIndices(),userList,readOnlyList);
@@ -249,10 +290,44 @@ private SortedListModel userPool = null;
 		}else if(buttonRemoveReadWrite == e.getSource()){
 			moveListItems(readWriteList.getSelectedIndices(),readWriteList,userList);
 			
+		}else if(buttonModify2View == e.getSource()){
+			
+			moveListItems(readWriteList.getSelectedIndices(),readWriteList,readOnlyList);
+						
+		}else if(buttonView2Modify == e.getSource()){
+			
+			moveListItems(readOnlyList.getSelectedIndices(),readOnlyList,readWriteList);
+			
 		}
 		
 	}
 	
+	private void applyChanges() {
+		// TODO Take all the elements and assign them new acls somehow
+		
+		
+	}
+
+	private void restoreDefaults() {
+
+		//Clear out the old model
+		userPool.clear();
+		readOnlyPrincipals.clear();
+		readWritePrincipals.clear();
+		
+		//In with the new
+		userPool.addAll(userPoolDefault.getAllElements());
+		readOnlyPrincipals.addAll(readOnlyPrincipalsDefault.getAllElements());
+		readWritePrincipals.addAll(readWritePrincipalsDefault.getAllElements());
+		
+	}
+	
+	private void cancelChanges()
+	{
+		this.setVisible(false);
+		this.dispose();
+	}
+
 	private void moveListItems(int selectedIndices[],JList fromList,JList toList)
 	{
 		//ArrayList of selected items
@@ -278,13 +353,6 @@ private SortedListModel userPool = null;
 			((SortedListModel)fromList.getModel()).removeElement(item);
 			
 		}
-//		private JList userList;
-//		private JList readWriteList;
-//		private JList readOnlyList;
-		
-//		private SortedListModel readOnlyPrincipals = null;
-//		private SortedListModel readWritePrincipals = null;
-//		private SortedListModel userPool = null;
 		
 	}
 
