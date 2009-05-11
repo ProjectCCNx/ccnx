a45bc01dcbdd75ed77dee6e6d55ddec09ed7d733
Diana Smetters <smetters@parc.com>
2009-05-11 00:06:07 -0700

    Turn internal node key storage into a Key. refs #170

--- Java_CCN/com/parc/ccn/security/access/NodeKey.java
+++ Java_CCN/com/parc/ccn/security/access/NodeKey.java
@@ -1,8 +1,10 @@
 package com.parc.ccn.security.access;
 
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.sql.Timestamp;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.stream.XMLStreamException;
 
 import com.parc.ccn.data.ContentName;
@@ -13,6 +15,17 @@ import com.parc.ccn.security.crypto.CCNDigestHelper;
 import com.parc.ccn.security.crypto.KeyDerivationFunction;
 
 public class NodeKey {
+	
+	/**
+	 * Default data key length in bytes. No real reason this can't be bumped up to 32. It
+	 * acts as the seed for a KDF, not an encryption key.
+	 */
+	public static final int DEFAULT_NODE_KEY_LENGTH = 16; 
+	/**
+	 * The keys we're wrapping are really seeds for a KDF, not keys in their own right.
+	 * Eventually we'll use CMAC, so call them AES...
+	 */
+	public static final String DEFAULT_NODE_KEY_ALGORITHM = "AES";
 	
 	/**
 	 * The node this key is associated with, with _access_ information stripped.
@@ -27,7 +40,7 @@ public class NodeKey {
 	/**
 	 * The unwrapped node key
 	 */
-	private byte [] _nodeKey;
+	private Key _nodeKey;
 	
 	public NodeKey(ContentName nodeKeyName, byte [] unwrappedNodeKey) {
 		if ((null == nodeKeyName) || (null == unwrappedNodeKey)) {
@@ -35,7 +48,7 @@ public class NodeKey {
 		}
 		_storedNodeKeyName = nodeKeyName;
 		_storedNodeKeyID = generateKeyID(unwrappedNodeKey);
-		_nodeKey = unwrappedNodeKey;
+		_nodeKey = new SecretKeySpec(unwrappedNodeKey, DEFAULT_NODE_KEY_ALGORITHM);
 		_nodeName = AccessControlProfile.accessRoot(nodeKeyName);
 		if ((null == _nodeName) || (!AccessControlProfile.isNodeKeyName(nodeKeyName))) {
 			throw new IllegalArgumentException("NodeKey: key name " + nodeKeyName + " is not a valid node key name.");			
@@ -47,21 +60,21 @@ public class NodeKey {
 		_storedNodeKeyName = ancestorNodeKeyName;
 		_storedNodeKeyID = ancestorNodeKeyID;
 		_nodeName = nodeName;
-		_nodeKey = derivedNodeKey;
+		_nodeKey = new SecretKeySpec(derivedNodeKey, DEFAULT_NODE_KEY_ALGORITHM);
 	}
 	
 	public NodeKey computeDescendantNodeKey(ContentName descendantNodeName, String keyLabel) throws InvalidKeyException, XMLStreamException {
 		if (!nodeName().isPrefixOf(descendantNodeName)) {
 			throw new IllegalArgumentException("Node " + descendantNodeName + " is not a child of this node " + nodeName());
 		}
-		byte [] derivedKey = KeyDerivationFunction.DeriveKeyForNode(nodeName(), nodeKey(), keyLabel, descendantNodeName);
+		byte [] derivedKey = KeyDerivationFunction.DeriveKeyForNode(nodeName(), nodeKey().getEncoded(), keyLabel, descendantNodeName);
 		return new NodeKey(descendantNodeName, derivedKey, storedNodeKeyName(), storedNodeKeyID());
 	}
 	
 	public ContentName nodeName() { return _nodeName; }
 	public ContentName storedNodeKeyName() { return _storedNodeKeyName; }
 	public byte [] storedNodeKeyID() { return _storedNodeKeyID; }
-	public byte [] nodeKey() { return _nodeKey; }
+	public Key nodeKey() { return _nodeKey; }
 	
 	public boolean isDerivedNodeKey() {
 		return (!nodeName().isPrefixOf(storedNodeKeyName()));
