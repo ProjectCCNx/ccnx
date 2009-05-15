8fedf3ac3375a21ce3012e05943304b16219592e
Simon Barber <sbarber@parc.com>
2009-05-15 11:56:27 -0700

    Add encryption tests for CCNFileInputStream. refs #236,#237

--- Java_CCN/test/ccn/library/io/CCNSecureInputStreamTest.java
+++ Java_CCN/test/ccn/library/io/CCNSecureInputStreamTest.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.DigestInputStream;
 import java.security.DigestOutputStream;
 import java.security.InvalidAlgorithmParameterException;
@@ -31,6 +32,8 @@ import com.parc.ccn.data.security.PublisherPublicKeyDigest;
 import com.parc.ccn.data.security.SignedInfo;
 import com.parc.ccn.data.security.SignedInfo.ContentType;
 import com.parc.ccn.library.CCNLibrary;
+import com.parc.ccn.library.io.CCNFileInputStream;
+import com.parc.ccn.library.io.CCNFileOutputStream;
 import com.parc.ccn.library.io.CCNInputStream;
 import com.parc.ccn.library.io.CCNOutputStream;
 import com.parc.ccn.library.profiles.SegmentationProfile;
@@ -40,107 +43,194 @@ import com.parc.ccn.security.crypto.UnbufferedCipherInputStream;
 
 public class CCNSecureInputStreamTest {
 	
+	static protected abstract class StreamFactory {
+		int encrLength = 25*1024+301;
+		byte [] digest;
+		byte [] encrData;
+		ContentName name;
+		ContentKeys keys;
+		public StreamFactory(ContentName n) throws NoSuchAlgorithmException, XMLStreamException, IOException, InterruptedException {
+			name = VersioningProfile.versionName(n);
+			keys = ContentKeys.generateRandomKeys();
+			digest = writeFileFloss(encrLength);
+		}
+		public abstract InputStream makeInputStream() throws IOException, XMLStreamException;
+		public abstract OutputStream makeOutputStream() throws IOException, XMLStreamException;
+
+		/**
+		 * Trick to get around lack of repo. We want the test below to read data out of
+		 * ccnd. Problem is to do that, we have to get it into ccnd. This pre-loads
+		 * ccnd with data by "flossing" it -- starting up a reader thread that will
+		 * pull our generated data into ccnd for us, where it will wait till we read
+		 * it back out.
+		 */
+		public byte [] writeFileFloss(int fileLength) throws XMLStreamException, IOException, NoSuchAlgorithmException, InterruptedException {
+			Random randBytes = new Random(); // doesn't need to be secure
+			OutputStream stockOutputStream = makeOutputStream();
+
+			DigestOutputStream digestStreamWrapper = new DigestOutputStream(stockOutputStream, MessageDigest.getInstance("SHA1"));
+			ByteArrayOutputStream data = new ByteArrayOutputStream();
+
+			byte [] bytes = new byte[BUF_SIZE];
+			int elapsed = 0;
+			int nextBufSize = 0;
+			boolean firstBuf = true;
+			final double probFlush = .1;
+
+			while (elapsed < fileLength) {
+				nextBufSize = ((fileLength - elapsed) > BUF_SIZE) ? BUF_SIZE : (fileLength - elapsed);
+				randBytes.nextBytes(bytes);
+				digestStreamWrapper.write(bytes, 0, nextBufSize);
+				data.write(bytes, 0, nextBufSize);
+				elapsed += nextBufSize;
+				if (firstBuf) {
+					startReader(fileLength);
+					firstBuf = false;
+				}
+				if (randBytes.nextDouble() < probFlush) {
+					System.out.println("Flushing buffers.");
+					digestStreamWrapper.flush();
+				}
+			}
+			digestStreamWrapper.close();
+			encrData = data.toByteArray();
+			return digestStreamWrapper.getMessageDigest().digest();
+		}
+
+		public Thread startReader(final int fileLength) {
+			Thread t = new Thread(){
+				public void run() {
+					try {
+						InputStream inputStream = makeInputStream();
+						readFile(inputStream, fileLength);
+					} catch (Exception e) {
+						e.printStackTrace();
+						Assert.fail("Class setup failed! " + e.getClass().getName() + ": " + e.getMessage());
+		           }
+		        }
+		    };
+		    t.start();
+		    return t;
+		}
+
+		public void streamEncryptDecrypt() throws XMLStreamException, IOException {
+			// check we get identical data back out
+			System.out.println("Reading CCNInputStream from "+name);
+			CCNInputStream vfirst = new CCNInputStream(name, null, null, keys, inputLibrary);
+			byte [] readDigest = readFile(vfirst, encrLength);
+			Assert.assertArrayEquals(digest, readDigest);
+
+			// check things fail if we use different keys
+			ContentKeys keys2 = ContentKeys.generateRandomKeys();
+			CCNInputStream v2 = new CCNInputStream(name, null, null, keys2, inputLibrary);
+			byte [] readDigest2 = readFile(v2, encrLength);
+			Assert.assertFalse(digest.equals(readDigest2));
+		}
+
+		public void seeking() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+			// check really small seeks/reads (smaller than 1 Cipher block)
+			doSeeking(10);
+
+			// check small seeks (but bigger than 1 Cipher block)
+			doSeeking(600);
+
+			// check large seeks (multiple ContentObjects)
+			doSeeking(4096*5+350);
+		}
+
+		private void doSeeking(int length) throws XMLStreamException, IOException, NoSuchAlgorithmException {
+			System.out.println("Reading CCNInputStream from "+name);
+			CCNInputStream i = new CCNInputStream(name, null, null, keys, inputLibrary);
+			// make sure we start mid ContentObject and past the first Cipher block
+			int start = ((int) (encrLength*0.3) % 4096) +600;
+			i.seek(start);
+			readAndCheck(i, start, length);
+			i.seek(start);
+			readAndCheck(i, start, length);
+		}
+
+		private void readAndCheck(CCNInputStream i, int start, int length)
+				throws IOException, XMLStreamException, NoSuchAlgorithmException {
+			byte [] origData = new byte[length];
+			System.arraycopy(encrData, start, origData, 0, length);
+			byte [] readData = new byte[length];
+			i.read(readData);
+			Assert.assertArrayEquals(origData, readData);
+		}
+
+		public void skipping() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+			// read some data, skip some data, read some more data
+			System.out.println("Reading CCNInputStream from "+name);
+			CCNInputStream inStream = new CCNInputStream(name, null, null, keys, inputLibrary);
+
+			int start = (int) (encrLength*0.3);
+
+			// check first part reads correctly
+			readAndCheck(inStream, 0, start);
+
+			// skip a short bit (less than 1 cipher block)
+			inStream.skip(10);
+			start += 10;
+
+			// check second part reads correctly
+			readAndCheck(inStream, start, 100);
+			start += 100;
+
+			// skip a medium bit (more than than 1 cipher block)
+			inStream.skip(600);
+			start += 600;
+
+			// check third part reads correctly
+			readAndCheck(inStream, start, 600);
+			start += 600;
+
+			// skip a bug bit (more than than 1 Content object)
+			inStream.skip(600+4096*2);
+			start += 600+4096*2;
+
+			// check fourth part reads correctly
+			readAndCheck(inStream, start, 600);
+		}
+	}
+
 	static ContentName defaultStreamName;
-	static ContentName encrName;
-	static int encrLength;
-	static ContentKeys encrKeys;
-	static byte [] encrDigest;
-	static byte [] encrData;
 	static CCNLibrary outputLibrary;
 	static CCNLibrary inputLibrary;
 	static final int BUF_SIZE = 4096;
-	
+
+	static StreamFactory basic;
+	static StreamFactory file;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		Library.logger().setLevel(Level.FINER);
+		Library.logger().setLevel(Level.FINEST);
 		SystemConfiguration.setDebugFlag(SystemConfiguration.DEBUGGING_FLAGS.DEBUG_SIGNATURES, true);
-		Random randBytes = new Random(); // doesn't need to be secure
 		outputLibrary = CCNLibrary.open();
 		inputLibrary = CCNLibrary.open();
 		
 		// Write a set of output
 		defaultStreamName = ContentName.fromNative("/test/stream/versioning/LongOutput.bin");
 		
-		encrName = VersioningProfile.versionName(defaultStreamName);
-		encrLength = 25*1024+301;
-		encrKeys = ContentKeys.generateRandomKeys();
-		encrDigest = writeFileFloss(encrName, encrLength, randBytes, encrKeys);
-	}
-	
-	/**
-	 * Trick to get around lack of repo. We want the test below to read data out of
-	 * ccnd. Problem is to do that, we have to get it into ccnd. This pre-loads
-	 * ccnd with data by "flossing" it -- starting up a reader thread that will
-	 * pull our generated data into ccnd for us, where it will wait till we read
-	 * it back out.
-	 * @param completeName
-	 * @param fileLength
-	 * @param randBytes
-	 * @return
-	 * @throws XMLStreamException
-	 * @throws IOException
-	 * @throws NoSuchAlgorithmException
-	 * @throws InterruptedException 
-	 */
-	public static byte [] writeFileFloss(ContentName completeName, int fileLength, Random randBytes, ContentKeys keys) throws XMLStreamException, IOException, NoSuchAlgorithmException, InterruptedException {
-		CCNOutputStream stockOutputStream = new CCNOutputStream(completeName, null, null, null, keys, outputLibrary);
-		
-		DigestOutputStream digestStreamWrapper = new DigestOutputStream(stockOutputStream, MessageDigest.getInstance("SHA1"));
-		ByteArrayOutputStream data = new ByteArrayOutputStream();
-		
-		byte [] bytes = new byte[BUF_SIZE];
-		int elapsed = 0;
-		int nextBufSize = 0;
-		boolean firstBuf = true;
-		System.out.println("Writing file: " + completeName + " bytes: " + fileLength);
-		final double probFlush = .1;
-		
-		while (elapsed < fileLength) {
-			nextBufSize = ((fileLength - elapsed) > BUF_SIZE) ? BUF_SIZE : (fileLength - elapsed);
-			randBytes.nextBytes(bytes);
-			digestStreamWrapper.write(bytes, 0, nextBufSize);
-			data.write(bytes, 0, nextBufSize);
-			elapsed += nextBufSize;
-			if (firstBuf) {
-				startReader(completeName, fileLength, keys);
-				firstBuf = false;
+		basic = new StreamFactory(defaultStreamName){
+			public InputStream makeInputStream() throws IOException, XMLStreamException {
+				return new CCNInputStream(name, null, null, keys, inputLibrary);
 			}
-			System.out.println(completeName + " wrote " + elapsed + " out of " + fileLength + " bytes.");
-			if (randBytes.nextDouble() < probFlush) {
-				System.out.println("Flushing buffers.");
-				digestStreamWrapper.flush();
+			public OutputStream makeOutputStream() throws IOException, XMLStreamException {
+				return new CCNOutputStream(name, null, null, null, keys, outputLibrary);
 			}
-		}
-		digestStreamWrapper.close();
-		System.out.println("Finished writing file " + completeName);
-		encrData = data.toByteArray();
-		return digestStreamWrapper.getMessageDigest().digest();
-	}
-	
-	public static Thread startReader(final ContentName completeName, final int fileLength, final ContentKeys keys) {
-		Thread t = new Thread(){
-	        public void run() {
-	           try {
-				readFile(completeName, fileLength, keys);
-	           } catch (Exception e) {
-	        	   e.printStackTrace();
-	        	   Assert.fail("Class setup failed! " + e.getClass().getName() + ": " + e.getMessage());
-	           }
-	        }
-	    };
-	    t.start();
-	    return t;
-	}
-	
-	public static byte [] readFile(ContentName completeName, int fileLength, ContentKeys keys) throws XMLStreamException, IOException {
-		CCNInputStream inputStream = new CCNInputStream(completeName, null, null, keys, null);
-		System.out.println("Reading file : " + completeName);
-		return readFile(inputStream, fileLength);
+		};
+
+		file = new StreamFactory(defaultStreamName){
+			public InputStream makeInputStream() throws IOException, XMLStreamException {
+				return new CCNFileInputStream(name, 0L, null, keys, inputLibrary);
+			}
+			public OutputStream makeOutputStream() throws IOException, XMLStreamException {
+				return new CCNFileOutputStream(name, null, keys, outputLibrary);
+			}
+		};
 	}
 	
 	public static byte [] readFile(InputStream inputStream, int fileLength) throws IOException, XMLStreamException {
-		
 		System.out.println("Entering READFILE");
 		DigestInputStream dis = null;
 		try {
@@ -178,12 +268,12 @@ public class CCNSecureInputStreamTest {
 	 */
 	@Test
 	public void cipherEncryptDecrypt() throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-		Cipher c = encrKeys.getSegmentEncryptionCipher(0);
-		byte [] d = c.doFinal(encrData);
-		c = encrKeys.getSegmentDecryptionCipher(0);
+		Cipher c = basic.keys.getSegmentEncryptionCipher(0);
+		byte [] d = c.doFinal(basic.encrData);
+		c = basic.keys.getSegmentDecryptionCipher(0);
 		d = c.doFinal(d);
 		// check we get identical data back out
-		Assert.assertArrayEquals(encrData, d);
+		Assert.assertArrayEquals(basic.encrData, d);
 	}
 
 	/**
@@ -192,22 +282,22 @@ public class CCNSecureInputStreamTest {
 	 */
 	@Test
 	public void cipherStreamEncryptDecrypt() throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException {
-		Cipher c = encrKeys.getSegmentEncryptionCipher(0);
-		InputStream is = new ByteArrayInputStream(encrData, 0, encrData.length);
+		Cipher c = basic.keys.getSegmentEncryptionCipher(0);
+		InputStream is = new ByteArrayInputStream(basic.encrData, 0, basic.encrData.length);
 		is = new UnbufferedCipherInputStream(is, c);
 		byte [] cipherText = new byte[4096];
 		for(int total = 0, res = 0; res >= 0 && total < 4096; total+=res)
 			res = is.read(cipherText,total,4096-total);
 
-		c = encrKeys.getSegmentDecryptionCipher(0);
+		c = basic.keys.getSegmentDecryptionCipher(0);
 		is = new ByteArrayInputStream(cipherText);
 		is = new UnbufferedCipherInputStream(is, c);
 		byte [] output = new byte[4096];
 		for(int total = 0, res = 0; res >= 0 && total < 4096; total+=res)
 			res = is.read(output,total,4096-total);
 		// check we get identical data back out
-		byte [] input = new byte[Math.min(4096, encrLength)];
-		System.arraycopy(encrData, 0, input, 0, input.length);
+		byte [] input = new byte[Math.min(4096, basic.encrLength)];
+		System.arraycopy(basic.encrData, 0, input, 0, input.length);
 		Assert.assertArrayEquals(input, output);
 	}
 
@@ -218,10 +308,10 @@ public class CCNSecureInputStreamTest {
 	@Test
 	public void contentEncryptDecrypt() throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException {
 		// create an encrypted content block
-		Cipher c = encrKeys.getSegmentEncryptionCipher(0);
-		InputStream is = new ByteArrayInputStream(encrData, 0, encrData.length);
+		Cipher c = basic.keys.getSegmentEncryptionCipher(0);
+		InputStream is = new ByteArrayInputStream(basic.encrData, 0, basic.encrData.length);
 		is = new UnbufferedCipherInputStream(is, c);
-		ContentName rootName = SegmentationProfile.segmentRoot(encrName);
+		ContentName rootName = SegmentationProfile.segmentRoot(basic.name);
 		PublisherPublicKeyDigest publisher = outputLibrary.getDefaultPublisher();
 		PrivateKey signingKey = outputLibrary.keyManager().getSigningKey(publisher);
 		byte [] finalBlockID = SegmentationProfile.getSegmentID(1);
@@ -230,33 +320,27 @@ public class CCNSecureInputStreamTest {
 				is, 4096);
 
 		// attempt to decrypt the data
-		c = encrKeys.getSegmentDecryptionCipher(0);
+		c = basic.keys.getSegmentDecryptionCipher(0);
 		is = new UnbufferedCipherInputStream(new ByteArrayInputStream(co.content()), c);
 		byte [] output = new byte[co.contentLength()];
 		for(int total = 0, res = 0; res >= 0 && total < output.length; total+=res)
 			res = is.read(output, total, output.length-total);
 		// check we get identical data back out
 		byte [] input = new byte[Math.min(4096, co.contentLength())];
-		System.arraycopy(encrData, 0, input, 0, input.length);
+		System.arraycopy(basic.encrData, 0, input, 0, input.length);
 		Assert.assertArrayEquals(input, output);
 	}
 
 	/**
-	 * Test basic stream encryption & decryption work, and that using different keys for decryption fails
+	 * Test stream encryption & decryption work, and that using different keys for decryption fails
 	 */
 	@Test
-	public void streamEncryptDecrypt() throws XMLStreamException, IOException {
-		// check we get identical data back out
-		System.out.println("Reading CCNInputStream from "+encrName);
-		CCNInputStream vfirst = new CCNInputStream(encrName, null, null, encrKeys, inputLibrary);
-		byte [] readDigest = readFile(vfirst, encrLength);
-		Assert.assertArrayEquals(encrDigest, readDigest);
-
-		// check things fail if we use different keys
-		ContentKeys keys2 = ContentKeys.generateRandomKeys();
-		CCNInputStream v2 = new CCNInputStream(encrName, null, null, keys2, inputLibrary);
-		byte [] readDigest2 = readFile(v2, encrLength);
-		Assert.assertFalse(encrDigest.equals(readDigest2));
+	public void basicStreamEncryptDecrypt() throws XMLStreamException, IOException {
+		basic.streamEncryptDecrypt();
+	}
+	@Test
+	public void fileStreamEncryptDecrypt() throws XMLStreamException, IOException {
+		file.streamEncryptDecrypt();
 	}
 
 	/**
@@ -264,35 +348,12 @@ public class CCNSecureInputStreamTest {
 	 * do it for different size parts of the data
 	 */
 	@Test
-	public void seeking() throws XMLStreamException, IOException, NoSuchAlgorithmException {
-		// check really small seeks/reads (smaller than 1 Cipher block)
-		doSeeking(10);
-
-		// check small seeks (but bigger than 1 Cipher block)
-		doSeeking(600);
-
-		// check large seeks (multiple ContentObjects)
-		doSeeking(4096*5+350);
+	public void basicSeeking() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+		basic.seeking();
 	}
-
-	private void doSeeking(int length) throws XMLStreamException, IOException, NoSuchAlgorithmException {
-		System.out.println("Reading CCNInputStream from "+encrName);
-		CCNInputStream i = new CCNInputStream(encrName, null, null, encrKeys, inputLibrary);
-		// make sure we start mid ContentObject and past the first Cipher block
-		int start = ((int) (encrLength*0.3) % 4096) +600;
-		i.seek(start);
-		readAndCheck(i, start, length);
-		i.seek(start);
-		readAndCheck(i, start, length);
-	}
-
-	private void readAndCheck(CCNInputStream i, int start, int length)
-			throws IOException, XMLStreamException, NoSuchAlgorithmException {
-		byte [] origData = new byte[length];
-		System.arraycopy(encrData, start, origData, 0, length);
-		byte [] readData = new byte[length];
-		i.read(readData);
-		Assert.assertArrayEquals(origData, readData);
+	@Test
+	public void fileSeeking() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+		file.seeking();
 	}
 
 	/**
@@ -300,37 +361,11 @@ public class CCNSecureInputStreamTest {
 	 * Tries small/medium/large skips
 	 */
 	@Test
-	public void skipping() throws XMLStreamException, IOException, NoSuchAlgorithmException {
-		// read some data, skip some data, read some more data
-		System.out.println("Reading CCNInputStream from "+encrName);
-		CCNInputStream inStream = new CCNInputStream(encrName, null, null, encrKeys, inputLibrary);
-
-		int start = (int) (encrLength*0.3);
-
-		// check first part reads correctly
-		readAndCheck(inStream, 0, start);
-
-		// skip a short bit (less than 1 cipher block)
-		inStream.skip(10);
-		start += 10;
-
-		// check second part reads correctly
-		readAndCheck(inStream, start, 100);
-		start += 100;
-
-		// skip a medium bit (more than than 1 cipher block)
-		inStream.skip(600);
-		start += 600;
-
-		// check third part reads correctly
-		readAndCheck(inStream, start, 600);
-		start += 600;
-
-		// skip a bug bit (more than than 1 Content object)
-		inStream.skip(600+4096*2);
-		start += 600+4096*2;
-
-		// check fourth part reads correctly
-		readAndCheck(inStream, start, 600);
+	public void basicSkipping() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+		basic.skipping();
+	}
+	@Test
+	public void fileSkipping() throws XMLStreamException, IOException, NoSuchAlgorithmException {
+		file.skipping();
 	}
 }
