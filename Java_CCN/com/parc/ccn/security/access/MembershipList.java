312f16c29ba68b8784693fd90a605cc8e83bec09
Diana Smetters <smetters@parc.com>
2009-05-22 11:36:50 -0700

    Initial network object wrapper for membership list.

--- Java_CCN/com/parc/ccn/security/access/MembershipList.java
+++ Java_CCN/com/parc/ccn/security/access/MembershipList.java
@@ -1,19 +1,45 @@
 package com.parc.ccn.security.access;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
+import javax.xml.stream.XMLStreamException;
+
+import com.parc.ccn.config.ConfigurationException;
+import com.parc.ccn.data.ContentName;
 import com.parc.ccn.data.content.CollectionData;
-import com.parc.ccn.data.content.LinkReference;
+import com.parc.ccn.data.util.CCNEncodableObject;
+import com.parc.ccn.library.CCNLibrary;
 
-public class MembershipList extends CollectionData {
+/**
+ * Eventually should extend Collection, when that moves onto encodable objects.
+ * @author smetters
+ *
+ */
+public class MembershipList extends CCNEncodableObject<CollectionData> {
 
-	public MembershipList() {
-		super();
+	public MembershipList() throws ConfigurationException, IOException {
+		super(CollectionData.class);
+	}
+	
+	public MembershipList(CCNLibrary library) throws IOException {
+		super(CollectionData.class, library);
+	}
+	
+	public MembershipList(ContentName name, CCNLibrary library) throws XMLStreamException, IOException {
+		super(CollectionData.class, name, library);
 	}
 
-	public MembershipList(ArrayList<LinkReference> contents) {
-		super(contents);
-		// TODO Auto-generated constructor stub
+	public MembershipList(ContentName name) throws XMLStreamException, IOException, ConfigurationException {
+		super(CollectionData.class, name);
 	}
+	public MembershipList(ContentName name, CollectionData data, CCNLibrary library) {
+		super(CollectionData.class, name, data, library);
+	}
+
+	public MembershipList(ContentName name, CollectionData data) throws ConfigurationException, IOException {
+		super(CollectionData.class, name, data);
+	}
+	
+	public CollectionData membershipList() { return data(); }
 
 }
