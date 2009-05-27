2dbaa08bcf747ea80dfdd2ee77ad26f7a6b90619
Diana Smetters <smetters@parc.com>
2009-05-27 12:53:21 -0700

    Modification functionality, made membership list read lazy.

--- Java_CCN/com/parc/ccn/security/access/Group.java
+++ Java_CCN/com/parc/ccn/security/access/Group.java
@@ -3,7 +3,7 @@ package com.parc.ccn.security.access;
 import java.io.IOException;
 import java.security.PublicKey;
 import java.sql.Timestamp;
-import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -32,9 +32,6 @@ public class Group {
 		_library = library;
 		_groupNamespace = namespace;
 		_groupFriendlyName = groupFriendlyName;
-		_groupMembers = new MembershipList(_library);
-		_groupMembers.updateInBackground(
-				AccessControlProfile.groupMembershipListName(_groupNamespace, _groupFriendlyName), true);
 		_groupPublicKey = new PublicKeyObject(_library);
 		_groupPublicKey.updateInBackground(
 				AccessControlProfile.groupPublicKeyName(_groupNamespace, _groupFriendlyName), true);
@@ -48,7 +45,8 @@ public class Group {
 	 * Package constructor.
 	 * @return
 	 */
-	Group(ContentName namespace, String groupFriendlyName, MembershipList members, PublicKeyObject publicKey, CCNLibrary library) {
+	Group(ContentName namespace, String groupFriendlyName, MembershipList members, 
+		  PublicKeyObject publicKey, CCNLibrary library) {
 		_library = library;
 		_groupNamespace = namespace;
 		_groupFriendlyName = groupFriendlyName;
@@ -56,8 +54,17 @@ public class Group {
 		_groupPublicKey = publicKey;
 	}
 	
+	/**
+	 * Constructor that creates a new group and generates a first key pair for it.
+	 * @return
+	 */
+	Group(ContentName namespace, String groupFriendlyName, MembershipList members, CCNLibrary library) {
+		this(namespace, groupFriendlyName, members, null, library);
+		createGroupPublicKey(members);
+	}
+	
 	public boolean ready() {
-		return _groupMembers.ready() && _groupPublicKey.ready();
+		return _groupPublicKey.ready();
 	}
 	
 	public KeyDirectory privateKeyDirectory(AccessControlManager manager) throws IOException {
@@ -70,9 +77,21 @@ public class Group {
 	
 	public String friendlyName() { return _groupFriendlyName; }
 
-	public MembershipList membershipList() { return _groupMembers; }
-	public ContentName membershipListName() { return _groupMembers.getName(); }
-	public Timestamp membershipListVersion() {
+	public MembershipList membershipList() throws XMLStreamException, IOException { 
+		if (null == _groupMembers) {
+			// Read constructor. Synchronously updates.
+			_groupMembers = new MembershipList(AccessControlProfile.groupMembershipListName(_groupNamespace, _groupFriendlyName), _library);
+			// Keep dynamically updating.
+			_groupMembers.updateInBackground(true);
+		}
+		return _groupMembers; 
+	}
+	
+	public ContentName membershipListName() throws XMLStreamException, IOException { 
+		return membershipList().getName(); 
+	}
+	
+	public Timestamp membershipListVersion() throws XMLStreamException, IOException {
 		ContentName name = membershipListName();
 		if (VersioningProfile.isVersioned(name)) {
 			try {
@@ -82,6 +101,13 @@ public class Group {
 			}
 		}
 		return null;
+	}
+	
+	public void clearMembershipList() {
+		if (null != _groupMembers) {
+			_groupMembers.cancelInterest(); // stop updating
+			_groupMembers = null;
+		}
 	}
 
 	public PublicKey publicKey() { return _groupPublicKey.publicKey(); }
@@ -98,36 +124,26 @@ public class Group {
 		return null;
 	}
 
-	public void setMembershipList(ArrayList<LinkReference> newMembers) throws XMLStreamException, IOException {
+	public void setMembershipList(Collection<LinkReference> newMembers) throws XMLStreamException, IOException {
 		// need to figure out if we need to know private key; if we do and we don't, throw access denied.
 		// We're deleting anyone that exists
-		if (!_groupMembers.isGone() && _groupMembers.ready() && (_groupMembers.membershipList().contents().size() > 0)) {
-			// There were already members. Remove them and make a new key.
-			_groupMembers.membershipList().removeAll();
-			_groupMembers.membershipList().add(newMembers);
-			// Don't save till we know we can update private key.
-			// If we can't update the private key, this will throw AccessDeniedException.
-			newGroupPublicKey(friendlyName(), _groupMembers);
-			_groupMembers.save();
+		MembershipList ml = membershipList(); // force retrieval if haven't already.
+		if (!ml.isGone() && ml.ready() && (ml.membershipList().contents().size() > 0)) {
+			modify(newMembers, ml.membershipList().contents());
 		} else {
-			// No existing members. Just add. Don't have to make  a new key if one exists,
-			// just rewrap it for existing members.
-			if (null == _groupMembers.membershipList()) {
-				_groupMembers.save(new CollectionData(newMembers));
-			} else {
-				_groupMembers.membershipList().add(newMembers);
-				_groupMembers.save();
-			}
-			if (null != _groupPublicKey.publicKey()) {
-				_
-			}
+			modify(newMembers, null);
 		}
 	}
-
-	public static PublicKeyObject newGroupPublicKey(String groupFriendlyName,
-			MembershipList ml) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public void newGroupPublicKey(MembershipList ml) {
+	}
+	
+	public void createGroupPublicKey(MembershipList ml) {
+		
+	}
+	
+	public void updateGroupPublicKey(Collection<LinkReference> membersToAdd) {
+		
 	}
 	
 	@Override
@@ -143,23 +159,73 @@ public class Group {
 			sb.append(publicKeyName());
 		}
 		sb.append(" membership list: ");
-		if (!_groupMembers.ready()) {
-			sb.append("not ready, write to " + 
+		if ((null == _groupMembers) || (!_groupMembers.ready())) {
+			sb.append("not ready, will write to " + 
 					AccessControlProfile.groupMembershipListName(_groupNamespace, friendlyName()));
 		} else {
-			sb.append(membershipListName());
+			try {
+				sb.append(membershipListName());
+			} catch (Exception e) {
+				Library.logger().warning("Unexpected " + e.getClass().getName() + " exception in getMembershipListName(): " + e.getMessage());
+				sb.append("Membership list name unavailable!");
+			} 
 		}
 		return sb.toString();
 	}
 
-	public void modify(ArrayList<LinkReference> membersToAdd,
-			ArrayList<LinkReference> membersToRemove) {
-		// TODO Auto-generated method stub
+	public void modify(Collection<LinkReference> membersToAdd,
+					   Collection<LinkReference> membersToRemove) throws XMLStreamException, IOException {
 		
+		boolean addedMembers = false;
+		boolean removedMembers = false;
+		
+		if (((null == membersToAdd) || (membersToAdd.size() == 0)) && ((null == membersToRemove) || (membersToRemove.size() == 0))) {
+			return; // nothing to do
+		}
+
+		// Do we need to wait for data to come in? We use this to create new groups as well...
+		// so in that case, don't expect any.
+		
+		// Add before remove so that remove overrides adds.
+		if ((null != membersToAdd) && (!membersToAdd.isEmpty())) {
+			if (null == _groupMembers.membershipList()) {
+				_groupMembers.setData(new CollectionData(membersToAdd));
+				addedMembers = true;
+			} else {
+				// Optimization: check to see if any were already in there before adding them....
+				addedMembers = _groupMembers.membershipList().contents().addAll(membersToAdd);
+			}
+		}
+		if ((null != membersToRemove) && (!membersToRemove.isEmpty()) &&
+			(!_groupMembers.isGone()) && 
+			_groupMembers.ready() && // do we wait if it's not ready? we know one exists.
+			(_groupMembers.membershipList().contents().size() > 0)) {
+	
+			// There were already members. Remove them and make a new key.
+			removedMembers = _groupMembers.membershipList().contents().removeAll(membersToRemove);
+		}
+		
+		if (removedMembers) {
+			// Don't save membership list till we know we can update private key.
+			// If we can't update the private key, this will throw AccessDeniedException.
+			newGroupPublicKey(_groupMembers); 
+		} else if (addedMembers) {
+			// additions only. Don't have to make  a new key if one exists,
+			// just rewrap it for added members.
+			if (null != _groupPublicKey.publicKey()) {
+				updateGroupPublicKey(membersToAdd);
+			} else {
+				createGroupPublicKey( _groupMembers);
+			}
+		}
+		// Don't actually save the new membership list till we're sure we can update the
+		// key.
+		_groupMembers.save();
 	}
 
-	public void delete() {
-		// TODO Auto-generated method stub
-		
+	public void delete() throws IOException {
+		// Deleting the group -- mark both membership list and public key as GONE.
+		_groupMembers.saveAsGone();
+		_groupPublicKey.saveAsGone();
 	}
 }
