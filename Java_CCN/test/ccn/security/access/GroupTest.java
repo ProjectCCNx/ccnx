902ab04606b7eef3753f59751543f66e032822e2
Diana Smetters <smetters@parc.com>
2009-06-04 14:58:10 -0700

    Skeleton for group test case.

---
+++ Java_CCN/test/ccn/security/access/GroupTest.java
@@ -0,0 +1,94 @@
+package test.ccn.security.access;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GroupTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testReady() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrivateKeyDirectory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFriendlyName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMembershipList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMembershipListName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMembershipListVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearCachedMembershipList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPublicKeyName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPublicKeyVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMembershipList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewGroupPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateGroupPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateGroupPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
