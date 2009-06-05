c45936640bb0d21be8f2f12a2046d468efed86bc
Paul Rasmussen <rasmusse@parc.com>
2009-06-05 16:11:48 -0700

    Implement "one big file" repo performance test

--- Java_CCN/com/parc/ccn/perf/RepoPerformanceTester.java
+++ Java_CCN/com/parc/ccn/perf/RepoPerformanceTester.java
@@ -2,8 +2,10 @@ package com.parc.ccn.perf;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Level;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -12,11 +14,10 @@ import com.parc.ccn.config.ConfigurationException;
 import com.parc.ccn.data.ContentName;
 import com.parc.ccn.data.ContentObject;
 import com.parc.ccn.data.MalformedContentNameStringException;
+import com.parc.ccn.data.WirePacket;
 import com.parc.ccn.library.CCNFlowControl;
 import com.parc.ccn.library.CCNLibrary;
 import com.parc.ccn.library.io.CCNOutputStream;
-import com.parc.ccn.network.daemons.repo.RFSImpl;
-import com.parc.ccn.network.daemons.repo.Repository;
 import com.parc.ccn.network.daemons.repo.RepositoryException;
 
 /**
@@ -28,26 +29,39 @@ import com.parc.ccn.network.daemons.repo.RepositoryException;
 public class RepoPerformanceTester extends CCNOutputStream {
 	
 	private static RepoPerformanceTester _rpt = new RepoPerformanceTester();
+	private static TestFlowControl _tfc = null;
 
 	private class TestFlowControl extends CCNFlowControl {
 		
-		private Repository _repo = null;
+		private FileOutputStream _fos = null;
 		
 		public TestFlowControl(String repoName, ContentName name, CCNLibrary library)
 				throws MalformedContentNameStringException, RepositoryException {
 			super(name, library);
+			_tfc = this;
 			if (repoName != null) {
-				_repo = new RFSImpl();
-				_repo.initialize(new String[] {"-root", repoName});
+				File file = new File(repoName + "/" + "repoFile");
+				try {
+					File repoDir = new File(repoName);
+					repoDir.mkdirs();
+					file.createNewFile();
+					_fos = new FileOutputStream(file);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 			}
 		}
 		
 		public ContentObject put(ContentObject co) throws IOException {
-			if (_repo != null) {
+			if (_fos != null) {
 				try {
-					_repo.saveContent(co);
-				} catch (RepositoryException e) {
-					throw new IOException(e.getMessage());
+					WirePacket packet = new WirePacket(co);
+					_fos.write(packet.encode());
+				} catch (XMLStreamException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
 			return co;
@@ -61,6 +75,12 @@ public class RepoPerformanceTester extends CCNOutputStream {
 		super(name, null, null, _rpt.new TestFlowControl(repoName, name, library));
 	}
 	
+	public void close() throws IOException {
+		super.close();
+		if (_tfc._fos != null)
+			_tfc._fos.close();
+	}
+	
 	private static int BLOCK_SIZE = 8096;
 
 	/**
@@ -70,6 +90,7 @@ public class RepoPerformanceTester extends CCNOutputStream {
 	
 		ContentName argName;
 		long startTime = new Date().getTime();
+		Library.logger().setLevel(Level.SEVERE);	// turn off logging
 		try {
 			argName = ContentName.fromURI(args[0]);
 			CCNLibrary library = CCNLibrary.open();
