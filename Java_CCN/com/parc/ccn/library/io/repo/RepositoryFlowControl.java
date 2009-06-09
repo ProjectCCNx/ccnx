6978e0e2c507908862296032123f8982e7e6197a
Diana Smetters <smetters@parc.com>
2009-06-09 12:21:55 -0700

    Additional constructor to make refactoring work.

--- Java_CCN/com/parc/ccn/library/io/repo/RepositoryFlowControl.java
+++ Java_CCN/com/parc/ccn/library/io/repo/RepositoryFlowControl.java
@@ -87,8 +87,12 @@ public class RepositoryFlowControl extends CCNFlowControl implements CCNInterest
 	}
 
 	public RepositoryFlowControl(ContentName name, CCNLibrary library) throws IOException {
-		super(library); 
+		this(library); 
 		addNameSpace(name);
+	}
+	
+	public RepositoryFlowControl(CCNLibrary library) throws IOException {
+		super(library);
 	}
 
 	@Override
