e49b345d76bda7d93d5462d2327225f6e653d2f5
Simon Barber <sbarber@parc.com>
2009-06-11 12:43:15 -0700

    Utility compare function

--- Java_CCN/com/parc/ccn/data/util/DataUtils.java
+++ Java_CCN/com/parc/ccn/data/util/DataUtils.java
@@ -125,6 +125,22 @@ public class DataUtils {
 		return Base64.encode(input);
 	}
 
+	public static boolean arrayEquals(byte[] left, byte[] right) {
+		if (left == null) {
+			return ((right == null) ? true : false);
+		}
+		if (right == null) {
+			return ((left == null) ? true : false);
+		}
+		if (left.length != right.length)
+			return false;
+		for (int i = 0; i < left.length; i++) {
+			if (left[i] != right[i])
+				return false;
+		}
+		return true;
+	}
+	
 	public static boolean arrayEquals(byte[] left, byte[] right, int length) {
 		if (left == null) {
 			return ((right == null) ? true : false);
