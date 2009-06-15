10433524bb955ed0396e09034055ded6c5693f8c
Diana Smetters <smetters@parc.com>
2009-06-15 11:20:53 -0700

    Build test users.

--- Java_CCN/test/ccn/security/access/GroupTest.java
+++ Java_CCN/test/ccn/security/access/GroupTest.java
@@ -5,15 +5,40 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.parc.ccn.Library;
+import com.parc.ccn.data.ContentName;
+import com.parc.ccn.library.CCNLibrary;
+
 public class GroupTest {
+	
+	static boolean USE_REPO = true;
+	static int NUM_USERS = 20;
+	static char [] USER_PASSWORD = new String("password").toCharArray();
+	
+	static ContentName testStorePrefix = null;
+	static ContentName userStore = null;
+	static ContentName groupStore = null;
 	
 	/**
 	 * Have to make a bunch of users.
 	 * @throws Exception
 	 */
+	static TestUserData users = null;
+	static CCNLibrary userLibrary = null;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception { 
+		try {
+			testStorePrefix = ContentName.fromNative("/test/access");
+			userStore = ContentName.fromNative(testStorePrefix, "Users");
+			groupStore = ContentName.fromNative(testStorePrefix, "Groups");
+
+			users = new TestUserData(userStore, NUM_USERS, USE_REPO, USER_PASSWORD, userLibrary);
+		} catch (Exception e) {
+			Library.logger().warning("Exception in setupBeforeClass: " + e);
+			Library.warningStackTrace(e);
+			throw e;
+		}
 	}
 
 	@Test
