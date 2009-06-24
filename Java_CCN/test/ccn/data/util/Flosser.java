b6d59e208b37d7ac90c8498efbd1d8d06fbf2ae4
Simon Barber <sbarber@parc.com>
2009-06-23 19:05:06 -0700

    new Flosser(name) now works.

--- Java_CCN/test/ccn/data/util/Flosser.java
+++ Java_CCN/test/ccn/data/util/Flosser.java
@@ -43,6 +43,7 @@ public class Flosser implements CCNInterestListener {
 	}
 	
 	public Flosser(ContentName namespace) throws ConfigurationException, IOException {
+		this();
 		handleNamespace(namespace);
 	}
 	
