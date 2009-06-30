8c3c0111272136e9b66c2af62f0dfff7dee5311b
Simon Barber <sbarber@parc.com>
2009-06-29 19:46:42 -0700

    A quick new test to check a repo is run when access control tests that require a repo are run

---
+++ Java_CCN/test/ccn/security/access/SampleTestRepo.java
@@ -0,0 +1,41 @@
+package test.ccn.security.access;
+
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.parc.ccn.Library;
+import com.parc.ccn.data.ContentName;
+import com.parc.ccn.library.CCNLibrary;
+import com.parc.ccn.library.EnumeratedNameList;
+import com.parc.ccn.library.io.repo.RepositoryOutputStream;
+
+public class SampleTestRepo {
+	static final String base = "/parc.com/csl/ccn/repositories";
+	static final String file_name = "simon.txt";
+	static final String txt =  "Sample text file from Simon.";
+	static final Charset UTF8 = Charset.forName("UTF-8");
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ContentName name = ContentName.fromNative(base + "/" + file_name);
+		RepositoryOutputStream os = new RepositoryOutputStream(name, CCNLibrary.getLibrary());
+		
+		os.write(txt.getBytes(UTF8));
+		os.close();
+	}
+
+	@Test
+	public void readWrite() throws Exception {
+		EnumeratedNameList l = new EnumeratedNameList(ContentName.fromNative(base), null);
+		ArrayList<byte []> r = l.getNewData();
+		Assert.assertNotNull(r);
+		Assert.assertEquals(1, r.size());
+		Assert.assertEquals(file_name.getBytes(UTF8), r.get(0));
+	}
+}
