345183f3d6185fe47822ffb425f84248cdd2c25f
Michael Plass <plass@parc.com>
2009-07-01 17:22:27 -0700

    For more history, use git log -- Linux.mk ../../ccnd/conf/Linux.mk

--- src/conf/Linux.mk
+++ src/conf/Linux.mk
@@ -5,3 +5,4 @@ SHARED_LD_FLAGS = -shared --whole-archive -soname=$(SHLIBNAME) -lc
 PLATCFLAGS=-fPIC
 CWARNFLAGS = -Wall -Wpointer-arith -Wreturn-type -Wstrict-prototypes
 CPREFLAGS= -I../include -D_REENTRANT
+
