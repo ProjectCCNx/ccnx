527c8529573a1fe4a2382825efda8c562e57df7b
Michael Plass <plass@parc.com>
2009-07-09 10:21:23 -0700

    Split ccnd_msg() and ccnd_debug_ccnb() out of ccnd/ccnd_stats.c into new file ccnd/ccnd_msg.c

--- src/ccnd/ccnd_stats.c
+++ src/ccnd/ccnd_stats.c
@@ -8,7 +8,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -313,90 +312,4 @@ ccnd_stats_httpd_start(struct ccnd *h)
     ai = NULL;
     h->httpd_listener_fd = sock;
     return(0);
-}
-
-/* ccnd_msg may migrate into a different place ... */
-
-/**
- *  Produce ccnd debug output.
- *  Output is produced on stderr under the control of h->debug;
- *  prepends decimal timestamp and process identification.
- *  Caller should not supply newlines.
- *  @param      h  the ccnd handle
- *  @param      fmt  printf-like format string
- */
-void
-ccnd_msg(struct ccnd *h, const char *fmt, ...)
-{
-    struct timeval t;
-    va_list ap;
-    struct ccn_charbuf *b;
-    if (h != NULL && h->debug == 0)
-        return;
-    b = ccn_charbuf_create();
-    gettimeofday(&t, NULL);
-    if ((h != NULL) && ((h->debug & 64) != 0) &&
-        ((h->logbreak-- < 0 && t.tv_sec != h->logtime) ||
-          t.tv_sec >= h->logtime + 30)) {
-        fprintf(stderr, "%ld.000000 ccnd[%d]: _______________________ %s",
-                (long)t.tv_sec, (int)getpid(), ctime(&t.tv_sec));
-        h->logtime = t.tv_sec;
-        h->logbreak = 30;
-    }
-    ccn_charbuf_putf(b, "%ld.%06u ccnd[%d]: %s\n",
-        (long)t.tv_sec, (unsigned)t.tv_usec, (int)getpid(), fmt);
-    va_start(ap, fmt);
-    vfprintf(stderr, (const char *)b->buf, ap);
-    ccn_charbuf_destroy(&b);
-    /* if there's no one to hear, don't make a sound */
-    if (ferror(stderr))
-	h->debug = 0;
-}
-
-/**
- *  Produce a ccnd debug trace entry.
- *  Output is produced by calling ccnd_msg.
- *  @param      h  the ccnd handle
- *  @param      lineno  caller's source line number (usually __LINE__)
- *  @param      msg  a short text tag to identify the entry
- *  @param      face    handle of associated face; may be NULL
- *  @param      ccnb    points to ccnb-encoded Interest or ContentObject
- *  @param      ccnb_size   is in bytes
- */
-void
-ccnd_debug_ccnb(struct ccnd *h,
-                int lineno,
-                const char *msg,
-                struct face *face,
-                const unsigned char *ccnb,
-                size_t ccnb_size)
-{
-    struct ccn_charbuf *c;
-    struct ccn_parsed_interest pi;
-    const unsigned char *nonce = NULL;
-    size_t nonce_size = 0;
-    size_t i;
-    
-    if (h != NULL && h->debug == 0)
-        return;
-    c = ccn_charbuf_create();
-    ccn_charbuf_putf(c, "debug.%d %s ", lineno, msg);
-    if (face != NULL)
-        ccn_charbuf_putf(c, "%u ", face->faceid);
-    ccn_uri_append(c, ccnb, ccnb_size, 1);
-    ccn_charbuf_putf(c, " (%u bytes)", (unsigned)ccnb_size);
-    if (ccn_parse_interest(ccnb, ccnb_size, &pi, NULL) >= 0) {
-        ccn_ref_tagged_BLOB(CCN_DTAG_Nonce, ccnb,
-                  pi.offset[CCN_PI_B_Nonce],
-                  pi.offset[CCN_PI_E_Nonce],
-                  &nonce,
-                  &nonce_size);
-        if (nonce_size > 0) {
-            ccn_charbuf_putf(c, " ");
-            for (i = 0; i < nonce_size; i++)
-                ccn_charbuf_putf(c, "%02X", nonce[i]);
-        }
-    }
-    ccnd_msg(h, "%s", ccn_charbuf_as_string(c));
-    ccn_charbuf_destroy(&c);
 }
