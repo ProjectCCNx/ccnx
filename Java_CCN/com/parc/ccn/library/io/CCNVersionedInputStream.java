17a5d73b0fa09a5ff9e1067e63ba03a1630a0ec2
Diana Smetters <smetters@parc.com>
2009-07-13 16:20:40 -0700

    Fixes first block handling in the face of spurious data, like repo_start_writes. refs #498

--- Java_CCN/com/parc/ccn/library/io/CCNVersionedInputStream.java
+++ Java_CCN/com/parc/ccn/library/io/CCNVersionedInputStream.java
@@ -92,18 +92,61 @@ public class CCNVersionedInputStream extends CCNInputStream {
 			if (!verifyBlock(result)) {
 				return null;
 			}
-			_baseName = SegmentationProfile.segmentRoot(result.name());
 			// Now we know the version. Did we luck out and get first block?
-			if (isFirstBlock(result)) {
+			if (isFirstBlock(_baseName, result)) {
 				Library.logger().info("getFirstBlock: got first block on first try: " + result.name());
+				_baseName = SegmentationProfile.segmentRoot(result.name());
 				return result;
 			}
+			// This isn't the first block. Might be simply a later (cached) segment, or might be something
+			// crazy like a repo_start_write. So what we want is to get the version of this new block -- if getLatestVersion
+			// is doing its job, we now know the version we want (if we already knew that, we called super.getFirstBlock
+			// above. If we get here, _baseName isn't versioned yet. So instead of taking segmentRoot of what we got,
+			// which works fine only if we have the wrong segment rather than some other beast entirely (like metadata).
+			// So chop off the new name just after the (first) version, and use that. If getLatestVersion is working
+			// right, that should be the right thing.
+			_baseName = result.name().cut(_baseName.count() + 1);
 			Library.logger().info("getFirstBlock: Have version information, now querying first segment.");
 			return super.getFirstBlock(); // now that we have the latest version, go back for the first block.
 		} else {
 			Library.logger().info("getFirstBlock: no block available for latest version of " + _baseName);
 		}
 		return result;
+	}
+	
+	/**
+	 * Version of isFirstBlock that expects names to be versioned, and allows that desiredName
+	 * won't know what version it wants but will want some version.
+	 */
+	protected boolean isFirstBlock(ContentName desiredName, ContentObject block) {
+		if ((null != block) && (SegmentationProfile.isSegment(block.name()))) {
+			Library.logger().info("is " + block.name() + " a first block of " + desiredName);
+			// In theory, the block should be at most a versioning component different from desiredName.
+			// In the case of complex segmented objects (e.g. a KeyDirectory), where there is a version,
+			// then some name components, then a segment, desiredName should contain all of those other
+			// name components -- you can't use the usual versioning mechanisms to pull first block anyway.
+			if (!desiredName.isPrefixOf(block.name())) {
+				Library.logger().info("Desired name :" + desiredName + " is not a prefix of block: " + block.name());
+				return false;
+			}
+			int difflen = block.name().count() - desiredName.count();
+			if (difflen > 2) {
+				Library.logger().info("Have " + difflen + " extra components between " + block.name() + " and desired " + desiredName);
+				return false;
+			}
+			// Now need to make sure that if the difference is more than 1, that difference is
+			// a version component.
+			if ((difflen == 2) && (!VersioningProfile.isVersionComponent(block.name().component(block.name().count()-2)))) {
+				Library.logger().info("The " + difflen + " extra component between " + block.name() + " and desired " + desiredName + " is not a version.");
+				
+			}
+			if (null != _startingBlockIndex) {
+				return (_startingBlockIndex.equals(SegmentationProfile.getSegmentNumber(block.name())));
+			} else {
+				return SegmentationProfile.isFirstSegment(block.name());
+			}
+		}
+		return false;
 	}
 	
 	public Timestamp getVersionAsTimestamp() throws VersionMissingException {
