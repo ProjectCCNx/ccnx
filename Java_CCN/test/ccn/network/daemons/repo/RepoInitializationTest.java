3f484e0205ff39612923a75bb6e880922b8b3279
Paul Rasmussen <rasmusse@parc.com>
2009-07-30 11:22:48 -0700

    Add test to make sure repo is up and fix dependencies to make sure it's run in the correct spot in build.xml refs #518

---
+++ Java_CCN/test/ccn/network/daemons/repo/RepoInitializationTest.java
@@ -0,0 +1,33 @@
+package test.ccn.network.daemons.repo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.parc.ccn.data.ContentName;
+
+/**
+ * Test to insure repo is up before continuing
+ * @author rasmusse
+ *
+ */
+
+public class RepoInitializationTest extends RepoTestBase {
+	public static final int TEST_TIME = 15000; // ms - 30 seconds
+	public static final int TEST_INTERVAL = 3000;
+	public int countDown = TEST_TIME;
+	
+	@Test
+	public void testRepoIsUp() throws Throwable {
+		while (countDown >= 0) {
+			try {
+				testWriteToRepo(ContentName.fromNative("/repoTest/upTest"));
+				return;
+			} catch (IOException ioe) {}
+			Thread.sleep(TEST_INTERVAL);
+			countDown -= TEST_INTERVAL;
+		}
+		Assert.fail("Repo never came up");
+	}
+}
