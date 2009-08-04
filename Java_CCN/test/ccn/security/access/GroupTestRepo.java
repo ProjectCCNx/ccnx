ba3aa0f068cdc7fd8785d934b565079e9c2b992a
Elaine Shi <eshi@parc.com>
2009-08-03 18:08:08 -0700

    refs #398 group public key is null in test of creating group

--- Java_CCN/test/ccn/security/access/GroupTestRepo.java
+++ Java_CCN/test/ccn/security/access/GroupTestRepo.java
@@ -2,12 +2,25 @@ package test.ccn.security.access;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.SortedSet;
+
+
+
+import junit.framework.Assert;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.parc.ccn.Library;
+
 import com.parc.ccn.data.ContentName;
+import com.parc.ccn.data.content.LinkReference;
 import com.parc.ccn.library.CCNLibrary;
+import com.parc.ccn.library.EnumeratedNameList;
+import com.parc.ccn.security.access.AccessControlManager;
+import com.parc.ccn.security.access.GroupManager;
 
 public class GroupTestRepo {
 	
@@ -19,19 +32,30 @@ public class GroupTestRepo {
 	static ContentName userStore = null;
 	static ContentName groupStore = null;
 	
+	static EnumeratedNameList _userList = null;
+	static CCNLibrary _library = null;
+	
 	/**
 	 * Have to make a bunch of users.
 	 * @throws Exception
 	 */
 	static TestUserData users = null;
 	static CCNLibrary userLibrary = null;
+	static AccessControlManager _acm = null;
+	static GroupManager _gm = null;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception { 
 		try {
-			testStorePrefix = ContentName.fromNative("/test/access");
+			
+			testStorePrefix = ContentName.fromNative("/parc/");
 			userStore = ContentName.fromNative(testStorePrefix, "Users");
 			groupStore = ContentName.fromNative(testStorePrefix, "Groups");
+			
+			_library = CCNLibrary.open();
+			_acm = new AccessControlManager(testStorePrefix, groupStore, userStore);
+			_userList = _acm.userList();
+			_gm = _acm.groupManager();
 
 	//		users = new TestUserData(userStore, NUM_USERS, USE_REPO, USER_PASSWORD, userLibrary);
 		} catch (Exception e) {
@@ -42,10 +66,43 @@ public class GroupTestRepo {
 	}
 
 	@Test
-	public void testReady() {
-		fail("Not yet implemented");
+	public void testCreateGroup() {
+		try {
+			Assert.assertNotNull(_userList);
+			ContentName prefixTest = _userList.getName();
+			Assert.assertNotNull(prefixTest);
+			Library.logger().info("***************** Prefix is "+ prefixTest.toString());
+			Assert.assertEquals(prefixTest, userStore);
+			
+			_userList.waitForData();
+			Assert.assertTrue(_userList.hasNewData());
+			SortedSet<ContentName> returnedBytes = _userList.getNewData();
+			Assert.assertNotNull(returnedBytes);
+			Assert.assertFalse(returnedBytes.isEmpty());
+			
+			System.out.println("Got " + returnedBytes.size() + " children: " + returnedBytes);
+			System.out.print("names in list:");
+			for(ContentName n: returnedBytes)
+				System.out.print(" "+n);
+			System.out.println();
+			
+			ArrayList<LinkReference> newMembers = new ArrayList<LinkReference>();
+			assertTrue(returnedBytes.size() > 3);
+			Iterator<ContentName> it = returnedBytes.iterator();
+			
+			for(int i = 0; i <3; i++){
+				newMembers.add(new LinkReference(it.next()));
+			}
+			System.out.println("creating a group...");
+			_gm.createGroup("testGroup", newMembers);
+			
+		} catch (Exception e) {
+			System.out.println("Exception : " + e.getClass().getName() + ": " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
-
+	
+	
 	@Test
 	public void testPrivateKeyDirectory() {
 		fail("Not yet implemented");
