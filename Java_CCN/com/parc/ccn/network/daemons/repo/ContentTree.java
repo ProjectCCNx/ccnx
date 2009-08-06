2e8cb7206c8f732cdc524254f4db5f5187fdc432
Rebecca Braynard <rbraynar@parc.com>
2009-08-06 16:08:33 -0700

    cleaned up code from switch to collections.  added timestamp to NameEnumerationResponse object so it doesn't have to be added and then cut from the prefix name on a CollectionObject.save(Timestamp)

--- Java_CCN/com/parc/ccn/network/daemons/repo/ContentTree.java
+++ Java_CCN/com/parc/ccn/network/daemons/repo/ContentTree.java
@@ -461,6 +461,34 @@ public class ContentTree {
 		//first chop off NE marker
 		ContentName prefix = interest.name().cut(CCNNameEnumerator.NEMARKER);
 
+		//prefix = VersioningProfile.versionRoot(prefix);
+		boolean versionedInterest = false;
+		//get the index of the name enumeration marker
+		int markerIndex = prefix.count();
+		if (interest.name().count() > markerIndex) {
+			//we have something longer than just the name enumeration marker
+			if (VersioningProfile.findLastVersionComponent(interest.name()) > markerIndex)
+				versionedInterest = true;
+		}
+		
+		//does the interest have a timestamp?
+		Timestamp interestTS = null;
+		Timestamp nodeTS = null;
+
+		if (versionedInterest) {
+		
+			try {
+				byte[] versionComponent = interest.name().component(markerIndex+1);
+				interestTS = VersioningProfile.getVersionComponentAsTimestamp(versionComponent);
+				//interestTS = VersioningProfile.getLastVersionAsTimestamp(interest.name());
+				Library.logger().fine("interestTS: "+interestTS+" "+interestTS.getTime());
+			} catch(Exception e) {
+				interestTS = null;
+			}
+		} else {
+			Library.logger().finest("no timestamp in interest after the name enumeration marker");
+		}
+		
 		Library.logger().fine("checking for content names under: "+prefix);
 		
 		TreeNode parent = lookupNode(prefix, prefix.count());
