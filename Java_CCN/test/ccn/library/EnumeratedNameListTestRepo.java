03a267fdbadd9f3016da97b150383bb68b5e9a29
Diana Smetters <smetters@parc.com>
2009-08-06 00:10:48 -0700

    Extract type for use in tests.

--- Java_CCN/test/ccn/library/EnumeratedNameListTestRepo.java
+++ Java_CCN/test/ccn/library/EnumeratedNameListTestRepo.java
@@ -16,9 +16,6 @@ import org.junit.Test;
 import com.parc.ccn.Library;
 import com.parc.ccn.config.ConfigurationException;
 import com.parc.ccn.data.ContentName;
-import com.parc.ccn.data.ContentObject;
-import com.parc.ccn.data.security.PublisherPublicKeyDigest;
-import com.parc.ccn.data.util.CCNSerializableObject;
 import com.parc.ccn.library.CCNLibrary;
 import com.parc.ccn.library.EnumeratedNameList;
 
@@ -252,40 +249,6 @@ public class EnumeratedNameListTestRepo {
 			Assert.fail("Failed test with exception " + e.getMessage());
 		}			
 	}
-	
-	class CCNStringObject extends CCNSerializableObject<String> {
-
-		public CCNStringObject(ContentName name, String data, CCNLibrary library) throws ConfigurationException, IOException {
-			super(String.class, name, data, library);
-		}
-		
-		public CCNStringObject(ContentName name, PublisherPublicKeyDigest publisher,
-				CCNLibrary library) throws ConfigurationException, IOException, XMLStreamException {
-			super(String.class, name, publisher, library);
-		}
-		
-		/**
-		 * Read constructor -- opens existing object.
-		 * @param type
-		 * @param name
-		 * @param library
-		 * @throws XMLStreamException
-		 * @throws IOException
-		 * @throws ClassNotFoundException 
-		 */
-		public CCNStringObject(ContentName name, 
-				CCNLibrary library) throws ConfigurationException, IOException, XMLStreamException {
-			super(String.class, name, (PublisherPublicKeyDigest)null, library);
-		}
-		
-		public CCNStringObject(ContentObject firstBlock,
-				CCNLibrary library) throws ConfigurationException, IOException, XMLStreamException {
-			super(String.class, firstBlock, library);
-		}
-		
-		public String string() { return data(); }
-	}
-	
 	
 	/*
 	 * Adds data to the repo for testing
