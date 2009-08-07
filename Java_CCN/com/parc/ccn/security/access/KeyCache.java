aeb948acd73ca34977afc1983dd21d5b8d9c2ecd
Philippe Golle <pgolle@parc.com>
2009-08-07 14:05:07 -0700

    Fixed a bug in KeyCache

--- Java_CCN/com/parc/ccn/security/access/KeyCache.java
+++ Java_CCN/com/parc/ccn/security/access/KeyCache.java
@@ -15,11 +15,11 @@ import com.parc.ccn.security.keys.KeyManager;
  */
 public class KeyCache {
 	
-	private HashMap<byte [], Key> _keyMap = new HashMap<byte [], Key>();
-	private HashMap<byte [], PrivateKey> _myKeyMap = new HashMap<byte [], PrivateKey>();
-	private HashMap<byte [], PrivateKey> _privateKeyMap = new HashMap<byte [], PrivateKey>();
-	private HashMap<byte [], byte []> _privateKeyIdentifierMap = new HashMap<byte [], byte[]>();
-	private HashMap<byte [], ContentName> _keyNameMap = new HashMap<byte [], ContentName>();
+	private HashMap<String, Key> _keyMap = new HashMap<String, Key>();
+	private HashMap<String, PrivateKey> _myKeyMap = new HashMap<String, PrivateKey>();
+	private HashMap<String, PrivateKey> _privateKeyMap = new HashMap<String, PrivateKey>();
+	private HashMap<String, String> _privateKeyIdentifierMap = new HashMap<String, String>();
+	private HashMap<String, ContentName> _keyNameMap = new HashMap<String, ContentName>();
 	
 	public KeyCache() {
 		this(KeyManager.getKeyManager());
@@ -33,26 +33,29 @@ public class KeyCache {
 	}
 	
 	Key getKey(byte [] desiredKeyIdentifier) {
-		Key theKey = _keyMap.get(desiredKeyIdentifier);
+		String desiredKeyIdentifierAsString = new String(desiredKeyIdentifier);
+		Key theKey = _keyMap.get(desiredKeyIdentifierAsString);
 		if (null == theKey) {
-			theKey = _privateKeyMap.get(desiredKeyIdentifier);
+			theKey = _privateKeyMap.get(desiredKeyIdentifierAsString);
 		}
 		if (null == theKey) {
-			theKey = _myKeyMap.get(desiredKeyIdentifier);
+			theKey = _myKeyMap.get(desiredKeyIdentifierAsString);
 		}
 		return theKey;
 	}
 	
 	public boolean containsKey(byte [] keyIdentifier) {
-		if ((_keyMap.containsKey(keyIdentifier)) || (_myKeyMap.containsKey(keyIdentifier)) ||
-					(_privateKeyMap.containsKey(keyIdentifier))) {
+		String keyIdentifierAsString = new String(keyIdentifier);
+		if ((_keyMap.containsKey(keyIdentifierAsString)) || (_myKeyMap.containsKey(keyIdentifierAsString)) ||
+					(_privateKeyMap.containsKey(keyIdentifierAsString))) {
 			return true;
 		}
 		return false;
 	}
 	
 	public ContentName getKeyName(byte [] keyIdentifier) {
-		return _keyNameMap.get(keyIdentifier);
+		String keyIdentifierAsString = new String(keyIdentifier);
+		return _keyNameMap.get(keyIdentifierAsString);
 	}
 	
 	public ContentName getKeyName(Key key) {
@@ -60,27 +63,32 @@ public class KeyCache {
 	}
 
 	PrivateKey getPrivateKey(byte [] desiredPublicKeyIdentifier) {
-		PrivateKey key = _myKeyMap.get(desiredPublicKeyIdentifier);
+		String desiredPublicKeyIdentifierAsString = new String(desiredPublicKeyIdentifier);
+		PrivateKey key = _myKeyMap.get(desiredPublicKeyIdentifierAsString);
 		if (null == key) {
-			_privateKeyMap.get(desiredPublicKeyIdentifier);
+			_privateKeyMap.get(desiredPublicKeyIdentifierAsString);
 		}
 		return key;
 	}
 	
 	void addPrivateKey(ContentName keyName, byte [] publicKeyIdentifier, PrivateKey pk) {
-		_privateKeyMap.put(publicKeyIdentifier, pk);
-		_privateKeyIdentifierMap.put(getKeyIdentifier(pk), publicKeyIdentifier);
+		String publicKeyIdentifierAsString = new String(publicKeyIdentifier);
+		_privateKeyMap.put(publicKeyIdentifierAsString, pk);
+		String privateKeyIdentifierAsString = new String(getKeyIdentifier(pk));
+		_privateKeyIdentifierMap.put(privateKeyIdentifierAsString, publicKeyIdentifierAsString);
 		if (null != keyName)
-			_keyNameMap.put(publicKeyIdentifier, keyName);
+			_keyNameMap.put(publicKeyIdentifierAsString, keyName);
 	}
 
 	void addMyPrivateKey(byte [] publicKeyIdentifier, PrivateKey pk) {
-		_privateKeyIdentifierMap.put(getKeyIdentifier(pk), publicKeyIdentifier);
-		_myKeyMap.put(publicKeyIdentifier, pk);
+		String publicKeyIdentifierAsString = new String(publicKeyIdentifier);
+		String privateKeyIdentifierAsString = new String(getKeyIdentifier(pk));
+		_privateKeyIdentifierMap.put(privateKeyIdentifierAsString, publicKeyIdentifierAsString);
+		_myKeyMap.put(publicKeyIdentifierAsString, pk);
 	}
 	
 	public void addKey(ContentName name, Key key) {
-		byte [] id = getKeyIdentifier(key);
+		String id = new String(getKeyIdentifier(key));
 		_keyMap.put(id, key);
 		_keyNameMap.put(id, name);
 	}
