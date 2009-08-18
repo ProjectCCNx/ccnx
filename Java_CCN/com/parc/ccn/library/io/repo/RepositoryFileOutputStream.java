a5155de6167f886ebd6a6f7fd7b6b3b83179bd23
Paul Rasmussen <rasmusse@parc.com>
2009-08-18 12:17:46 -0700

    Fixes for new RepoFlowControl w/startWrite refs #546

--- Java_CCN/com/parc/ccn/library/io/repo/RepositoryFileOutputStream.java
+++ Java_CCN/com/parc/ccn/library/io/repo/RepositoryFileOutputStream.java
@@ -1,5 +1,7 @@
 package com.parc.ccn.library.io.repo;
 
+import com.parc.ccn.library.CCNFlowControl.Shape;
+import com.parc.ccn.library.io.CCNFileOutputStream;
 import java.io.IOException;
 
 import com.parc.ccn.data.ContentName;
@@ -7,18 +9,23 @@ import com.parc.ccn.data.security.KeyLocator;
 import com.parc.ccn.data.security.PublisherPublicKeyDigest;
 import com.parc.ccn.data.security.SignedInfo.ContentType;
 import com.parc.ccn.library.CCNLibrary;
-import com.parc.ccn.library.io.CCNFileOutputStream;
 
 public class RepositoryFileOutputStream extends CCNFileOutputStream {
 
 	public RepositoryFileOutputStream(ContentName name, CCNLibrary library) throws IOException {
-		this(name, null, library);
+		this(name, null, null, null, library);
 	}
 	
 	public RepositoryFileOutputStream(ContentName name,
 			PublisherPublicKeyDigest publisher, CCNLibrary library)
 			throws IOException {
-		super(name, null, publisher, null, new RepositoryFlowControl(name, library));
+		this(name, null, publisher, null, library);
+	}
+
+	public RepositoryFileOutputStream(ContentName name, KeyLocator locator,
+			PublisherPublicKeyDigest publisher, ContentType type, CCNLibrary library)
+			throws IOException {
+		super(name, locator, publisher, type, new RepositoryFlowControl(name, library));
 	}
 
 	public RepositoryFileOutputStream(ContentName name, KeyLocator locator,
