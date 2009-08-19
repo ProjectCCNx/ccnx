8d9fed959edbb9820e74fc4ca75dce25b990a629
Diana Smetters <smetters@parc.com>
2009-08-19 13:11:11 -0700

    Utility functions, make marker byte [] so don't have to convert multiple times.

--- Java_CCN/com/parc/ccn/library/profiles/MetadataProfile.java
+++ Java_CCN/com/parc/ccn/library/profiles/MetadataProfile.java
@@ -1,6 +1,12 @@
 package com.parc.ccn.library.profiles;
 
+import com.parc.ccn.data.ContentName;
+
 public class MetadataProfile implements CCNProfile {
 
-	public static final String CLIENT_METADATA_MARKER = MARKER + "meta" + MARKER;
+	public static final byte [] METADATA_MARKER = ContentName.componentParseNative(MARKER + "meta" + MARKER);
+	
+	public static ContentName metadataName(ContentName baseName) {
+		return new ContentName(baseName, METADATA_MARKER);
+	}
 }
