1872cf0d49ddcdb6f211acde88df1a5920f7e6c7
Simon Barber <sbarber@parc.com>
2009-08-20 18:07:16 -0700

    Add getBytes() method to ExcludeComponent. refs #627

--- Java_CCN/com/parc/ccn/data/query/ExcludeComponent.java
+++ Java_CCN/com/parc/ccn/data/query/ExcludeComponent.java
@@ -52,4 +52,8 @@ public class ExcludeComponent extends ExcludeFilter.Element implements Comparabl
 		ExcludeComponent ec = (ExcludeComponent) obj;
 		return DataUtils.arrayEquals(body, ec.body);
 	}
+
+	public byte [] getBytes() {
+		return body.clone();
+	}
 }
