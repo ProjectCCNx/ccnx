9d277aee02284bb70f53e1fa9d7d9b001ab46cd6
Diana Smetters <smetters@parc.com>
2009-08-30 09:45:48 -0700

    Removed specialized header-reading infrastructure. Now that header is a stream, should be fine.

--- javasrc/src/org/ccnx/ccn/profiles/CommandMarkers.java
+++ javasrc/src/org/ccnx/ccn/profiles/CommandMarkers.java
@@ -13,7 +13,6 @@ public class CommandMarkers {
 	 * Repository "markers"
 	 */
 	public static final byte[] REPO_START_WRITE = {(byte)0xc0, 'R', 0x1};
-	public static final byte[] REPO_GET_HEADER = {(byte)0xc0, 'R', 0x2};
 	
 	/**
 	 * Nonce marker
