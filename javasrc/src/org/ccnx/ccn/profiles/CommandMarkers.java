1185aa5fdfca7239e89d3e3ed3cc12f0c3191c64
Diana Smetters <smetters@parc.com>
2009-08-30 07:47:55 -0700

    Added comment. Not sure if these are just supposed to be repo command markers, or all reserved bytes in general. if the latter, need to add segmentation and versioning. If the former, need to name for better clarity.

--- javasrc/src/org/ccnx/ccn/profiles/CommandMarkers.java
+++ javasrc/src/org/ccnx/ccn/profiles/CommandMarkers.java
@@ -8,7 +8,7 @@ package org.ccnx.ccn.profiles;
  *
  */
 public class CommandMarkers {
-
+	
 	/**
 	 * Repository "markers"
 	 */
@@ -19,6 +19,10 @@ public class CommandMarkers {
 	 * Nonce marker
 	 */
 	public static final byte[] NONCE_MARKER = {(byte)0xc0, 'N'};
+	
+	/**
+	 * Reserved bytes.
+	 */
 	public static byte[] CCN_reserved_markers = { (byte)0xC0, (byte)0xC1, (byte)0xF5, 
 	(byte)0xF6, (byte)0xF7, (byte)0xF8, (byte)0xF9, (byte)0xFA, (byte)0xFB, (byte)0xFC, 
 	(byte)0xFD, (byte)0xFE};
