ea046f8bc21a99ea289876bc320ff0e1df0159d1
Michael Plass <plass@parc.com>
2009-09-02 12:43:02 -0700

    Change sequence numbering prefix but from 0xF0 to 0x00 (csrc amd doc)x/.
    refs #548  binary prefix in name components

--- csrc/include/ccn/ccn.h
+++ csrc/include/ccn/ccn.h
@@ -184,10 +184,10 @@ int ccn_name_append_components(struct ccn_charbuf *c,
 
 enum ccn_marker {
     CCN_MARKER_NONE = -1,
-    CCN_MARKER_SEQNUM  = 0xF8, /* consecutive block sequence numbers */
-    CCN_MARKER_BLKID   = 0xFB, /* nonconsecutive block ids */
-    CCN_MARKER_VERSION = 0xFD, /* timestamp-based versioning */
-    CCN_MARKER_NAMES   = 0xFE  /* name enumeration protocol */
+    CCN_MARKER_SEQNUM  = 0x00, /**< consecutive block sequence numbers */
+    CCN_MARKER_OSEQNUM = 0xF8, /**< deprecated */
+    CCN_MARKER_BLKID   = 0xFB, /**< nonconsecutive block ids */
+    CCN_MARKER_VERSION = 0xFD  /**< timestamp-based versioning */
 };
 
 /*
