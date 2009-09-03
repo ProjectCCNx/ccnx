db80ff90bce2366df1490f0df096623a5c70ba4a
Diana Smetters <smetters@parc.com>
2009-09-03 16:11:47 -0700

    Skeleton test refs #686

---
+++ javasrc/src/org/ccnx/ccn/test/impl/support/CCNTimeTest.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package org.ccnx.ccn.test.impl.support;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author smetters
+ *
+ */
+public class CCNTimeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#setTime(long)}.
+	 */
+	@Test
+	public void testSetTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#setNanos(int)}.
+	 */
+	@Test
+	public void testSetNanos() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#CCNTime(long)}.
+	 */
+	@Test
+	public void testCCNTimeLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#CCNTime(java.sql.Timestamp)}.
+	 */
+	@Test
+	public void testCCNTimeTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#CCNTime(java.util.Date)}.
+	 */
+	@Test
+	public void testCCNTimeDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#CCNTime(byte[])}.
+	 */
+	@Test
+	public void testCCNTimeByteArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#CCNTime()}.
+	 */
+	@Test
+	public void testCCNTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#fromBinaryTimeAsLong(long)}.
+	 */
+	@Test
+	public void testFromBinaryTimeAsLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#toBinaryTime()}.
+	 */
+	@Test
+	public void testToBinaryTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#toBinaryTimeAsLong()}.
+	 */
+	@Test
+	public void testToBinaryTimeAsLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#equals(java.sql.Timestamp)}.
+	 */
+	@Test
+	public void testEqualsTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#compareTo(java.util.Date)}.
+	 */
+	@Test
+	public void testCompareToDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#compareTo(java.sql.Timestamp)}.
+	 */
+	@Test
+	public void testCompareToTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#before(java.sql.Timestamp)}.
+	 */
+	@Test
+	public void testBeforeTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#after(java.sql.Timestamp)}.
+	 */
+	@Test
+	public void testAfterTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#before(java.util.Date)}.
+	 */
+	@Test
+	public void testBeforeDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#after(java.util.Date)}.
+	 */
+	@Test
+	public void testAfterDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.ccnx.ccn.impl.support.CCNTime#now()}.
+	 */
+	@Test
+	public void testNow() {
+		fail("Not yet implemented");
+	}
+
+}
