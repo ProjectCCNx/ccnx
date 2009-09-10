6867e13df5cea5f8207444cda6c7823038518c1e
Michael Plass <plass@parc.com>
2009-09-10 09:48:35 -0700

    Add GPLv2 to ccndsmoketest and the various test programs in csrc/lib. refs #712

--- csrc/lib/hashtbtest.c
+++ csrc/lib/hashtbtest.c
@@ -1,10 +1,29 @@
+/**
+ * @file hashtbtest.c
+ * Try out some hash table calls (ccn/hashtb).
+ *
+ * A CCNx program.
+ *
+ * Copyright (C) 2008, 2009 Palo Alto Research Center, Inc.
+ *
+ * This work is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation. 
+ * This work is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <ccn/hashtb.h>
 
-void
+static void
 Dump(struct hashtb *h)
 {
     struct hashtb_enumerator eee;
@@ -18,7 +37,7 @@ Dump(struct hashtb *h)
     hashtb_end(e);
 }
 
-void
+static void
 finally(struct hashtb_enumerator *e)
 {
     char *who = hashtb_get_param(e->ht, NULL);
