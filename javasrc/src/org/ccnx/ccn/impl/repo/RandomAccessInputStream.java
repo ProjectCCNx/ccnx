57cd2421984067d8c296b7d8ce6863a8be06d32f
Rebecca Braynard <rbraynar@parc.com>
2009-09-15 13:54:48 -0700

    documentation

--- javasrc/src/org/ccnx/ccn/impl/repo/RandomAccessInputStream.java
+++ javasrc/src/org/ccnx/ccn/impl/repo/RandomAccessInputStream.java
@@ -21,57 +21,73 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
 
+/**
+ * RandomAccessInputStream extends InputStream to allow random reads
+ * of the backend repository RandomAccessFile.
+ * 
+ * This class is intended for backend repository use and is not meant
+ * for general CCN stream use.
+ * 
+ * 
+ * @see InputStream
+ * @see RandomAccessFile
+ */
 public class RandomAccessInputStream extends InputStream {
 
 	protected RandomAccessFile underlying;
-	
+
+	/**
+	 * Constructor to set the backend repository file for random reads.
+	 * @param f Backend RandomAccessFile
+	 */
 	public RandomAccessInputStream(RandomAccessFile f) {
 		underlying = f;
 	}
 
+	/**
+	 * Method to implement the read() method for the abstract class InputStream.
+	 * When called, this method returns the next byte of the file.
+	 * 
+	 * @return int Next byte of the file
+	 * 
+	 * @see InputStream.read()
+	 * 
+	 * @throws IOException
+	 */
 	@Override
 	public int read() throws IOException {
 		return underlying.read();
 	}
 	
+	/**
+	 * Method to read some number of bytes into the byte[] b.  The number of bytes
+	 * read is returned as an integer. If no bytes are read, the method returns -1.
+	 * 
+	 * @param b byte[] to read data into
+	 * 
+	 * @return int Number of bytes read (-1 if no more data is available)
+	 * 
+	 * @throws IOException
+	 */
 	@Override
 	public  int read(byte[] b) throws IOException {
 		return underlying.read(b);
 	}
 	
+	
+	/**
+	 * Method to read len bytes into byte[] b starting at a specific offset.
+	 * 
+	 * @param b byte[] to read bytes into
+	 * @param off starting position for reading
+	 * @param len number of bytes to read into the byte array
+	 * 
+	 * @return int number of bytes read into the byte array
+	 * 
+	 * @throws IOException
+	 */
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
 		return underlying.read(b, off, len);
 	}
-	/*
-	@Override
-	public boolean markSupported(){
-		return true;
-	}
-	
-	@Override
-	public void mark(int readlimit){
-		synchronized(underlying){
-			try{
-				mark = readlimit;
-				position = underlying.getFilePointer();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	@Override
-	public void reset(){
-		synchronized(underlying){
-			try {
-				underlying.seek(position);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-*/
 }
