7448df96f7054fc72d141c74282ec2be1b18c459
Michael Plass <plass@parc.com>
2009-09-16 13:41:22 -0700

    GPLv2 on makefiles

--- csrc/util/dir.mk
+++ csrc/util/dir.mk
@@ -1,4 +1,16 @@
-# makefile for csrc/util directory
+# util/dir.mk
+# 
+# Part of the CCNx distribution.
+#
+# Copyright (C) 2009 Palo Alto Research Center, Inc.
+#
+# This work is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+# This work is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
 
 SCRIPTSRC = ccndstart.sh shebang
 PROGRAMS = $(INSTALLED_PROGRAMS)
