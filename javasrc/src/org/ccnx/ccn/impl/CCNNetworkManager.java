872827463fbe93cfc683e12a9f61807308b9562c
Michael Plass <plass@parc.com>
2009-10-01 11:00:48 -0700

    Replace the temporary default port number with the newly-assigned registered port number for ccnx, 9695.
    See http://www.iana.org/assignments/port-numbers
    
    refs #100014

--- javasrc/src/org/ccnx/ccn/impl/CCNNetworkManager.java
+++ javasrc/src/org/ccnx/ccn/impl/CCNNetworkManager.java
@@ -64,7 +64,7 @@ import org.ccnx.ccn.protocol.WirePacket;
  */
 public class CCNNetworkManager implements Runnable {
 	
-	public static final int DEFAULT_AGENT_PORT = 64206;
+	public static final int DEFAULT_AGENT_PORT = 9695; // ccnx registered port
 	public static final String DEFAULT_AGENT_HOST = "localhost";
 	public static final String PROP_AGENT_PORT = "ccn.agent.port";
 	public static final String PROP_AGENT_HOST = "ccn.agent.host";
