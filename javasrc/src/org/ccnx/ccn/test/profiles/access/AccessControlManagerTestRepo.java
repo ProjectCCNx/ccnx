48f462638b81fa208037072741aab8e1dda5726c
Philippe Golle <pgolle@parc.com>
2009-10-27 10:41:28 -0700

    New test for AccessControlManagerTestRepo

--- javasrc/src/org/ccnx/ccn/test/profiles/access/AccessControlManagerTestRepo.java
+++ javasrc/src/org/ccnx/ccn/test/profiles/access/AccessControlManagerTestRepo.java
@@ -1,52 +1,127 @@
 package org.ccnx.ccn.test.profiles.access;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import junit.framework.Assert;
 
 import java.util.Random;
 import java.util.ArrayList;
-import java.security.PublicKey;
 
-import junit.framework.Assert;
-
-import org.ccnx.ccn.KeyManager;
+import org.ccnx.ccn.CCNHandle;
+import org.ccnx.ccn.config.UserConfiguration;
+import org.ccnx.ccn.io.RepositoryOutputStream;
 import org.ccnx.ccn.io.content.Link;
-import org.ccnx.ccn.io.content.LinkAuthenticator;
 import org.ccnx.ccn.protocol.ContentName;
-import org.ccnx.ccn.protocol.PublisherID;
 import org.ccnx.ccn.profiles.access.AccessControlManager;
 import org.ccnx.ccn.profiles.access.ACL;
 import org.ccnx.ccn.profiles.access.ACL.ACLObject;
+import org.ccnx.ccn.test.profiles.access.TestUserData;
+
+/**
+ * This test relies on org.ccn.ccn.test.profiles.access.TestUserData to generate users
+ * for the access control test.
+ *
+ */
 
 public class AccessControlManagerTestRepo {
 
-	@Test
-	public void testSetAndGetACL() throws Exception {
+	static AccessControlManager acm;
+	static ContentName baseNode, childNode, grandchildNode;
+	static ContentName userKeyStorePrefix, userNamespace;
+	static int userCount = 3;
+	static TestUserData td;
+	static String[] friendlyNames;
+	static Link lk, lk2;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// create base node, child node and grandchild node
 		Random rand = new Random();
 		String directoryBase = "/test/AccessControlManagerTestRepo-";
-		ContentName baseNodeName = ContentName.fromNative(directoryBase + Integer.toString(rand.nextInt(10000)));
+		baseNode = ContentName.fromNative(directoryBase + Integer.toString(rand.nextInt(10000)));
+		childNode = ContentName.fromNative(baseNode, "child");
+		grandchildNode = ContentName.fromNative(childNode, "grandchild");
 		
-		// publish my identity
-		AccessControlManager acm = new AccessControlManager(baseNodeName);
-		ContentName myIdentity = ContentName.fromNative("/test/parc/users/pgolle");
-		acm.publishMyIdentity(myIdentity, null);	
-
-		// set ACL
+		// write some content in grandchild node
+//		RepositoryOutputStream ros = new RepositoryOutputStream(grandchildNode, CCNHandle.open());
+//		ros.setTimeout(5000);
+//		byte [] data = "Grandchild content".getBytes();
+//		ros.write(data, 0, data.length);
+//		ros.close();
+		
+		// create user identities with TestUserData
+		ContentName testPrefix = UserConfiguration.defaultNamespace();
+		userKeyStorePrefix = ContentName.fromNative(testPrefix, "_access_");
+		userNamespace = ContentName.fromNative(testPrefix, "home");
+		td = new TestUserData(userKeyStorePrefix, userCount, true, "password".toCharArray(), CCNHandle.open());
+		td.saveUserPK2Repo(userNamespace);
+		friendlyNames = td.friendlyNames().toArray(new String[0]);
+		Assert.assertEquals(userCount, friendlyNames.length);
+	}
+	
+	/**
+	 * Set the ACL for the base node (we make user 0 a manager of the base node).
+	 * @throws Exception
+	 */
+	@Test
+	public void testSetBaseACL() throws Exception {		
+		String user0 = friendlyNames[0];
 		ArrayList<Link> ACLcontents = new ArrayList<Link>();
-		KeyManager km = KeyManager.getKeyManager();
-		PublicKey pk = km.getDefaultPublicKey();
-		PublisherID pubID = new PublisherID(pk, true);
-		LinkAuthenticator la = new LinkAuthenticator(pubID);
-		Link lk = new Link(myIdentity, "r", la);
+		ContentName ID0 = ContentName.fromNative(userNamespace, user0);
+		lk = new Link(ID0, "rw+", null);
 		ACLcontents.add(lk);
 		ACL baseACL = new ACL(ACLcontents);
+		CCNHandle handle = td.getHandleForUser(user0);
+		acm = new AccessControlManager(baseNode, handle);
 		acm.initializeNamespace(baseACL);
-		
-		// get ACL
-		ACLObject aclo = acm.getEffectiveACLObject(baseNodeName);
-		ACL aclRetrieved = aclo.acl();
-		Link linkRetrieved = aclRetrieved.remove(0);
-		Assert.assertTrue(linkRetrieved.equals(lk));
 	}
 
+	/**
+	 * Retrieve the ACL for the base node.
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetBaseACL() throws Exception {
+		ACLObject aclo = acm.getEffectiveACLObject(baseNode);
+		ACL aclRetrieved = aclo.acl();
+		Link linkRetrieved = aclRetrieved.remove(0);
+		Assert.assertTrue(linkRetrieved.equals(lk));		
+	}
+	
+	/**
+	 * Retrieve the ACL for the grandchild node.
+	 * This ACL comes from the base node.
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetACLFromAncestor() throws Exception {
+		ACLObject aclo = acm.getEffectiveACLObject(grandchildNode);
+		ACL aclRetrieved = aclo.acl();
+		Link linkRetrieved = aclRetrieved.remove(0);
+		Assert.assertTrue(linkRetrieved.equals(lk));		
+	}
+	
+	/**
+	 * Interpose a different ACL at the child node (we make user 1 a reader)
+	 * Retrieve the ACL for the grandchild node and check that it now comes 
+	 * from the child node.
+	 * @throws Exception
+	 */
+	@Test
+	public void testSetACL() throws Exception {
+		// set interposed ACL
+		ArrayList<Link> newACLContents = new ArrayList<Link>();
+		ContentName ID1 = ContentName.fromNative(userNamespace, friendlyNames[1]);
+		Link lk2 = new Link(ID1, "r", null);
+		newACLContents.add(lk2);
+		ACL newACL = new ACL(newACLContents);
+		acm.setACL(childNode, newACL);
+		
+		// retrieve ACL at grandchild node
+		ACLObject aclo = acm.getEffectiveACLObject(grandchildNode);
+		ACL aclRetrieved = aclo.acl();
+		Link linkRetrieved = aclRetrieved.remove(0);
+		Assert.assertTrue(linkRetrieved.equals(lk2));		
+	}
 	
 }
