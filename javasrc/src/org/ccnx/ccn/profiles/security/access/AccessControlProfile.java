1a3d6777955212bfac9d0b9df3af165622306848
Diana Smetters <smetters@parc.com>
2009-11-26 12:34:41 -0800

    Pull methods up to superclass. refs #100063

--- javasrc/src/org/ccnx/ccn/profiles/security/access/AccessControlProfile.java
+++ javasrc/src/org/ccnx/ccn/profiles/security/access/AccessControlProfile.java
@@ -1,5 +1,72 @@
 package org.ccnx.ccn.profiles.security.access;
 
+import org.ccnx.ccn.profiles.CCNProfile;
+import org.ccnx.ccn.profiles.VersioningProfile;
+import org.ccnx.ccn.protocol.ContentName;
+
 public class AccessControlProfile {
+
+	public static final String ACCESS_CONTROL_MARKER = CCNProfile.MARKER + "access" + CCNProfile.MARKER;
+	public static final byte [] ACCESS_CONTROL_MARKER_BYTES = ContentName.componentParseNative(ACCESS_CONTROL_MARKER);
+	public static final String ROOT_NAME = "ROOT";
+	public static final byte [] ROOT_NAME_BYTES = ContentName.componentParseNative(ROOT_NAME);
+	public static final String DATA_KEY_NAME = "DK";
+	public static final byte [] DATA_KEY_NAME_BYTES = ContentName.componentParseNative(DATA_KEY_NAME);
+
+	/**
+	 * Returns whether the specified name contains the access control marker
+	 * @param name the name
+	 * @return
+	 */
+	public static boolean isAccessName(ContentName name) {
+		return name.contains(ACCESS_CONTROL_MARKER_BYTES);
+	}
+
+	/**
+	 * Truncates the specified name at the access control marker
+	 * @param name the name
+	 * @return the truncated name
+	 */
+	public static ContentName accessRoot(ContentName name) {
+		return name.cut(ACCESS_CONTROL_MARKER_BYTES);
+	}
+
+	/**
+	 * Return the name of the root access control policy information object,
+	 * if it is stored at node nodeName.
+	 **/
+	public static ContentName rootName(ContentName nodeName) {
+		ContentName baseName = (isAccessName(nodeName) ? accessRoot(nodeName) : nodeName);
+		ContentName aclRootName = new ContentName(baseName, ACCESS_CONTROL_MARKER_BYTES, ROOT_NAME_BYTES);
+		return aclRootName;
+	}
+
+	/**
+	 * Get the name of the data key for a given content node.
+	 * This is nodeName/_access_/DK.
+	 * @param nodeName the name of the content node
+	 * @return the name of the corresponding data key
+	 */
+	public static ContentName dataKeyName(ContentName nodeName) {
+		ContentName baseName = accessRoot(nodeName);
+		ContentName dataKeyName = new ContentName(baseName, ACCESS_CONTROL_MARKER_BYTES, DATA_KEY_NAME_BYTES);
+		return dataKeyName;
+	}
+
+	/**
+	 * Returns whether the specified name is a data key name
+	 * @param name the name
+	 * @return
+	 */
+	public static boolean isDataKeyName(ContentName name) {
+		if (!isAccessName(name) || VersioningProfile.hasTerminalVersion(name)) {
+			return false;
+		}
+		int versionComponent = VersioningProfile.findLastVersionComponent(name);
+		if (name.stringComponent(versionComponent - 1).equals(DATA_KEY_NAME)) {
+			return true;
+		}
+		return false;
+	}
 
 }
--- javasrc/src/org/ccnx/ccn/profiles/security/access/group/GroupAccessControlProfile.java
+++ javasrc/src/org/ccnx/ccn/profiles/security/access/group/GroupAccessControlProfile.java
@@ -38,13 +38,6 @@ import org.ccnx.ccn.protocol.ContentName;
  */
 public class GroupAccessControlProfile extends AccessControlProfile implements CCNProfile {
 	
-	// Is it better to use .access or _access_? The former might be used by "real" data more often...
-	public static final String ACCESS_CONTROL_MARKER = CCNProfile.MARKER + "access" + CCNProfile.MARKER;
-	public static final byte [] ACCESS_CONTROL_MARKER_BYTES = ContentName.componentParseNative(ACCESS_CONTROL_MARKER);
-	
-    public static final String ROOT_NAME = "ROOT";
-	public static final byte [] ROOT_NAME_BYTES = ContentName.componentParseNative(ROOT_NAME);
-
 	// These may eventually want to move somewhere more general
 	public static final String GROUP_PREFIX = "Groups";
 	public static final byte [] GROUP_PREFIX_BYTES = ContentName.componentParseNative(GROUP_PREFIX);
@@ -60,9 +53,6 @@ public class GroupAccessControlProfile extends AccessControlProfile implements C
 	public static final byte [] ACL_NAME_BYTES = ContentName.componentParseNative(ACL_NAME);
 	public static final String NODE_KEY_NAME = "NK";
 	public static final byte [] NODE_KEY_NAME_BYTES = ContentName.componentParseNative(NODE_KEY_NAME);	
-	public static final String DATA_KEY_NAME = "DK";
-	public static final byte [] DATA_KEY_NAME_BYTES = ContentName.componentParseNative(DATA_KEY_NAME);
-
 	// These two must be the same length
 	public static final byte [] PRINCIPAL_PREFIX = ContentName.componentParseNative("p" + CCNProfile.COMPONENT_SEPARATOR_STRING);
 	public static final byte [] GROUP_PRINCIPAL_PREFIX = ContentName.componentParseNative("g" + CCNProfile.COMPONENT_SEPARATOR_STRING);
@@ -93,34 +83,6 @@ public class GroupAccessControlProfile extends AccessControlProfile implements C
 		public CCNTime versionTimestamp() { return _versionTimestamp; }
 	}
 	
-	/**
-	 * Returns whether the specified name contains the access control marker
-	 * @param name the name
-	 * @return
-	 */
-	public static boolean isAccessName(ContentName name) {
-		return name.contains(ACCESS_CONTROL_MARKER_BYTES);
-	}
-
-	/**
-	 * Truncates the specified name at the access control marker
-	 * @param name the name
-	 * @return the truncated name
-	 */
-	public static ContentName accessRoot(ContentName name) {
-		return name.cut(ACCESS_CONTROL_MARKER_BYTES);
-	}
-	
-    /**
-     * Return the name of the root access control policy information object,
-     * if it is stored at node nodeName.
-     **/
-	public static ContentName rootName(ContentName nodeName) {
-		ContentName baseName = (isAccessName(nodeName) ? accessRoot(nodeName) : nodeName);
-		ContentName aclRootName = new ContentName(baseName, ACCESS_CONTROL_MARKER_BYTES, ROOT_NAME_BYTES);
-		return aclRootName;
-	}
-
 	/**
 	 * Returns whether the specified name is the name of a node key
 	 * @param name the name
@@ -159,34 +121,6 @@ public class GroupAccessControlProfile extends AccessControlProfile implements C
 		ContentName baseName = accessRoot(nodeName);
 		ContentName aclName = new ContentName(baseName, ACCESS_CONTROL_MARKER_BYTES, ACL_NAME_BYTES);
 		return aclName;
-	}
-	
-	/**
-	 * Get the name of the data key for a given content node.
-	 * This is nodeName/_access_/DK.
-	 * @param nodeName the name of the content node
-	 * @return the name of the corresponding data key
-	 */	
-	public static ContentName dataKeyName(ContentName nodeName) {
-		ContentName baseName = accessRoot(nodeName);
-		ContentName dataKeyName = new ContentName(baseName, ACCESS_CONTROL_MARKER_BYTES, DATA_KEY_NAME_BYTES);
-		return dataKeyName;
-	}
-	
-	/**
-	 * Returns whether the specified name is a data key name
-	 * @param name the name
-	 * @return
-	 */
-	public static boolean isDataKeyName(ContentName name) {
-		if (!isAccessName(name) || VersioningProfile.hasTerminalVersion(name)) {
-			return false;
-		}
-		int versionComponent = VersioningProfile.findLastVersionComponent(name);
-		if (name.stringComponent(versionComponent - 1).equals(DATA_KEY_NAME)) {
-			return true;
-		}
-		return false;
 	}
 	
 	/**
