e38b10fa5c0dc3cba8cd0b70f1928c8e768bcf9c
Diana Smetters <smetters@parc.com>
2009-12-12 10:21:07 -0800

    Update expanded crypto tests to new objct API.

--- apps/examples/ExpandedCryptoTests/src/org/ccnx/ccn/test/io/content/ExpandedPublicKeyObjectTestRepo.java
+++ apps/examples/ExpandedCryptoTests/src/org/ccnx/ccn/test/io/content/ExpandedPublicKeyObjectTestRepo.java
@@ -33,6 +33,7 @@ import org.bouncycastle.jce.spec.ElGamalParameterSpec;
 import org.ccnx.ccn.CCNHandle;
 import org.ccnx.ccn.KeyManager;
 import org.ccnx.ccn.config.ConfigurationException;
+import org.ccnx.ccn.impl.CCNFlowControl.SaveType;
 import org.ccnx.ccn.impl.support.Log;
 import org.ccnx.ccn.io.content.PublicKeyObject;
 import org.ccnx.ccn.profiles.VersionMissingException;
@@ -188,8 +189,7 @@ public class ExpandedPublicKeyObjectTestRepo {
 		if (null == flosser) {
 			flosser = new Flosser();
 		} 
-		PublicKeyObject pko = new PublicKeyObject(keyName, key, handle);
-		pko.setRawSave();
+		PublicKeyObject pko = new PublicKeyObject(keyName, key, SaveType.RAW, handle);
 		flosser.handleNamespace(keyName);
 		pko.save();
 		Log.info("Saved " + pko.getVersionedName() + ", now trying to read.");
@@ -206,7 +206,7 @@ public class ExpandedPublicKeyObjectTestRepo {
 		}
 		if (null != optional2ndKey) {
 			Log.info("Reading and writing second raw key " + keyName + " key 1: " + key.getAlgorithm() + " key 2: " + ((null == optional2ndKey) ? "null" : optional2ndKey.getAlgorithm()));
-			pkoread.setRawSave();
+			pkoread.setupSave(SaveType.RAW);
 			pkoread.save(optional2ndKey);
 			Assert.assertTrue(VersioningProfile.isLaterVersionOf(pkoread.getVersionedName(), pko.getVersionedName()));
 			pko.update();
@@ -231,7 +231,7 @@ public class ExpandedPublicKeyObjectTestRepo {
 	public void testRepoKeyReadWrite(ContentName keyName, PublicKey key, PublicKey optional2ndKey) throws ConfigurationException, IOException, VersionMissingException {
 
 		Log.info("Reading and writing key to repo " + keyName + " key 1: " + key.getAlgorithm() + " key 2: " + ((null == optional2ndKey) ? "null" : optional2ndKey.getAlgorithm()));
-		PublicKeyObject pko = new PublicKeyObject(keyName, key, handle);
+		PublicKeyObject pko = new PublicKeyObject(keyName, key, SaveType.REPOSITORY, handle);
 		pko.save();
 		Assert.assertTrue(VersioningProfile.hasTerminalVersion(pko.getVersionedName()));
 		Log.info("Saved " + pko.getVersionedName() + " to repo, now trying to read.");
@@ -248,6 +248,7 @@ public class ExpandedPublicKeyObjectTestRepo {
 		}
 		if (null != optional2ndKey) {
 			Log.info("Reading and writing second key to repo " + keyName + " key 1: " + key.getAlgorithm() + " key 2: " + ((null == optional2ndKey) ? "null" : optional2ndKey.getAlgorithm()));
+			pkoread.setupSave(SaveType.REPOSITORY);
 			pkoread.save(optional2ndKey);
 			Assert.assertTrue(VersioningProfile.isLaterVersionOf(pkoread.getVersionedName(), pko.getVersionedName()));
 			pko.update();
