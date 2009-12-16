8a9b98143d1787418ea6bdfab32ea7ed052f6d08
Tim Diebert <diebert@parc.com>
2009-12-16 13:20:53 -0800

    general code cleanup.  Removed code that was commented out

--- javasrc/src/org/ccnx/ccn/profiles/ccnd/CCNDaemonHandle.java
+++ javasrc/src/org/ccnx/ccn/profiles/ccnd/CCNDaemonHandle.java
@@ -18,10 +18,6 @@
 package	org.ccnx.ccn.profiles.ccnd;
 
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SignatureException;
 
 import org.ccnx.ccn.CCNHandle;
 import org.ccnx.ccn.ContentVerifier;
@@ -46,8 +42,7 @@ public class CCNDaemonHandle {
 	
 	protected CCNNetworkManager _manager;
 	
-	public CCNDaemonHandle() {
-	}
+	public CCNDaemonHandle() { }
 	
 	public CCNDaemonHandle(CCNNetworkManager manager)  throws CCNDaemonException {
 		_manager = manager;
@@ -63,90 +58,27 @@ public class CCNDaemonHandle {
 		return ContentName.componentPrintURI(digested);
 	}
 	
-	public byte[] getBinaryEncoding(GenericXMLEncodable encodeMe) {
-		// Do setup. Binary codec doesn't write a preamble or anything.
-		// If allow to pick, text encoder would sometimes write random stuff...
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		XMLEncoder encoder = XMLCodecFactory.getEncoder(BinaryXMLCodec.CODEC_NAME);
-//		try {
-//			encoder.beginEncoding(baos);
-//			encode(encoder);
-//			encoder.endEncoding();	
-//		} catch (ContentEncodingException e) {
-//			String reason = e.getMessage();
-//			Log.fine("Unexpected error encoding allocated FaceInstance.  reason: " + reason + "\n");
-//			Log.warningStackTrace(e);
-//			throw new IllegalArgumentException("Unexpected error encoding allocated FaceInstance.  reason: " + reason);
-//		}
-//		return baos.toByteArray();
-		
-		
-		byte[] contentOutBits;
+	protected byte[] sendIt(ContentName interestNamePrefix, GenericXMLEncodable encodeMe) throws CCNDaemonException {
+		byte[] encoded;
 		try {
-			contentOutBits = encodeMe.encode(BinaryXMLCodec.CODEC_NAME);
+			encoded = encodeMe.encode(BinaryXMLCodec.CODEC_NAME);
 		} catch (ContentEncodingException e) {
 			String reason = e.getMessage();
-			Log.fine("Unexpected error encoding allocated FaceInstance.  reason: " + reason + "\n");
-			Log.warningStackTrace(e);
-			throw new IllegalArgumentException("Unexpected error encoding allocated FaceInstance.  reason: " + reason);
+			Log.info("Unexpected error encoding encodeMe parameter.  reason: " + e.getMessage());
+			throw new IllegalArgumentException("Unexpected error encoding encodeMe parameter.  reason: " + reason);
 		}
-		return contentOutBits;
-
-	}
-	
-	protected byte[] sendIt(ContentName interestNamePrefix, GenericXMLEncodable encodeMe) throws CCNDaemonException {
-		byte[] out = getBinaryEncoding(encodeMe);
-		return sendIt(interestNamePrefix, out);
-	}
-	
-	protected byte[] sendIt(ContentName interestNamePrefix, byte[] payloadIn) throws CCNDaemonException {
-		ContentObject contentOut = null;
 		KeyManager keyManager = _manager.getKeyManager();
-		
-// 		PublicKey publicKey = km.getDefaultPublicKey();
-//		KeyLocator kl = new KeyLocator(km.getDefaultPublicKey());
-//		PublisherPublicKeyDigest publicKeyDigest = km.getDefaultKeyID();
-//		PrivateKey privateKey = km.getDefaultSigningKey();
-//
-//		/*
-//		 * We need to build a ContentObject that contains the faceBits as the content, a KeyLocator
-//		 * and SignedInfo.  First get our KeyManager from the handle.  From the KeyManager, get our 
-//		 * PublicKey.  From there, we create a KeyLocator. From there we make a SignedInfo. We then
-//		 * make a content object that has a null name.
-//		 */
-//		SignedInfo signedInfo = new SignedInfo(publicKeyDigest, SignedInfo.ContentType.DATA, kl, null, /* finalBlockID */ null);
-//		ContentName nullName = new ContentName();
-//		try {
-//			contentOut = new ContentObject(nullName, signedInfo, payloadIn, privateKey);
-//		} catch (InvalidKeyException e1) {
-//			String reason = e1.getMessage();
-//			String msg = ("Unexpected InvalidKeyException in call creating ContentObject reason: " + reason);
-//			Log.fine(msg);
-//			Log.warningStackTrace(e1);
-//			throw new CCNDaemonException(msg);
-//		} catch (SignatureException e1) {
-//			String reason = e1.getMessage();
-//			String msg = ("Unexpected SignatureException in call creating ContentObject reason: " + reason);
-//			Log.fine(msg);
-//			Log.warningStackTrace(e1);
-//			throw new CCNDaemonException(msg);
-//		}
-//		Log.info("Original CO: {0}", contentOut);
-		
-		contentOut = ContentObject.buildContentObject(new ContentName(), SignedInfo.ContentType.DATA, payloadIn, 
+		ContentObject contentOut = ContentObject.buildContentObject(new ContentName(), SignedInfo.ContentType.DATA, 
+														encoded, 
 														keyManager.getDefaultKeyID(), 
 														new KeyLocator(keyManager.getDefaultPublicKey()), keyManager, 
 														/* finalBlockID */ null);
-		Log.finest("sendIt  CO for payLoadIn: {0}", contentOut);
-
 		byte[] contentOutBits;
 		try {
 			contentOutBits = contentOut.encode(BinaryXMLCodec.CODEC_NAME);
 		} catch (ContentEncodingException e) {
-			String reason = e.getMessage();
-			String msg = ("Unexpected ContentEncodingException, reason: " + reason);
-			Log.fine(msg);
-			Log.warningStackTrace(e);
+			String msg = ("Unexpected ContentEncodingException, reason: " + e.getMessage());
+			Log.info(msg);
 			throw new CCNDaemonException(msg);
 		}
 		
@@ -162,27 +94,21 @@ public class CCNDaemonHandle {
 		try {
 			contentIn = _manager.get(interested, 1000);
 		} catch (IOException e) {
-			String reason = e.getMessage();
-			String msg = ("Unexpected IOException in call getting FaceInstance return value, reason: " + reason);
-			Log.fine(msg);
-			Log.warningStackTrace(e);
+			String msg = ("Unexpected IOException in call getting FaceInstance return value, reason: " + e.getMessage());
+			Log.info(msg);
 			throw new CCNDaemonException(msg);
 		} catch (InterruptedException e) {
-			String reason = e.getMessage();
-			String msg = ("Unexpected InterruptedException in call getting FaceInstance return value, reason: " + reason);
-			Log.fine(msg);
-			Log.warningStackTrace(e);
+			String msg = ("Unexpected InterruptedException in call getting FaceInstance return value, reason: " + e.getMessage());
+			Log.info(msg);
 			throw new CCNDaemonException(msg);
 		}
 		if (null == contentIn) {
 			String msg = ("Fetch of content from face or prefix registration call failed due to timeout.");
-			Log.fine(msg);
+			Log.info(msg);
 			throw new CCNDaemonException(msg);
 		}
-
 		
 		PublisherPublicKeyDigest sentID = contentIn.signedInfo().getPublisherKeyID();
-		// TODO: This needs to be fixed once the KeyRepository is fixed to provide a KeyManager
 		ContentVerifier verifyer = new ContentObject.SimpleVerifier(sentID, _manager.getKeyManager());
 		if (!verifyer.verify(contentIn)) {
 			String msg = ("CCNDIdGetter: Fetch of content reply from ping failed to verify.");
