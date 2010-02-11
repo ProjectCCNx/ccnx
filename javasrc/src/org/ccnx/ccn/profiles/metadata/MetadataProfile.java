3b6c781052bf900f1c1d1e406cda6c77116c85e5
Paul Rasmussen <rasmusse@parc.com>
2010-02-11 12:20:32 -0800

    Some initial infrastructure for thumbnails

--- javasrc/src/org/ccnx/ccn/profiles/metadata/MetadataProfile.java
+++ javasrc/src/org/ccnx/ccn/profiles/metadata/MetadataProfile.java
@@ -17,8 +17,15 @@
 
 package org.ccnx.ccn.profiles.metadata;
 
+import java.io.IOException;
+
+import org.ccnx.ccn.CCNHandle;
+import org.ccnx.ccn.ContentVerifier;
 import org.ccnx.ccn.profiles.CCNProfile;
+import org.ccnx.ccn.profiles.VersioningProfile;
 import org.ccnx.ccn.protocol.ContentName;
+import org.ccnx.ccn.protocol.ContentObject;
+import org.ccnx.ccn.protocol.PublisherPublicKeyDigest;
 
 public class MetadataProfile implements CCNProfile {
 
@@ -26,5 +33,12 @@ public class MetadataProfile implements CCNProfile {
 	
 	public static ContentName metadataName(ContentName baseName) {
 		return new ContentName(baseName, METADATA_MARKER);
+	}
+	
+	public static ContentName getLatestVersion(ContentName baseName, byte[] metaName, PublisherPublicKeyDigest publisher, 
+			 long timeout, ContentVerifier verifier, CCNHandle handle) throws IOException {
+		ContentObject baseVersion = VersioningProfile.getLatestVersion(baseName, publisher, timeout, verifier, handle);
+		ContentObject meta = VersioningProfile.getLatestVersion(new ContentName(baseVersion.name(), METADATA_MARKER, metaName), publisher, timeout, verifier, handle);
+		return meta.name();
 	}
 }
---
+++ javasrc/src/org/ccnx/ccn/profiles/metadata/ThumbnailProfile.java
@@ -0,0 +1,29 @@
+/**
+ * Part of the CCNx Java Library.
+ *
+ * Copyright (C) 2008, 2009 Palo Alto Research Center, Inc.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.ccnx.ccn.profiles.metadata;
+
+import org.ccnx.ccn.protocol.ContentName;
+import org.ccnx.ccn.profiles.CCNProfile;
+
+public class ThumbnailProfile implements CCNProfile {
+	public static final byte [] THUMBNAIL_MARKER = ContentName.componentParseNative(MARKER + "thumbnail" + MARKER);
+	
+	public static ContentName metadataName(ContentName baseName) {
+		return new ContentName(MetadataProfile.metadataName(baseName), THUMBNAIL_MARKER);
+	}
+}
