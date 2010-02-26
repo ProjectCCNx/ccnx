d8aea84f831f89e05acae686d9527b7facc4208f
Diana Smetters <smetters@parc.com>
2010-02-26 10:50:59 -0800

    Remove pre-check before publishing -- we're not pushing the data, we're just making it available. If someone else has it, likely no one will come to us. (Though if we do respond, it'll get to our ccnd cache, and likely go out in the next round, which might not be optimal...) If we do pre-check, we just don't want to block.
    Remove extra variable

--- javasrc/src/org/ccnx/ccn/impl/security/keys/KeyServer.java
+++ javasrc/src/org/ccnx/ccn/impl/security/keys/KeyServer.java
@@ -21,16 +21,11 @@ import java.security.PublicKey;
 
 import org.ccnx.ccn.CCNHandle;
 import org.ccnx.ccn.KeyManager;
-import org.ccnx.ccn.config.SystemConfiguration;
 import org.ccnx.ccn.impl.CCNFlowServer;
-import org.ccnx.ccn.impl.support.Log;
-import org.ccnx.ccn.io.CCNReader;
 import org.ccnx.ccn.io.content.PublicKeyObject;
 import org.ccnx.ccn.protocol.ContentName;
-import org.ccnx.ccn.protocol.ContentObject;
 import org.ccnx.ccn.protocol.KeyLocator;
 import org.ccnx.ccn.protocol.PublisherPublicKeyDigest;
-import org.ccnx.ccn.protocol.SignedInfo.ContentType;
 
 /**
  * This class publishes our keys, only if someone asks for them. It doesn't make its own
@@ -39,7 +34,6 @@ import org.ccnx.ccn.protocol.SignedInfo.ContentType;
 public class KeyServer {
 
 	protected CCNHandle _handle = null;
-	protected boolean _ourHandle = false;
 	protected CCNFlowServer _keyServer = null;
 
 	/**
@@ -68,16 +62,15 @@ public class KeyServer {
 	}
 
 	/**
-	 * Publish a signed record for this key if one doesn't exist.
-	 * (if it does exist, pulls it at least to our ccnd, and optionally
-	 * makes it available). (TODO: decide what to do if it's published by someone
-	 * else... need another option for that.) By default (if both saveType and flowController)
-	 * are null, will publish to the internal key server. If a flow controller is specified
-	 * (e.g. the internal key server), will publish using it, and will ignore any saveType set.
-	 * Otherwise will use default network object publishing behavior based on the saveType.
-	 * Dovetail it this way in order to have only one place where defaulting of signing
-	 * information is done, to avoid ending up having different behaviors depending on the
-	 * path by which you publish a key.
+	 * Start serving signed records for this key. 
+	 * We used to check to see if this key was already on the network before publishing
+	 * it. That was silly -- the interest mechanism would do this for us. Much better way
+	 * to handle things is to just listen for interests for this key, and only publish it
+	 * if someone asks (we really want to be the server of last resort, so we should answer
+	 * late, but don't know how to do that). So we could make an interest handler and then
+	 * publish the key on first interest (and assume it's cached thereafter?). If the cost
+	 * of signing is negligable, we can be cheezier. Just publish to our key server, and
+	 * if someone comes looking, they'll get it. If not....
 	 * 
 	 * @param keyName the key's content name. Will add a version when saving if it doesn't
 	 * 	have one already. If it does have a version, will use that one (see below for effect
@@ -91,35 +84,18 @@ public class KeyServer {
 	 * 	If not specified, we look for the default locator for the signing key. If there is none,
 	 * 	and we are signing with the same key we are publishing, we build a
 	 * 	self-referential key locator, using the name passed in (versioned or not).
-	 * @param saveType -- if we don't want to hand in a special-purpose flow controller, set saveType to RAW
-	 *   or REPO to get standard publishing behavior.
-	 * @param flowController flow controller to use. If non-null, saveType is ignored.
-	 * @return the published information about this key, whether we published it or someone else had
+	 * @return the published information about this key
 	 * @throws IOException
 	 */
 	public PublicKeyObject serveKey(ContentName keyName, PublicKey keyToPublish,
-						   			   PublisherPublicKeyDigest signingKeyID, 
-						   			   KeyLocator signingKeyLocator) 
-
-	throws IOException {
-
-		PublisherPublicKeyDigest keyDigest = new PublisherPublicKeyDigest(keyToPublish);
+						   			PublisherPublicKeyDigest signingKeyID, 
+						   			KeyLocator signingKeyLocator) throws IOException {
 		
 		// Set up key server if it hasn't been set up already
 		initialize();
-
-		// See if this key is on the network
-		ContentObject keySegment = CCNReader.isContentAvailable(keyName, ContentType.KEY, 
-				keyDigest.digest(), null, SystemConfiguration.getDefaultTimeout(), _handle);
-		if (null != keySegment) {
-			Log.info("Key {0} is already available as {1}, not re-publishing.", keyName, keySegment.name());
-			// A little dangerous, handing up our handle.
-			return new PublicKeyObject(keySegment, _handle);
-		}
 		
-		// Otherwise it's not published, so make an object to write it
-		// with. We've already tried to pull it, so don't try here. Will
-		// set publisher info below.
+		// Use the key publishing framework in KeyManager to format this key, but use our _keyServer
+		// as the flow controller to serve it.
 		return KeyManager.publishKey(keyName, keyToPublish, signingKeyID, signingKeyLocator, 
 				_keyServer, null, null, _handle.keyManager());
 	}
