cd8fd06132d6ab3bab31f2394213562877dc901f
Diana Smetters <smetters@parc.com>
2010-03-09 16:51:13 -0800

    Pull access control manager stuff out, basic policy tracking only.

--- javasrc/src/org/ccnx/ccn/profiles/namespace/NamespaceManager.java
+++ javasrc/src/org/ccnx/ccn/profiles/namespace/NamespaceManager.java
@@ -19,72 +19,44 @@ package org.ccnx.ccn.profiles.namespace;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 import org.ccnx.ccn.CCNHandle;
-import org.ccnx.ccn.config.ConfigurationException;
 import org.ccnx.ccn.config.SystemConfiguration;
 import org.ccnx.ccn.impl.support.Log;
-import org.ccnx.ccn.io.ErrorStateException;
-import org.ccnx.ccn.io.content.ContentGoneException;
-import org.ccnx.ccn.io.content.ContentNotReadyException;
 import org.ccnx.ccn.profiles.VersioningProfile;
 import org.ccnx.ccn.profiles.search.Pathfinder;
 import org.ccnx.ccn.profiles.search.Pathfinder.SearchResults;
-import org.ccnx.ccn.profiles.security.access.AccessControlManager;
-import org.ccnx.ccn.profiles.security.access.AccessControlProfile;
-import org.ccnx.ccn.profiles.security.access.AccessControlPolicyMarker.RootObject;
-import org.ccnx.ccn.profiles.security.access.group.GroupAccessControlManager;
 import org.ccnx.ccn.protocol.ContentName;
-import org.ccnx.ccn.protocol.MalformedContentNameStringException;
 
 /**
- *
+ * Maintains a list of existing policy marker prefixes. 
  **/
 public class NamespaceManager {
 	
-	protected static Set<AccessControlManager> _acmList = new HashSet<AccessControlManager>(); 
 	protected static Set<ContentName> _searchedPathCache = new HashSet<ContentName>();
+	protected static Set<ContentName> _policyControlledNamespaces = new HashSet<ContentName>();
 	
-	
-	public static AccessControlManager createAccessControlManager(RootObject policyInformation, CCNHandle handle) throws ContentNotReadyException, ContentGoneException, ErrorStateException, InstantiationException, IllegalAccessException, ConfigurationException, IOException {
-		Class<? extends AccessControlManager> acmClazz = null;
-		synchronized(NamespaceManager.class) {
-			acmClazz = _accessControlManagerTypes.get(policyInformation.root().profileName());
-		}
-		if (null != acmClazz) {
-			AccessControlManager acm = (AccessControlManager)acmClazz.newInstance();
-			acm.initialize(policyInformation, handle);
-			return acm;
-		}
-		return null;
-	}
-
 	/**
-	 * Find an ACM object that covers operations on a specific name. 
-	 * If none exists in memory then this searches up the name tree 
-	 * looking for the root of a namespace under access control,
-	 * and if it finds one creates an ACM. If none is found null is returned.
-	 * @param name
-	 * @param handle
-	 * @return null if namespace is not under access control, or an ACM to perform 
-	 * 	operations on the name if it is.
-	 * @throws IOException
-	 * @throws ConfigurationException 
+	 * Find the closest policy controlled namespace that cover operations on a specific name.
+	 * If none exists in memory than this searches up the name tree.
+	 * @throws IOException 
 	 */
-	public static AccessControlManager findACM(ContentName name, CCNHandle handle)  throws IOException, ConfigurationException {
-		// See if we already have an AccessControlManager covering this namespace
-		for (AccessControlManager acm : _acmList) {
-			if (acm.inProtectedNamespace(name)) {
-				Log.info("Found cached access control manager rooted at {0} protecting {1}", acm.getNamespaceRoot(), name);
-				return acm;
+	public static ContentName findPolicyControlledNamespace(ContentName controlledName, CCNHandle handle) throws IOException {
+		
+		// See if we already have a prefix controlling this name.
+		for (ContentName prefix : _policyControlledNamespaces) {
+			if (inProtectedNamespace(prefix, controlledName)) {
+				// Doesn't handle nesting... want to find the longest match that matches this name,
+				// while marking ones we don't have to search again. Works for now, might need to make
+				// this more sophisticated if applications warrant.
+				Log.info("Found policy control prefix {0} protecting {1}", prefix, controlledName);
+				return prefix;
 			}
 		}
-		// No ACM exists for this name - now look to see if we can find an ACL down the path to create one...
-		ContentName searchName = VersioningProfile.cutTerminalVersion(name).first();
-		Log.info("No cached access control manager found, searching for root object for {0}. Removed terminal version, checking path {1}", name, searchName);
+		// No known prefix exists for this name - now look to see if we can find a prefix up the path to create one...
+		ContentName searchName = VersioningProfile.cutTerminalVersion(controlledName).first();
+		Log.info("No cached policy control prefix found, searching for root object for {0}. Removed terminal version, checking path {1}", controlledName, searchName);
 		
 		// Have a cache of searched paths, so we don't re-search.
 		if (cacheContainsPath(searchName)) {
@@ -93,7 +65,8 @@ public class NamespaceManager {
 		}
 
 		// Search up a path (towards the root) for an Access Control root marker
-		Pathfinder pathfinder = new Pathfinder(searchName, null, AccessControlProfile.rootPostfix(), true, false, 
+		Pathfinder pathfinder = new Pathfinder(searchName, null, 
+											  NamespaceProfile.policyPostfix(), true, false, 
 											  SystemConfiguration.SHORT_TIMEOUT, 
 											  _searchedPathCache,
 											  handle);
@@ -101,41 +74,12 @@ public class NamespaceManager {
 		if (null != results.getExcluded()) {
 			_searchedPathCache.addAll(results.getExcluded());
 		}
-		
 		if (null != results.getResult()) {
-			// does this seek?
-			Log.info("Got a segment of an object, is it the first segment of the right object: {0}", results.getResult().name());
-			RootObject ro = new RootObject(results.getResult(), handle);
-			AccessControlManager acm;
-			try {
-				acm = NamespaceManager.createAccessControlManager(ro, handle);
-			} catch (InstantiationException e) {
-				Log.severe("InstantiationException attempting to create access control manager: " + e.getMessage());
-				Log.warningStackTrace(e);
-				throw new ConfigurationException("InstantiationException attempting to create access control manager: " + e.getMessage(), e);
-			} catch (IllegalAccessException e) {
-				Log.severe("IllegalAccessException attempting to create access control manager: " + e.getMessage());
-				Log.warningStackTrace(e);
-				throw new ConfigurationException("IllegalAccessException attempting to create access control manager: " + e.getMessage(), e);
-			}
-			registerACM(acm);
-			return acm;
+			ContentName policyPrefix = results.getResult().name().cut(searchName.count() + NamespaceProfile.policyPostfix().count());
+			_policyControlledNamespaces.add(policyPrefix);
+			return policyPrefix;
 		}
-
-		Log.info("No ac ROOT found on path {0}", searchName);
 		return null;
-	}
-
-	/**
-	 * A way to add an access control manager to the search set without namespace support.
-	 * @param acm
-	 */
-	public static void registerACM(AccessControlManager acm) {
-		if (null != acm) {
-			synchronized(_acmList) {
-				_acmList.add(acm);
-			}
-		}
 	}
 	
 	public synchronized static void clearSearchedPathCache() { _searchedPathCache.clear(); }
@@ -162,4 +106,9 @@ public class NamespaceManager {
 		}
 		return false;
 	}
+	
+	public static boolean inProtectedNamespace(ContentName namespace, ContentName content) {
+		return namespace.isPrefixOf(content);
+	}
+
 }
