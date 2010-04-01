bd76c47af6cebe28e00bbee71adb7d9d48d0baad
Diana Smetters <smetters@parc.com>
2010-04-01 14:15:49 -0700

    Tool to update headers from old to new names. Untested.

--- javasrc/src/org/ccnx/ccn/utils/updateheader.java
+++ javasrc/src/org/ccnx/ccn/utils/updateheader.java
@@ -20,16 +20,20 @@ import java.io.IOException;
 import java.util.logging.Level;
 
 import org.ccnx.ccn.CCNHandle;
+import org.ccnx.ccn.config.SystemConfiguration;
+import org.ccnx.ccn.impl.CCNFlowControl.SaveType;
 import org.ccnx.ccn.impl.support.Log;
 import org.ccnx.ccn.io.content.Header.HeaderObject;
+import org.ccnx.ccn.profiles.VersioningProfile;
 import org.ccnx.ccn.profiles.metadata.MetadataProfile;
 import org.ccnx.ccn.protocol.ContentName;
+import org.ccnx.ccn.protocol.ContentObject;
 import org.ccnx.ccn.protocol.MalformedContentNameStringException;
 
 public class updateheader {
 
 	public void usage() {
-		System.out.println("usage: updateheader [-log level] <ccnname> [<ccnname>*>]");
+		System.out.println("usage: updateheader [-log level] <ccnname> [<ccnname>*>]\n Assumes content is in a repository.");
 		System.exit(1);
 	}
 
@@ -38,13 +42,45 @@ public class updateheader {
 		ContentName fileName = ContentName.parse(ccnxName);
 		
 		// Want a versioned name, either this version or latest version
+		
 		ContentName fileVersionedName = null;
+		if (VersioningProfile.hasTerminalVersion(fileName)) {
+			fileVersionedName = fileName;
+		} else {
+			ContentObject fileObject = VersioningProfile.getLatestVersion(fileName, null, SystemConfiguration.getDefaultTimeout(), null, handle);
+			if (null == fileObject) {
+				System.out.println("Cannot find file " + fileName + " to update. Skipping.");
+				return;
+			}
+			fileVersionedName = fileObject.name().subname(0, fileName.count() + 1);
+		}
+		
+		// Should only update content in repositories -- makes no sense to bother with other
+		// content, really. 
 		
 		HeaderObject newHeader = new HeaderObject(MetadataProfile.headerName(fileVersionedName), null, handle);
 		newHeader.updateInBackground();
 		
 		HeaderObject oldHeader = new HeaderObject(MetadataProfile.oldHeaderName(fileVersionedName), null, handle);
 		oldHeader.updateInBackground();
+		
+		oldHeader.waitForData(SystemConfiguration.getDefaultTimeout());
+		
+		if (!oldHeader.available()) {
+			System.out.println("No old-style header found. Skipping " + ccnxName);
+			oldHeader.cancelInterest();
+			newHeader.cancelInterest();
+			return;
+		}
+		
+		if (newHeader.available()) {
+			System.out.println("Already have a new header: " + newHeader.getVersionedName() + ", skipping file " + ccnxName);
+		}
+		newHeader.cancelInterest();
+		
+		newHeader.setupSave(SaveType.REPOSITORY);
+		newHeader.save(oldHeader.header());
+		newHeader.close();
 	}
 	/**
 	 * @param args
@@ -56,10 +92,15 @@ public class updateheader {
 			arg += 2;
 		}
 		
+		CCNHandle handle = CCNHandle.getHandle();
+		
 		for (int i=arg; i <= args.length; ++i) {
-			moveHeader(args[i]);
+			try {
+				moveHeader(args[i], handle);
+			} catch (Exception e) {
+				System.out.println("Exception processing file " + args[i] + ": " + e);
+				e.printStackTrace();
+			}
 		}
-
 	}
-
 }
