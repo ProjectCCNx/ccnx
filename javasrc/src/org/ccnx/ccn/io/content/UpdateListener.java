c12a0ce06b5c274047c4123fdff4f622f146e68f
Diana Smetters <smetters@parc.com>
2010-04-21 11:49:32 -0700

    Callback interface for network object updates.

---
+++ javasrc/src/org/ccnx/ccn/io/content/UpdateListener.java
@@ -0,0 +1,27 @@
+/**
+ * Part of the CCNx Java Library.
+ *
+ * Copyright (C) 2010 Palo Alto Research Center, Inc.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.ccnx.ccn.io.content;
+
+/**
+ * Interface that allows external listeners to register to be notified when a network
+ * object receives an update.
+ */
+public interface UpdateListener {
+	
+	public void newVersionAvailable(CCNNetworkObject<?> newVersion);
+
+}
