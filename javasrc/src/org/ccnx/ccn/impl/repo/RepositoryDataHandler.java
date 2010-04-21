685dae41f700f11cddc19565dd161fae58c961cb
Paul Rasmussen <rasmusse@parc.com>
2010-04-21 11:41:26 -0700

    Use single thread for writing repo data instead of threadpool

--- javasrc/src/org/ccnx/ccn/impl/repo/RepositoryDataHandler.java
+++ javasrc/src/org/ccnx/ccn/impl/repo/RepositoryDataHandler.java
@@ -17,8 +17,11 @@
 
 package org.ccnx.ccn.impl.repo;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
 
+import org.ccnx.ccn.config.SystemConfiguration;
 import org.ccnx.ccn.impl.support.Log;
 import org.ccnx.ccn.profiles.nameenum.NameEnumerationResponse;
 import org.ccnx.ccn.protocol.ContentObject;
@@ -29,14 +32,19 @@ import org.ccnx.ccn.protocol.ContentObject;
  */
 
 public class RepositoryDataHandler implements Runnable {
-	private ContentObject _content;
 	private RepositoryServer _server;
+	private Queue<ContentObject> _queue = new ConcurrentLinkedQueue<ContentObject>();
+	private boolean _shutdown = false;
 	
-	public RepositoryDataHandler(ContentObject co, RepositoryServer server) {
-		if (Log.isLoggable(Log.FAC_REPO, Level.INFO))
-			Log.info(Log.FAC_REPO, "Saw data: {0}", co.name());
-		_content = co;
+	public RepositoryDataHandler(RepositoryServer server) {
 		_server = server;
+	}
+	
+	public void add(ContentObject co) {
+		synchronized(_queue) {
+			_queue.add(co);
+			_queue.notify();
+		}
 	}
 
 	/**
@@ -48,18 +56,38 @@ public class RepositoryDataHandler implements Runnable {
 	 * @see RepositoryStore
 	 */
 	public void run() {
-		try {
-			if (Log.isLoggable(Log.FAC_REPO, Level.FINER)) {
-				Log.finer(Log.FAC_REPO, "Saving content in: " + _content.name().toString());
+		while (!_shutdown) {
+			ContentObject co = null;
+			synchronized (_queue) {
+				do {
+					co = _queue.poll();
+					if (null == co)
+						try {
+							_queue.wait(SystemConfiguration.MEDIUM_TIMEOUT);
+						} catch (InterruptedException e) {}
+					if (_shutdown)
+						break;
+				} while (null == co);
 			}
-			
-			NameEnumerationResponse ner = _server.getRepository().saveContent(_content);
-			if (ner!=null && ner.hasNames()) {
-				_server.sendEnumerationResponse(ner);
+			if (! _shutdown) {
+				try {
+					if (Log.isLoggable(Log.FAC_REPO, Level.FINER)) {
+						Log.finer(Log.FAC_REPO, "Saving content in: " + co.toString());
+					}
+					
+					NameEnumerationResponse ner = _server.getRepository().saveContent(co);
+					if (ner!=null && ner.hasNames()) {
+						_server.sendEnumerationResponse(ner);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					Log.logStackTrace(Level.WARNING, e);
+				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.logStackTrace(Level.WARNING, e);
 		}
+	}
+	
+	public void shutdown() {
+		_shutdown = true;
 	}
 }
