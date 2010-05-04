2e487e0d86473dcc78bc21386be4598f8ac27557
Jim Thornton <jthornton@parc.com>
2010-05-03 23:49:34 -0700

    Reorganized benchmark test with raw digest and basic signing tests now.

--- javasrc/src/org/ccnx/ccn/test/BenchmarkTest.java
+++ javasrc/src/org/ccnx/ccn/test/BenchmarkTest.java
@@ -17,13 +17,15 @@
 
 package org.ccnx.ccn.test;
 
-import java.io.ByteArrayOutputStream;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.util.Random;
+
+import org.ccnx.ccn.KeyManager;
 import org.ccnx.ccn.impl.security.crypto.CCNDigestHelper;
+import org.ccnx.ccn.impl.security.crypto.util.SignatureHelper;
 import org.ccnx.ccn.io.NullOutputStream;
-import org.ccnx.ccn.io.content.ContentEncodingException;
 import org.ccnx.ccn.profiles.SegmentationProfile;
 import org.ccnx.ccn.profiles.VersioningProfile;
 import org.ccnx.ccn.protocol.ContentName;
@@ -41,79 +43,101 @@ import org.junit.Test;
 public class BenchmarkTest {
 
 	public static final int NUM_DIGESTS = 1000;
+	public static final int NUM_SIGNATURES = 1000;
+	public static final int NUM_ITER = 1000;
 	
-	public static final double NanoToMilli = 1000000.0;
+	public static final double NanoToMilli = 1000000.0d;
 	
 	public static CCNTestHelper testHelper = new CCNTestHelper(BenchmarkTest.class);
 
 	public static ContentName testName;
+	public static byte[] shortPayload;
+	public static byte[] longPayload;
 
+	public abstract class Operation<T> {
+		abstract void execute(T input) throws Exception;
+		
+		int size(T input) {
+			if (input instanceof byte[]) {
+				return ((byte[])input).length;
+			} else if (input instanceof ContentObject) {
+				return ((ContentObject)input).content().length;
+			} else {
+				throw new RuntimeException("Unsupported input type " + input.getClass());
+			}
+		}
+	}
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		ContentName namespace = testHelper.getTestNamespace("benchmarkTest");
 		testName = ContentName.fromNative(namespace, "BenchmarkObject");
 		testName = VersioningProfile.addVersion(testName);
+		shortPayload = ("this is sample segment content").getBytes();
+		longPayload = new byte[1000];
+		Random rnd = new Random();
+		rnd.nextBytes(longPayload);
+		System.out.println("Benchmark Test starting on " + System.getProperty("os.name"));
 	}
 
+	@SuppressWarnings("unchecked")
+	public void runBenchmark(String desc, Operation op, Object input) throws Exception {
+		long start = System.nanoTime();
+		op.execute(input);
+		long dur = System.nanoTime() - start;
+		//System.out.println("Start " + start + " dur " + dur);
+		System.out.println("Initial time to " + desc + " (payload " + op.size(input) + " bytes) = " + dur/NanoToMilli + " ms.");
+		
+		start = System.nanoTime();
+		for (int i=0; i<NUM_ITER; i++) {
+			op.execute(input);
+		}
+		dur = System.nanoTime() - start;
+		System.out.println("Avg. to " + desc + " (" + NUM_ITER + " iterations) = " + dur/NUM_ITER/NanoToMilli + " ms.");		
+	}
 	@Test
 	public void testDigest() throws Exception {
-
-		// Base case: short payload
-		byte[] toWrite = ("this is sample segment content").getBytes();
+		System.out.println("==== Digest Benchmarks");
+		Operation<byte[]> digest = new Operation<byte[]>() {
+			void execute(byte[] input) throws Exception {
+				MessageDigest md = MessageDigest.getInstance(CCNDigestHelper.DEFAULT_DIGEST_ALGORITHM);
+				md.update(input);
+				md.digest();
+			}
+		};
+		System.out.println("--- Raw = digest only of byte[]");
+		runBenchmark("raw digest short", digest, shortPayload);
+		runBenchmark("raw digest long", digest, longPayload);
 		ContentName segment = SegmentationProfile.segmentName(testName, 0);
-		ContentObject obj = ContentObject.buildContentObject(segment, toWrite, null, null, SegmentationProfile.getSegmentNumberNameComponent(0));
-		long start = System.nanoTime();
-		digest(obj);
-		long dur = System.nanoTime() - start;
-		System.out.println("Time to compute initial short digest " + dur/NanoToMilli + " ms.");
-		start = System.nanoTime();
-		digest(obj);
-		dur = System.nanoTime() - start;
-		System.out.println("Time to compute second short digest " + dur/NanoToMilli + " ms.");
-		start = System.nanoTime();
-		for (int i=0; i<NUM_DIGESTS; i++) {
-			digest(obj);
-		}
-		dur = System.nanoTime() - start;
-		System.out.println("Avg. time to compute short digest over " + NUM_DIGESTS + " trials = " + dur/NUM_DIGESTS/NanoToMilli + " ms.");
-	
-		// Longer payload case
-		byte[] toWriteLong = new byte[1024];
-		segment = SegmentationProfile.segmentName(testName, 1);
-		obj = ContentObject.buildContentObject(segment, toWriteLong, null, null, SegmentationProfile.getSegmentNumberNameComponent(1));
-		start = System.nanoTime();
-		for (int j=0; j<NUM_DIGESTS; j++) {
-			digest(obj);
-		}
-		dur = System.nanoTime() - start;
-		System.out.println("Avg. time to compute long digest over " + NUM_DIGESTS + " trials = " + dur/NUM_DIGESTS/NanoToMilli + " ms.");
+		
+		Operation<ContentObject> digestObj = new Operation<ContentObject>() {
+			void execute(ContentObject input) throws Exception {
+				MessageDigest md = MessageDigest.getInstance(CCNDigestHelper.DEFAULT_DIGEST_ALGORITHM);
+				DigestOutputStream dos = new DigestOutputStream(new NullOutputStream(), md);
+				input.encode(dos);
+				md.digest();
+			}
+		};
+		System.out.println("--- Object = digest of ContentObject");
+		ContentObject shortObj = ContentObject.buildContentObject(segment, shortPayload, null, null, SegmentationProfile.getSegmentNumberNameComponent(0));
+		ContentObject longObj = ContentObject.buildContentObject(segment, longPayload, null, null, SegmentationProfile.getSegmentNumberNameComponent(0));
+		runBenchmark("obj digest short", digestObj, shortObj);
+		runBenchmark("obj digest long", digestObj, longObj);
+	}
+		
+	@Test
+	public void testRawSigning() throws Exception {
+		
+		Operation<byte[]> sign = new Operation<byte[]>() {
+			KeyManager keyManager = KeyManager.getDefaultKeyManager();
+			PrivateKey signingKey = keyManager.getDefaultSigningKey();
 
-		// Reference case: running raw digest computation on encoded only
-		ByteArrayOutputStream baos = new ByteArrayOutputStream(1500);
-		obj.encode(baos);
-		byte[] toDigest = baos.toByteArray();
-		start = System.nanoTime();
-		for (int k=0; k<NUM_DIGESTS; k++) {
-			digestRaw(toDigest);
-		}
-		dur = System.nanoTime() - start;
-		System.out.println("Avg. time to compute long digest without encoding over " + NUM_DIGESTS + " trials = " + dur/NUM_DIGESTS/NanoToMilli + " ms.");
-			
-	}
-	
-	// the ContentObject.digest() method saves the result to avoid computation on every request
-	// we want the repeated computation for benchmarking
-	public static byte[] digest(ContentObject obj) throws NoSuchAlgorithmException, ContentEncodingException {
-		MessageDigest md = MessageDigest.getInstance(CCNDigestHelper.DEFAULT_DIGEST_ALGORITHM);
-		DigestOutputStream dos = new DigestOutputStream(new NullOutputStream(), md);
-		obj.encode(dos);
-		return md.digest();
-	}
-	
-	// For comparison, simply run the data through the digest directly, no encoding
-	public static byte[] digestRaw(byte[] input) throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance(CCNDigestHelper.DEFAULT_DIGEST_ALGORITHM);
-		md.update(input);
-		return md.digest();
+			void execute(byte[] input) throws Exception {
+				SignatureHelper.sign(CCNDigestHelper.DEFAULT_DIGEST_ALGORITHM, input, signingKey);
+			}
+		};
+		System.out.println("==== PK Signing Benchmarks");
+		runBenchmark("sign short", sign, shortPayload);
+		runBenchmark("sign long", sign, longPayload);		
 	}
 }
