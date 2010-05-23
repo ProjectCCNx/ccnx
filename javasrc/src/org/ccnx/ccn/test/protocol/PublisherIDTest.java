0d3eeb43bdc731bcfd89ae25b183644cff5063ea
Diana Smetters <smetters@parc.com>
2010-05-22 17:32:13 -0700

    Handle weird case where we might get handed too long of a PPKD..

--- javasrc/src/org/ccnx/ccn/test/protocol/PublisherIDTest.java
+++ javasrc/src/org/ccnx/ccn/test/protocol/PublisherIDTest.java
@@ -17,9 +17,14 @@
 
 package org.ccnx.ccn.test.protocol;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 
+import org.ccnx.ccn.impl.security.crypto.CCNDigestHelper;
+import org.ccnx.ccn.impl.support.DataUtils;
 import org.ccnx.ccn.protocol.PublisherID;
+import org.ccnx.ccn.protocol.PublisherPublicKeyDigest;
 import org.ccnx.ccn.protocol.PublisherID.PublisherType;
 import org.ccnx.ccn.test.impl.encoding.XMLEncodableTester;
 import org.junit.BeforeClass;
@@ -66,6 +71,48 @@ public class PublisherIDTest {
 		PublisherID bpubisscertDec = new PublisherID();
 		XMLEncodableTester.encodeDecodeTest("PublisherID(isscert)", pubisscert, pubisscertDec, bpubisscertDec);
 			
+	}
+	
+	@Test
+	public void testPublisherToString() {
+		
+		String id = "1lplh6o3q52k6jnnr0n7utmegju6cjjg3p7jhgfn8h1siubg20r7";
+		System.out.println("id length " + id.length());
+		PublisherPublicKeyDigest did = new PublisherPublicKeyDigest(id);
+		
+		byte [] sid = CCNDigestHelper.scanBytes(id, 32);
+		System.out.println("sid hex " + DataUtils.printHexBytes(sid));
+		
+		System.out.println("id " + id);
+		System.out.println("id hex " + DataUtils.printHexBytes(id.getBytes()));
+		System.out.println("did " + did);
+		System.out.println("PPKD(did) " + new PublisherPublicKeyDigest(did.toString()));
+		
+		String id2 = "6n6m4r0f8kagqeuvc2svrmpq2fopiee0f4ue61ut247ibpe083";
+		System.out.println("id2 length " + id2.length());
+		PublisherPublicKeyDigest did2 = new PublisherPublicKeyDigest(id2);
+		
+		byte [] sid2 = CCNDigestHelper.scanBytes(id2, 32);
+		System.out.println("sid2 hex " + DataUtils.printHexBytes(sid2));
+		
+		System.out.println("id2 " + id2);
+		System.out.println("id2 hex " + DataUtils.printHexBytes(id2.getBytes()));
+		System.out.println("did2 " + did2);
+		System.out.println("PPKD(did2) " + new PublisherPublicKeyDigest(did2.toString()));
+		
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(os);
+		ps.println(did2.toString());
+		ps.close();
+		
+		byte [] output = os.toByteArray();
+		System.out.println("Via printstream: " + DataUtils.printHexBytes(output));
+		String input = new String(output);
+		System.out.println("into a string: " + input);
+		// the PrintStream puts a CR onto the end, which causes trouble.
+	//	byte [] sid3 = CCNDigestHelper.scanBytes(input, 32);
+	//	System.out.println("sid3 hex " + DataUtils.printHexBytes(sid3));
+		
 	}
 
 }
