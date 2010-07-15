7f64b760a7f14b37f3f2328e36f168b292ea3d20
Michael Plass <plass@parc.com>
2010-07-15 13:14:15 -0700

    Teach ccntestloop to update sources and rebuild on each iteration.

--- csrc/util/ccntestloop.sh
+++ csrc/util/ccntestloop.sh
@@ -12,12 +12,14 @@
 # FOR A PARTICULAR PURPOSE.
 #
 
+: ${CCN_LOG_LEVEL_ALL:=WARNING}
+: ${CCN_TEST_BRANCH:=HEAD}
+: ${CCN_TEST_GITCOMMAND:=`command -v git || :`}
+
 Fail () {
     echo '***' Failed - $* >&2
     exit 1;
 }
-
-: ${CCN_LOG_LEVEL_ALL:=WARNING}
 
 case ${CCN_LOG_LEVEL_ALL} in
     OFF) ;;
@@ -31,10 +33,14 @@ case ${CCN_LOG_LEVEL_ALL} in
     ALL) ;;
     *) Fail CCN_LOG_LEVEL_ALL=$CCN_LOG_LEVEL_ALL is invalid.
 esac
+
 export CCN_LOG_LEVEL_ALL
+export CCN_TEST_BRANCH
+export CCN_TEST_GITCOMMAND
 
 test -d javasrc || Fail $0 is intended to be run at the top level of ccnx
-test -d .git && git status
+test -d .git || CCN_TEST_GITCOMMAND=:
+
 set | grep ^CCN
 
 SaveLogs () {
@@ -54,16 +60,21 @@ RunTest () {
     echo;
     test -d testout && Fail testout directory is present.
     test -d testout.$1 && Fail testout.$1 directory is present.
+    mkdir -p testout/logs
+    echo Updating for run $1 >&2
+    $CCN_TEST_GITCOMMAND checkout $CCN_TEST_BRANCH
+    $CCN_TEST_GITCOMMAND pull --no-commit origin $CCN_TEST_BRANCH
+    echo Building for run $1 >&2
+    (cd .. && ${MAKE:-make}; ) || Fail did not build
     echo Starting run $1
     date;
-    mkdir -p testout/logs
     RunCTest && ant -DCHATTY=${CCN_LOG_LEVEL_ALL} test && SaveLogs $1 && return 0
     SaveLogs $1.FAILED
     return 1
 }
 
 LastRun () {
-   ls -td javasrc/testout.* | head -n 1 | cut -d . -f 2
+   ls -td javasrc/testout.* 2>/dev/null | head -n 1 | cut -d . -f 2
 }
 
 RunTests () {
