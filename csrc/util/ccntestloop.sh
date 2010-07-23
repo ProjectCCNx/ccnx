f236f8e1cea0e4d5e8cd30d9583892192732b912
Michael Plass <plass@parc.com>
2010-07-23 14:57:44 -0700

    Update ccntestloop to be able to pull in new revisions of itself.
    A major refactoring of this script makes it easier to follow.
    Console logs are captured as well.

--- csrc/util/ccntestloop.sh
+++ csrc/util/ccntestloop.sh
@@ -12,75 +12,163 @@
 # FOR A PARTICULAR PURPOSE.
 #
 
+# Provide defaults for environment
 : ${CCN_LOG_LEVEL_ALL:=WARNING}
 : ${CCN_TEST_BRANCH:=HEAD}
 : ${CCN_TEST_GITCOMMAND:=`command -v git || echo :`}
-
-Fail () {
-    echo '***' Failed - $* >&2
-    exit 1;
-}
-
-case ${CCN_LOG_LEVEL_ALL} in
-    OFF) ;;
-    SEVERE) ;;
-    WARNING) ;;
-    INFO) ;;
-    CONFIG) ;;
-    FINE) ;;
-    FINER) ;;
-    FINEST) ;;
-    ALL) ;;
-    *) Fail CCN_LOG_LEVEL_ALL=$CCN_LOG_LEVEL_ALL is invalid.
-esac
-
+: ${MAKE:=make}
+test -d .git || CCN_TEST_GITCOMMAND=:
 export CCN_LOG_LEVEL_ALL
 export CCN_TEST_BRANCH
 export CCN_TEST_GITCOMMAND
+export MAKE
 
-test -d javasrc || Fail $0 is intended to be run at the top level of ccnx
-test -d .git || CCN_TEST_GITCOMMAND=:
+THIS=$0
 
-set | grep ^CCN
+# Say things to both stdout and stderr so that output may be captured with tee.
+Echo () {
+	echo "$*" >&2
+	echo "$*"
+}
+
+Fail () {
+    Echo '***' Failed - "$*"
+    exit 1;
+}
+
+CheckLogLevel () {
+	case ${CCN_LOG_LEVEL_ALL} in
+		OFF) ;;
+		SEVERE) ;;
+		WARNING) ;;
+		INFO) ;;
+		CONFIG) ;;
+		FINE) ;;
+		FINER) ;;
+		FINEST) ;;
+		ALL) ;;
+		*) Fail CCN_LOG_LEVEL_ALL=$CCN_LOG_LEVEL_ALL is invalid.
+	esac
+}
+
+CheckDirectory () {
+	test -d javasrc || Fail $THIS is intended to be run at the top level of ccnx
+	test -d javasrc/testout && Fail javasrc/testout directory is present.
+}
+
+PrintRelevantSettings () {
+	uname -a
+	set | grep ^CCN
+	$CCN_TEST_GITCOMMAND rev-list --max-count=1 HEAD
+	$CCN_TEST_GITCOMMAND status
+}
+
+PrintDetails () {
+	uname -a
+	$CCN_TEST_GITCOMMAND rev-list --max-count=1 HEAD
+	$CCN_TEST_GITCOMMAND status
+	$CCN_TEST_GITCOMMAND diff
+	head -999 csrc/conf.mk
+}
 
 SaveLogs () {
-    test -d testout && mv testout testout.$1
+    test -d javasrc/testout || return 1
+	PrintDetails > javasrc/testout/TEST-details.txt
+	grep -e BUILD -e 'Total time.*minutes' javasrc/testout/TEST-javasrc-testlog.txt
+	mv javasrc/testout javasrc/testout.$1
+}
+
+UpdateSources () {
+	Echo Updating for run $1
+	$CCN_TEST_GITCOMMAND checkout $CCN_TEST_BRANCH
+    $CCN_TEST_GITCOMMAND pull --no-commit origin $CCN_TEST_BRANCH
+}
+
+SourcesChanged () {
+	: For now, always rebuild
+	true
+}
+
+ScriptChanged () {
+	trap "rm .~ctloop~" 0
+	tail -n +2 $THIS > .~ctloop~
+	diff .~ctloop~ csrc/util/ccntestloop.sh && return 1
+	return 0 # Yes, it changed.
+}
+
+Rebuild () {
+	local LOG;
+	Echo Building for run $1
+	LOG=javasrc/testout/TEST-buildlog.txt
+	mkdir -p javasrc/testout
+	(./configure && $MAKE; ) 2>&1 > $LOG && return 0
+	tail $LOG
+	Echo build failed
+	return 1
 }
 
 RunCTest () {
-   # return 0
-   echo Running csrc tests...
-   ( cd ../csrc && make test 2>&1 ) > testout/logs/csrc-tests.log
-   date
+	local LOG;
+	Echo Running csrc tests...
+	LOG=javasrc/testout/TEST-csrc-testlog.txt
+	mkdir -p javasrc/testout
+	( cd csrc && $MAKE test 2>&1 ) > $LOG && return 0
+	tail $LOG
+	Echo csrc tests failed
+	return 1
 }
 
-RunTest () { 
-    echo;
-    echo;
-    echo;
-    test -d testout && Fail testout directory is present.
-    test -d testout.$1 && Fail testout.$1 directory is present.
-    mkdir -p testout/logs
-    echo Updating for run $1 >&2
-    $CCN_TEST_GITCOMMAND checkout $CCN_TEST_BRANCH
-    $CCN_TEST_GITCOMMAND pull --no-commit origin $CCN_TEST_BRANCH
-    echo Building for run $1 >&2
-    (cd .. && ${MAKE:-make}; ) || Fail did not build
-    echo Starting run $1
-    date;
-    RunCTest && ant -DCHATTY=${CCN_LOG_LEVEL_ALL} test && SaveLogs $1 && return 0
-    SaveLogs $1.FAILED
-    return 1
+RunJavaTest () {
+	local LOG;
+	Echo Running javasrc tests...
+	LOG=javasrc/testout/TEST-javasrc-testlog.txt
+	(cd javasrc && ant -DCHATTY=${CCN_LOG_LEVEL_ALL} test; ) > $LOG && return 0
+	tail $LOG
+	Echo javasrc tests failed
+	return 1
 }
 
-LastRun () {
-   ls -td javasrc/testout.* 2>/dev/null | head -n 1 | cut -d . -f 2
+RunTest () {
+	Echo Starting run $1 at `date`
+	RunCTest && RunJavaTest && SaveLogs $1 && return 0
+	SaveLogs $1.FAILED
+	return 1
 }
 
-RunTests () {
-    L=$((`LastRun` +0))
-    I=$((L+1))
-    ( cd javasrc; while RunTest $I; do I=$((I+1)); sleep 2; done; )
+LastRunNumber () {
+	ls -td javasrc/testout.* 2>/dev/null | head -n 1 | cut -d . -f 2
 }
 
-RunTests
+ThisRunNumber () {
+	echo $((`LastRunNumber` + 1))
+}
+
+# Finally, here's what we actually want to do
+
+CheckLogLevel
+
+CheckDirectory
+
+RUN=`ThisRunNumber`
+
+PrintRelevantSettings
+
+UpdateSources $RUN
+
+if ScriptChanged; then
+	Echo "*** Script changed - will clean, rebuild and restart"
+	$MAKE clean || Fail make clean
+	$MAKE || Fail make
+	echo Pausing for 10 seconds before restart...
+	sleep 10
+	exec ./bin/ccntestloop || Fail exec
+fi
+
+if SourcesChanged; then
+	Rebuild $RUN || Fail make
+fi
+
+RunTest $RUN || Fail RunTest - stopping
+Echo Run number $RUN was successful
+sleep 2
+exec ./bin/ccntestloop || Fail exec
