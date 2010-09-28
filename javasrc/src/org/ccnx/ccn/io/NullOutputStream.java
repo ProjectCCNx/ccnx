6f757a9eae5789b08ddc58972574ca2610a8c82d
Michael Plass <plass@parc.com>
2010-09-27 20:37:35 -0700

    Copyright date, whitespace

--- javasrc/src/org/ccnx/ccn/io/NullOutputStream.java
+++ javasrc/src/org/ccnx/ccn/io/NullOutputStream.java
@@ -1,7 +1,7 @@
 /*
  * Part of the CCNx Java Library.
  *
- * Copyright (C) 2008, 2009 Palo Alto Research Center, Inc.
+ * Copyright (C) 2008, 2009, 2010 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
@@ -36,15 +36,15 @@ public class NullOutputStream extends OutputStream {
 	public void write(int b) throws IOException {
 		// Do nothing.
 	}
-    
-    @Override
+	
+	@Override
 	public void write(byte [] b) throws IOException {
-        // Do nothing.
+		// Do nothing.
 	}
-    
+	
 	@Override
 	public void write(byte[] b, int off, int len) throws IOException {
-        // Do nothing.
+		// Do nothing.
 	}
 
 }
