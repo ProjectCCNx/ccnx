7a2b5e53fcbe44afba0574bd2ec4cd0e44efba70
Michael Plass <plass@parc.com>
2010-12-07 17:23:28 -0800

    Publish ccnd key under ccnx:/%C1.M.S.neighborhood/%C1.M.SRV/ccnd/KEY as well as the localhost name.  refs #100187
    Also record the ccnd start time to microsecond precision, and use it to set
    the version stamps on the key objects.

--- csrc/ccnd/ccnd.c
+++ csrc/ccnd/ccnd.c
@@ -4605,6 +4605,7 @@ ccnd_create(const char *progname, ccnd_logger logger, void *loggerdata)
     h->ticktock.data = h;
     h->sched = ccn_schedule_create(h, &h->ticktock);
     h->starttime = h->sec;
+    h->starttime_usec = h->usec;
     h->oldformatcontentgrumble = 1;
     h->oldformatinterestgrumble = 1;
     h->data_pause_microsec = 10000;
