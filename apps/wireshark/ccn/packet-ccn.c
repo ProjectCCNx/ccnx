3df307c48100612be253c66aca45111ddd649df7
Nick Briggs <briggs@parc.com>
2011-01-14 15:56:01 -0800

    Update copyright date.

--- apps/wireshark/ccn/packet-ccn.c
+++ apps/wireshark/ccn/packet-ccn.c
@@ -3,7 +3,7 @@
  * 
  * A wireshark plugin for CCNx protocols.
  *
- * Copyright (C) 2009 Palo Alto Research Center, Inc.
+ * Copyright (C) 2009,2011 Palo Alto Research Center, Inc.
  *
  * This work is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
