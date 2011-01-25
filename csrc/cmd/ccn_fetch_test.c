ecb0c0185ad9afd217e7bb2276408f54d9e521f9
Michael Plass <plass@parc.com>
2011-01-25 15:49:01 -0800

    refs #100303  Remove binary that was added accidentally.  Fix copyright date.

--- csrc/cmd/ccn_fetch_test.c
+++ csrc/cmd/ccn_fetch_test.c
@@ -2,7 +2,7 @@
  * @file ccn_fetch_test.c
  * @brief Provides a test platform for ccn_fetch.
  *
- * Copyright (C) 2010 Palo Alto Research Center, Inc.
+ * Copyright (C) 2011 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
