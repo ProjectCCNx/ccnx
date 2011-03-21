9c79067777d9f5b8e81377ae5c89c28795d7e15b
Marc Mosko <marc.mosko@parc.com>
2011-03-20 23:11:32 -0700

    Refs #100390.  Update copyright.

--- android/CCNx-Android-Lib/src/org/ccnx/android/ccnlib/CCNxWrapper.java
+++ android/CCNx-Android-Lib/src/org/ccnx/android/ccnlib/CCNxWrapper.java
@@ -1,7 +1,7 @@
 /*
  * CCNx Android Helper Library.
  *
- * Copyright (C) 2010 Palo Alto Research Center, Inc.
+ * Copyright (C) 2010,2011 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
