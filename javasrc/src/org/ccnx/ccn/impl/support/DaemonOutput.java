7798522c6adb6c1c2762afe9bf23946391f422e2
Paul Rasmussen <rasmusse@parc.com>
2011-03-25 15:38:57 -0700

    Update copyright notice, fix possible non close of _is in DaemonOutput refs #100360

--- javasrc/src/org/ccnx/ccn/impl/support/DaemonOutput.java
+++ javasrc/src/org/ccnx/ccn/impl/support/DaemonOutput.java
@@ -1,7 +1,7 @@
 /*
  * Part of the CCNx Java Library.
  *
- * Copyright (C) 2008, 2009 Palo Alto Research Center, Inc.
+ * Copyright (C) 2008, 2009, 2011 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
@@ -53,7 +53,10 @@ public class DaemonOutput extends Thread {
 	}
 	
 	public void close() throws IOException {
-		_os.close();
-		_is.close();
+		try {
+			_os.close();
+		} finally {
+			_is.close();
+		}
 	}
 }
