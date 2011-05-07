0b9cac546b9c59509e8a86c5297fe2071cfc9fdc
Paul Rasmussen <rasmusse@parc.com>
2011-05-06 18:00:17 -0700

    Add more testing to initial policy test refs #100472

--- javasrc/src/org/ccnx/ccn/test/repo/RepoInitialPolicyTest.java
+++ javasrc/src/org/ccnx/ccn/test/repo/RepoInitialPolicyTest.java
@@ -15,23 +15,38 @@ public class RepoInitialPolicyTest extends RepoTestBase {
 		RepoDaemonTest rdt = new RepoDaemonTest(new String[]{"-root", _fileTestDir2, 
 						"-policy", _topdir + "/org/ccnx/ccn/test/repo/policyTest.xml",
 						"-global", "/parc.com/csl/ccn/repositories/TestRepository"}, this);
-		Thread th = new Thread(rdt);
-		th.start();
-		synchronized (this) {
-			this.wait();
-		}
-		rdt.shutdown();
-		th.join();
+		startAndStopRepo(rdt);
 		File testFile = new File(_fileTestDir2 + "/" + LogStructRepoStoreProfile.CONTENT_FILE_PREFIX + "1");
 		long testLength = testFile.length();
-		th = new Thread(rdt);
+		startAndStopRepo(rdt);
+		Assert.assertEquals(testLength, testFile.length());
+		RepoDaemonTest rdtNoChange = new RepoDaemonTest(new String[]{"-root", _fileTestDir2,
+				"-global", "/parc.com/csl/ccn/repositories/TestRepository"}, this);
+		Thread th = new Thread(rdtNoChange);
 		th.start();
 		synchronized (this) {
 			this.wait();
 		}
-		rdt.shutdown();
+		checkNameSpace("/repoTest/data3", false);
+		checkNameSpace("/testNameSpace/data1", true);
+		rdtNoChange.shutdown();
 		th.join();
+		rdt = new RepoDaemonTest(new String[]{"-root", _fileTestDir2, 
+				"-prefix", "/policyTest/foo",
+				"-global", "/parc.com/csl/ccn/repositories/TestRepository"}, this);
+		startAndStopRepo(rdt);
+		testLength = testFile.length();
+		startAndStopRepo(rdt);
 		Assert.assertEquals(testLength, testFile.length());
+		th = new Thread(rdtNoChange);
+		th.start();
+		synchronized (this) {
+			this.wait();
+		}
+		checkNameSpace("/testNameSpace/data1", false);
+		checkNameSpace("/policyTest/foo/bar", true);
+		rdtNoChange.shutdown();
+		th.join();
 	}
 	
 	public class RepoDaemonTest extends RepositoryDaemon implements Runnable {
@@ -57,5 +72,15 @@ public class RepoInitialPolicyTest extends RepoTestBase {
 		public void shutdown() {
 			_wt.shutDown();
 		}
+	}
+	
+	private void startAndStopRepo(RepoDaemonTest rdt) throws InterruptedException {
+		Thread th = new Thread(rdt);
+		th.start();
+		synchronized (this) {
+			this.wait();
+		}
+		rdt.shutdown();
+		th.join();
 	}
 }
