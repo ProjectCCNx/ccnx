f2aeb76d37ba85c416f75764cc4162c9baf160a3
Nick Briggs <briggs@parc.com>
2011-05-31 18:08:56 -0700

    Add check for command markers in the direct client. Create function to append a RepositoryInfo to a charbuf for use in StartWrite responses.

--- csrc/ccnr/ccnr_proto.c
+++ csrc/ccnr/ccnr_proto.c
@@ -35,6 +35,7 @@
 #include <ccn/schedule.h>
 #include <ccn/sockaddrutil.h>
 #include <ccn/uri.h>
+#include <ccn/coding.h>
 #include "ccnr_private.h"
 
 #include "ccnr_proto.h"
@@ -45,6 +46,9 @@
 #include "ccnr_sendq.h"
 #include "ccnr_store.h"
 
+#define REPO_SW "\301.R.sw"
+#define REPO_SWC "\301.R.sw-c"
+#define NAME_BE "\301.E.be"
 PUBLIC enum ccn_upcall_res
 r_proto_answer_req(struct ccn_closure *selfp,
                  enum ccn_upcall_kind kind,
@@ -58,6 +62,7 @@ r_proto_answer_req(struct ccn_closure *selfp,
     struct ccnr_handle *ccnr = NULL;
     struct content_entry *content = NULL;
     int res = 0;
+    int ncomps;
     // struct ccn_signing_params sp = CCN_SIGNING_PARAMS_INIT;
     
     switch (kind) {
@@ -85,6 +90,17 @@ r_proto_answer_req(struct ccn_closure *selfp,
         goto Finish;
     }
     // XXX - Here is where we should check for command markers
+    ncomps = info->interest_comps->n;
+    if (ncomps >= 2 && 0 == ccn_name_comp_strcmp(info->interest_ccnb, info->interest_comps, ncomps - 2, NAME_BE)) {
+        ccnr_debug_ccnb(ccnr, __LINE__, "name_enumeration", NULL, info->interest_ccnb, info->pi->offset[CCN_PI_E]);
+        goto Finish;
+    } else if (ncomps >= 3 && 0 == ccn_name_comp_strcmp(info->interest_ccnb, info->interest_comps, ncomps - 3, REPO_SW)) {
+        ccnr_debug_ccnb(ccnr, __LINE__, "repo_start_write", NULL, info->interest_ccnb, info->pi->offset[CCN_PI_E]);
+        goto Finish;
+    } else if (ncomps >= 5 && 0 == ccn_name_comp_strcmp(info->interest_ccnb, info->interest_comps, ncomps - 5, REPO_SWC)) {
+        ccnr_debug_ccnb(ccnr, __LINE__, "repo_start_write_checked", NULL, info->interest_ccnb, info->pi->offset[CCN_PI_E]);
+        goto Finish;
+    }
     goto Finish;
 Bail:
     res = CCN_UPCALL_RESULT_ERR;
@@ -120,3 +136,47 @@ r_proto_init(struct ccnr_handle *ccnr) {
     r_proto_uri_listen(ccnr, ccnr->direct_client, "ccnx:/", r_proto_answer_req, 0);
 }
 
+/* Construct a charbuf with an encoding of a RepositoryInfo -- currently a
+ * constant, but may need to vary
+ *
+ *  <RepositoryInfo>
+ *  <Version>1.1</Version>
+ *  <Type>INFO</Type>
+ *  <RepositoryVersion>1.4</RepositoryVersion>
+ *  <GlobalPrefixName>
+ *  <Component ccnbencoding="text">parc.com</Component>
+ *  <Component ccnbencoding="text">csl</Component>
+ *  <Component ccnbencoding="text">ccn</Component>
+ *  <Component ccnbencoding="text">Repos</Component>
+ *  </GlobalPrefixName>
+ *  <LocalName>Repository</LocalName>
+ *  </RepositoryInfo>
+*/ 
+PUBLIC int
+r_proto_append_repo_info(struct ccn_charbuf *rinfo) {
+    int res;
+    struct ccn_charbuf *name = ccn_charbuf_create();
+    if (name == NULL) return (-1);
+    res = ccnb_element_begin(rinfo, CCN_DTAG_RepositoryInfo);
+    res |= ccnb_tagged_putf(rinfo, CCN_DTAG_Version, "%s", "1.1");
+    res |= ccnb_tagged_putf(rinfo, CCN_DTAG_Type, "%s", "DATA");
+    res |= ccnb_tagged_putf(rinfo, CCN_DTAG_RepositoryVersion, "%s", "2.0");
+
+    res |= ccnb_element_begin(name, CCN_DTAG_GlobalPrefixName); // same structure as Name
+    res |= ccnb_element_end(name);
+    res |= ccn_name_append_str(name, "parc.com");
+    res |= ccn_name_append_str(name, "csl");
+    res |= ccn_name_append_str(name, "ccn");
+    res |= ccn_name_append_str(name, "Repos");
+    res |= ccn_charbuf_append_charbuf(rinfo, name);
+    
+    name->length = 0;
+    res |= ccnb_element_begin(name, CCN_DTAG_LocalName);
+    res |= ccnb_element_end(name);
+    res |= ccn_name_append_str(name, "Repository");
+    res |= ccn_charbuf_append_charbuf(rinfo, name);
+
+    res |= ccnb_element_end(rinfo); // CCN_DTAG_RepositoryInfo
+    ccn_charbuf_destroy(&name);
+    return (res);
+}
