fbc78b6551455e2e003de50a58e38ed31002a859
Michael Plass <plass@parc.com>
2011-07-06 12:59:02 -0700

    Update dependencies for repo and sync.

--- csrc/ccnr/dir.mk
+++ csrc/ccnr/dir.mk
@@ -60,10 +60,10 @@ ccnr_dispatch.o: ccnr_dispatch.c ../include/ccn/bloom.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
   ../include/ccn/face_mgmt.h ../include/ccn/sockcreate.h \
   ../include/ccn/hashtb.h ../include/ccn/schedule.h \
-  ../include/ccn/reg_mgmt.h ../include/ccn/uri.h ccnr_private.h \
-  ../include/ccn/seqwriter.h ccnr_dispatch.h ccnr_forwarding.h ccnr_io.h \
-  ccnr_link.h ccnr_match.h ccnr_msg.h ccnr_sendq.h ccnr_stats.h \
-  ccnr_store.h ccnr_util.h
+  ../include/ccn/reg_mgmt.h ../include/ccn/uri.h ../sync/SyncBase.h \
+  ../sync/SyncMacros.h ccnr_private.h ../include/ccn/seqwriter.h \
+  ccnr_dispatch.h ccnr_forwarding.h ccnr_io.h ccnr_link.h ccnr_match.h \
+  ccnr_msg.h ccnr_sendq.h ccnr_stats.h ccnr_store.h ccnr_util.h
 ccnr_forwarding.o: ccnr_forwarding.c ../include/ccn/bloom.h \
   ../include/ccn/ccn.h ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
@@ -79,15 +79,16 @@ ccnr_init.o: ccnr_init.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/hashtb.h ../include/ccn/schedule.h \
   ../include/ccn/reg_mgmt.h ../include/ccn/uri.h ../sync/SyncBase.h \
   ../sync/SyncMacros.h ccnr_private.h ../include/ccn/seqwriter.h \
-  ccnr_init.h ccnr_forwarding.h ccnr_internal_client.h ccnr_io.h \
-  ccnr_msg.h ccnr_net.h ccnr_proto.h ccnr_store.h ccnr_util.h
+  ccnr_init.h ccnr_dispatch.h ccnr_forwarding.h ccnr_internal_client.h \
+  ccnr_io.h ccnr_msg.h ccnr_net.h ccnr_proto.h ccnr_store.h ccnr_util.h
 ccnr_internal_client.o: ccnr_internal_client.c ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
   ../include/ccn/schedule.h ../include/ccn/sockaddrutil.h \
-  ../include/ccn/uri.h ccnr_private.h ../include/ccn/reg_mgmt.h \
-  ../include/ccn/seqwriter.h ccnr_internal_client.h ccnr_forwarding.h \
-  ../include/ccn/hashtb.h ccnr_io.h ccnr_msg.h
+  ../include/ccn/uri.h ../include/ccn/keystore.h ccnr_private.h \
+  ../include/ccn/reg_mgmt.h ../include/ccn/seqwriter.h \
+  ccnr_internal_client.h ccnr_forwarding.h ../include/ccn/hashtb.h \
+  ccnr_io.h ccnr_msg.h ccnr_proto.h ccnr_util.h
 ccnr_io.o: ccnr_io.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
@@ -129,10 +130,11 @@ ccnr_net.o: ccnr_net.c ccnr_private.h ../include/ccn/ccn_private.h \
 ccnr_proto.o: ccnr_proto.c ../include/ccn/ccn.h ../include/ccn/coding.h \
   ../include/ccn/charbuf.h ../include/ccn/indexbuf.h \
   ../include/ccn/ccn_private.h ../include/ccn/schedule.h \
-  ../include/ccn/sockaddrutil.h ../include/ccn/uri.h ccnr_private.h \
-  ../include/ccn/reg_mgmt.h ../include/ccn/seqwriter.h ccnr_proto.h \
-  ccnr_dispatch.h ccnr_forwarding.h ../include/ccn/hashtb.h ccnr_io.h \
-  ccnr_msg.h ccnr_sendq.h ccnr_store.h
+  ../include/ccn/sockaddrutil.h ../include/ccn/uri.h ../sync/SyncBase.h \
+  ../sync/SyncMacros.h ccnr_private.h ../include/ccn/reg_mgmt.h \
+  ../include/ccn/seqwriter.h ccnr_proto.h ccnr_dispatch.h \
+  ccnr_forwarding.h ../include/ccn/hashtb.h ccnr_io.h ccnr_msg.h \
+  ccnr_sendq.h ccnr_store.h ccnr_util.h
 ccnr_sendq.o: ccnr_sendq.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
@@ -157,7 +159,9 @@ ccnr_store.o: ccnr_store.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
 ccnr_sync.o: ccnr_sync.c ../include/ccn/ccn.h ../include/ccn/coding.h \
   ../include/ccn/charbuf.h ../include/ccn/indexbuf.h ccnr_private.h \
   ../include/ccn/ccn_private.h ../include/ccn/reg_mgmt.h \
-  ../include/ccn/schedule.h ../include/ccn/seqwriter.h ccnr_sync.h
+  ../include/ccn/schedule.h ../include/ccn/seqwriter.h ccnr_dispatch.h \
+  ccnr_io.h ccnr_msg.h ccnr_sendq.h ccnr_store.h ../include/ccn/hashtb.h \
+  ccnr_sync.h ccnr_util.h
 ccnr_util.o: ccnr_util.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
