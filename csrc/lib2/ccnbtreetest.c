689ffaf3b0ed73adeb8c6b80bdfc18dc7c0ed43f
Michael Plass <plass@parc.com>
2011-07-20 14:12:52 -0700

    refs #880 Add test for directory creation.

--- csrc/lib2/ccnbtreetest.c
+++ csrc/lib2/ccnbtreetest.c
@@ -20,9 +20,57 @@
  * Boston, MA 02110-1301, USA.
  */
  
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include <ccn/btree.h>
+#include <ccn/charbuf.h>
+
+#define FAILIF(cond) do {} while ((cond) && fatal(__func__, __LINE__))
+#define CHKSYS(res) FAILIF((res) == -1)
+#define CHKPTR(p)   FAILIF((p) == NULL)
+
+static int
+fatal(const char *fn, int lineno)
+{
+    char buf[80] = {0};
+    snprintf(buf, sizeof(buf)-1, "OOPS - function %s, line %d", fn, lineno);
+    perror(buf);
+    exit(1);
+    return(0);
+}
+
+static int
+test_directory_creation(void)
+{
+    int res;
+    struct ccn_charbuf *dirbuf;
+    char *temp;
+    
+    dirbuf = ccn_charbuf_create();
+    CHKPTR(dirbuf);
+    res = ccn_charbuf_putf(dirbuf, "./%s", "_bt_XXXXXX");
+    CHKSYS(res);
+    temp = mkdtemp(ccn_charbuf_as_string(dirbuf));
+    CHKPTR(temp);
+    res = ccn_charbuf_putf(dirbuf, "/%s", "_test");
+    CHKSYS(res);
+    res = mkdir(ccn_charbuf_as_string(dirbuf), 0777);
+    CHKSYS(res);
+    printf("Created directory %s\n", ccn_charbuf_as_string(dirbuf));
+    ccn_charbuf_destroy(&dirbuf);
+    return(res);
+}
+
 int
 main(int argc, char **argv)
 {
+    int res;
+
+    res = test_directory_creation();
+    CHKSYS(res);
     return(0);
 }
