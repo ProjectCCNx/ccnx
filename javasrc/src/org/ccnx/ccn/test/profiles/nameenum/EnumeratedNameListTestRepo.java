f3d707b679e931d2ba51f08a1dcafee68f1f9c93
Paul Rasmussen <rasmusse@parc.com>
2011-10-21 15:21:35 -0700

    refs #100427 Fix silly problem in EnumeratedNameListTestRepo

--- javasrc/src/org/ccnx/ccn/test/profiles/nameenum/EnumeratedNameListTestRepo.java
+++ javasrc/src/org/ccnx/ccn/test/profiles/nameenum/EnumeratedNameListTestRepo.java
@@ -1,7 +1,7 @@
 /*
  * A CCNx library test.
  *
- * Copyright (C) 2008, 2009, 2010 Palo Alto Research Center, Inc.
+ * Copyright (C) 2008-2011 Palo Alto Research Center, Inc.
  *
  * This work is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
@@ -321,7 +321,7 @@ public class EnumeratedNameListTestRepo {
 		addContentToRepo(name5, putHandle);
 		addContentToRepo(name6, putHandle);
 		addContentToRepo(name7, putHandle);
-		noPoolOps(poolList);
+		noPoolOps(noPoolList);
 		Assert.assertEquals(4, contentSeenNoPool);
 	}
 	
