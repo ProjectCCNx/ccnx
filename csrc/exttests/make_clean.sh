d1f541fedeb4aac5ecc48313bd95d8eedf158daf
Michael Plass <plass@parc.com>
2012-02-02 11:11:02 -0800

    Refs #100610.  While here, arrange to keep repo perf test files around.
    We don't want these to vanish with just a make clean, but we don't
    want git to nag about their presence, either.

--- csrc/exttests/make_clean.sh
+++ csrc/exttests/make_clean.sh
@@ -15,6 +15,6 @@ echo Running $0 >&2
 cd `dirname $0` || exit 1
 grep '[c]leaning' make_clean.sh >&2 || exit 1
 # cleaning test debris
-# Please keep ignore and .gitignore in sync - this is a reminder
-test -f .gitignore && { ( echo Makefile; cat ignore; ) | diff -u - .gitignore || echo ''; }
+# Please keep preserve+ignore and .gitignore in sync - this is a reminder
+test -f .gitignore && { cat preserve ignore | diff -u - .gitignore || echo ''; }
 eval rm -R -f `cat ignore`
