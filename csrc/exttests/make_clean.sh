0784c192f588ca325c5ddca82f00caabb1574105
Michael Plass <plass@parc.com>
2012-02-02 10:57:12 -0800

    Refs #100610. Adjust ignorance.

--- csrc/exttests/make_clean.sh
+++ csrc/exttests/make_clean.sh
@@ -2,7 +2,7 @@
 # 
 # Part of the CCNx distribution.
 #
-# Copyright (C) 2011 Palo Alto Research Center, Inc.
+# Copyright (C) 2011-2012 Palo Alto Research Center, Inc.
 #
 # This work is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License version 2 as published by the
@@ -16,5 +16,5 @@ cd `dirname $0` || exit 1
 grep '[c]leaning' make_clean.sh >&2 || exit 1
 # cleaning test debris
 # Please keep ignore and .gitignore in sync - this is a reminder
-test -f .gitignore && { diff -u ignore .gitignore || echo ''; }
+test -f .gitignore && { ( echo Makefile; cat ignore; ) | diff -u - .gitignore || echo ''; }
 eval rm -R -f `cat ignore`
