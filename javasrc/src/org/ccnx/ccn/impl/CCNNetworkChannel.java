f52d2a1e7733939d47b79405055a180dd0f46bcd
Paul Rasmussen <rasmusse@parc.com>
2012-02-29 16:43:28 -0800

    refs #100690 First try to get Marc's wire decoding optimization to work

--- javasrc/src/org/ccnx/ccn/impl/CCNNetworkChannel.java
+++ javasrc/src/org/ccnx/ccn/impl/CCNNetworkChannel.java
@@ -5,7 +5,7 @@
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
- * as published by the Free Software Foundation. 
+ * as published by the Free Software Foundation.
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -35,6 +35,7 @@ import java.util.logging.Level;
 import org.ccnx.ccn.config.SystemConfiguration;
 import org.ccnx.ccn.impl.CCNNetworkManager.NetworkProtocol;
 import org.ccnx.ccn.impl.encoding.CCNProtocolDTags;
+import org.ccnx.ccn.impl.encoding.WireDecoder;
 import org.ccnx.ccn.impl.encoding.XMLDecoder;
 import org.ccnx.ccn.impl.encoding.XMLEncodable;
 import org.ccnx.ccn.impl.support.Log;
@@ -44,7 +45,7 @@ import org.ccnx.ccn.protocol.WirePacket;
 /**
  *  This guy manages all of the access to the network connection.
  *  It is capable of supporting both UDP and TCP transport protocols
- *  
+ *
  *  It also creates a stream interface for input to the decoders. It is necessary to
  *  create our own input stream for TCP because the stream that can be obtained via the
  *  socket interface is not markable. Originally the UDP code used to translate the UDP
@@ -60,28 +61,28 @@ public class CCNNetworkChannel extends InputStream {
 	// This is to make log messages intelligible
 	protected final static AtomicInteger _channelIdCounter = new AtomicInteger(0);
 	protected final int _channelId;
-	
+
 	// These are set in the constructor
 	protected final String _ncHost;
 	protected final int _ncPort;
 	protected final NetworkProtocol _ncProto;
 	protected final FileOutputStream _ncTapStreamIn;
-	
+
 	/**
 	 * Error recover from receipt of a bad packet which can cause the read to error out in the
 	 * middle of reading the packet. For UDP we can just ignore this and skip to the next packet, but
 	 * for TCP where data is continuous we need to skip over the rest of the bad data so that we can
 	 * resync on the next good packet.
 	 */
-	protected class TCPErrorCorrectionStrategy implements WirePacket.ErrorCorrectionStrategy {	
+	protected class TCPErrorCorrectionStrategy implements WirePacket.ErrorCorrectionStrategy {
 		/**
 		 * Sync to next packet after an error
-		 * @throws IOException 
+		 * @throws IOException
 		 */
 		public void resync(XMLDecoder decoder) {
 			while (true) {
 				try {
-					if (decoder.peekStartElement(CCNProtocolDTags.Interest) 
+					if (decoder.peekStartElement(CCNProtocolDTags.Interest)
 							|| decoder.peekStartElement(CCNProtocolDTags.ContentObject)) {
 						return;
 					}
@@ -94,28 +95,28 @@ public class CCNNetworkChannel extends InputStream {
 					return;
 				}
 			}
-		}	
+		}
 	}
-	
+
 	protected int _ncLocalPort;
 	protected DatagramChannel _ncDGrmChannel = null;
 	protected SocketChannel _ncSockChannel = null;
-	
+
 	// This lock provides exclusion between calls to open() and close()
 	protected Object _opencloseLock = new Object();
 	protected Selector _ncReadSelector = null;
 	protected Selector _ncWriteSelector = null;			 // Not needed for UDP
 	protected int _downDelay = 250;
-	
+
 	// This lock (maybe unnecessary now?), if used with _openCloseLock, should be contained inside it.
 	protected Object _ncConnectedLock = new Object();
 	protected boolean _ncConnected = false; // Actually asking the channel if its connected doesn't appear to be reliable
 	protected boolean _retry = true; // Attempt to reconnect
-	
+
 	protected boolean _ncInitialized = false;
 	protected Timer _ncHeartBeatTimer = null;
 	protected Boolean _ncStarted = false;
-	
+
 	// Allocate datagram buffer
 	protected ByteBuffer _datagram = ByteBuffer.allocateDirect(CCNNetworkManager.MAX_PAYLOAD);
 	// The following lines can be uncommented to help with debugging (i.e. you can't easily look at
@@ -126,42 +127,42 @@ public class CCNNetworkChannel extends InputStream {
 	private int _mark = -1;
 	private int _readLimit = 0;
 	private int _lastMark = 0;
-	
+
 	public CCNNetworkChannel(String host, int port, NetworkProtocol proto, FileOutputStream tapStreamIn) throws IOException {
 		_ncHost = host;
 		_ncPort = port;
 		_ncProto = proto;
 		_ncTapStreamIn = tapStreamIn;
 		_channelId = _channelIdCounter.incrementAndGet();
-		
+
 		if (Log.isLoggable(Log.FAC_NETMANAGER, Level.INFO))
 			Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: Starting up CCNNetworkChannel using {1}.",  _channelId, proto.toString());
 	}
-	
+
 	/**
 	 * Open the channel to ccnd depending on the protocol, connect on the ccnd port and
 	 * set up the selector
-	 * 
+	 *
 	 * @throws IOException
 	 */
 	public void open() throws IOException {
 		synchronized(_opencloseLock) {
 			if (Log.isLoggable(Log.FAC_NETMANAGER, Level.INFO))
 				Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: open()",  _channelId);
-	
+
 			if( _ncConnected ) {
 				Log.severe(Log.FAC_NETMANAGER, "NetworkChannel {0}: Calling open on an already connected channel!", _channelId);
 				throw new IOException("NetworkChannel " + _channelId + ": channel already connected");
 			}
-			
+
 			_ncReadSelector = Selector.open();
-	
+
 			if (_ncProto == NetworkProtocol.UDP) {
 				try {
 					_ncDGrmChannel = DatagramChannel.open();
 					_ncDGrmChannel.connect(new InetSocketAddress(_ncHost, _ncPort));
 					_ncDGrmChannel.configureBlocking(false);
-					
+
 					// For some weird reason we seem to have to test writing twice when ccnd is down
 					// before the channel actually notices. There might be some kind of timing/locking
 					// problem responsible for this but I can't figure out what it is.
@@ -206,10 +207,10 @@ public class CCNNetworkChannel extends InputStream {
 			} else {
 				throw new IOException("NetworkChannel " + _channelId + ": invalid protocol specified");
 			}
-			
+
 			if (Log.isLoggable(Log.FAC_NETMANAGER, Level.INFO)) {
 				String connecting = (_ncInitialized ? "Reconnecting to" : "Contacting");
-				Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: {1} CCN agent at {2}:{3} on local port {4}", 
+				Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: {1} CCN agent at {2}:{3} on local port {4}",
 						_channelId,
 						connecting,
 						_ncHost,
@@ -225,20 +226,20 @@ public class CCNNetworkChannel extends InputStream {
 			}
 		}
 	}
-	
+
 	/**
 	 * Get the next packet from the network. It could be either an interest or data. If ccnd is
 	 * down this is where we do a sleep to avoid a busy wait.  We go ahead and try to read in
 	 * the initial data here also because if there isn't any we want to find out here, not in the middle
 	 * of thinking we might be able to decode something. Also since this is supposed to happen
-	 * on packet boundaries, we reset the data buffer to its start during the initial read. We only do 
-	 * the initial read if there's nothing already in the buffer though because in TCP we could have 
+	 * on packet boundaries, we reset the data buffer to its start during the initial read. We only do
+	 * the initial read if there's nothing already in the buffer though because in TCP we could have
 	 * read in some or all of a preceding packet during the last reading.
-	 * 
+	 *
 	 * Also it should be noted that we are relying on ccnd to guarantee that all packets sent
 	 * to us are complete ccn packets. This code does not have the ability to recover from
 	 * receiving a partial ccn packet followed by correctly formed ones.
-	 * 
+	 *
 	 * @return a ContentObject, an Interest, or null if there's no data waiting
 	 * @throws IOException
 	 */
@@ -250,8 +251,8 @@ public class CCNNetworkChannel extends InputStream {
 				int ret = doReadIn(0);
 				if (ret <= 0 || !isConnected())
 					return null;
-			} 
-			WirePacket packet = new WirePacket();
+			}
+			WireDecoder packet = new WireDecoder();
 			packet.decode(this);
 			return packet.getPacket();
 		}
@@ -268,21 +269,22 @@ public class CCNNetworkChannel extends InputStream {
 			} else {
 				// We do not want to spin without a delay
 				Thread.sleep(_downDelay);
-			}	
+			}
 		} catch (InterruptedException e) {
 			Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: interrupted",  _channelId);
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Close the channel depending on the protocol
 	 * @throws IOException
 	 */
+	@Override
 	public void close() throws IOException {
 		close(false);
 	}
-	
+
 	private void close(boolean retry) throws IOException {
 		synchronized(_opencloseLock) {
 			if (Log.isLoggable(Log.FAC_NETMANAGER, Level.INFO))
@@ -305,7 +307,7 @@ public class CCNNetworkChannel extends InputStream {
 			}
 		}
 	}
-	
+
 	/**
 	 * Check whether the channel is currently connected.  This is really a test
 	 * to see whether ccnd is running. If it isn't the channel is not connected.
@@ -316,7 +318,7 @@ public class CCNNetworkChannel extends InputStream {
 			return _ncConnected;
 		}
 	}
-	
+
 	/**
 	 * Write to ccnd using methods based on the protocol type
 	 * @param src - ByteBuffer to write
@@ -327,9 +329,9 @@ public class CCNNetworkChannel extends InputStream {
 		if (! isConnected())
 			return -1; // XXX - is this documented?
 		if (Log.isLoggable(Log.FAC_NETMANAGER, Level.FINEST))
-			Log.finest(Log.FAC_NETMANAGER, 
+			Log.finest(Log.FAC_NETMANAGER,
 					"NetworkChannel {0}: write() on port {1}", _channelId, _ncLocalPort);
-		
+
 		try {
 			if (_ncDGrmChannel != null) {
 				return (_ncDGrmChannel.write(src));
@@ -349,14 +351,14 @@ public class CCNNetworkChannel extends InputStream {
 					}
 				}
 				return written;
-			} 
+			}
 		} catch (PortUnreachableException pue) {}
 		  catch (ClosedChannelException cce) {}
 		Log.info(Log.FAC_NETMANAGER, "NetworkChannel {0}: closing due to error on write", _channelId);
 		close(true);
 		return -1;
 	}
-	
+
 	/**
 	 * Force wakeup from a select
 	 * @return the selector
@@ -364,15 +366,15 @@ public class CCNNetworkChannel extends InputStream {
 	public Selector wakeup() {
 		return (_ncReadSelector.wakeup());
 	}
-	
+
 	/**
 	 * Initialize the channel at the point when we are actually ready to create faces
 	 * with ccnd
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public void init() throws IOException {
 	}
-	
+
 	private void initStream() {
 		_datagram.clear();
 		_datagram.limit(0);
@@ -383,7 +385,7 @@ public class CCNNetworkChannel extends InputStream {
 		while (true) {
 			try {
 				if (_datagram.hasRemaining()) {
-					int ret = (int)_datagram.get();
+					int ret = _datagram.get();
 					return ret & 0xff;
 				}
 			} catch (BufferUnderflowException bfe) {}
@@ -393,12 +395,12 @@ public class CCNNetworkChannel extends InputStream {
 			}
 		}
 	}
-	
+
 	@Override
 	public int read(byte[] b) throws IOException {
 		return read(b, 0, b.length);
 	}
-	
+
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
 		int ret = 0;
@@ -415,18 +417,18 @@ public class CCNNetworkChannel extends InputStream {
 		_datagram.get(b, off, ret);
 		return ret;
 	}
-	
+
 	@Override
 	public boolean markSupported() {
 		return true;
 	}
-	
+
 	@Override
 	public void mark(int readlimit) {
 		_readLimit = readlimit;
 		_mark = _datagram.position();
 	}
-	
+
 	@Override
 	public void reset() throws IOException {
 		if (_mark < 0)
@@ -436,13 +438,13 @@ public class CCNNetworkChannel extends InputStream {
 		}
 		_datagram.position(_mark);
 	}
-	
+
 	/**
 	 * Refill the buffer. We don't reset the start of it unless necessary (i.e. we have
 	 * reached the end of the buffer). If the start is reset and a mark has been set within
 	 * "readLimit" bytes of the end, we need to copy the end of the previous buffer out
 	 * to the start so that a reset is possible.
-	 * 
+	 *
 	 * @return
 	 * @throws IOException
 	 */
@@ -470,7 +472,7 @@ public class CCNNetworkChannel extends InputStream {
 		}
 		return doReadIn(position);
 	}
-	
+
 	/**
 	 * Read in data to the buffer starting at the specified position.
 	 * @param position
@@ -514,7 +516,7 @@ public class CCNNetworkChannel extends InputStream {
 		}
 		return ret;
 	}
-	
+
 	/**
 	 * @return true if heartbeat sent
 	 */
@@ -524,9 +526,9 @@ public class CCNNetworkChannel extends InputStream {
 			_ncDGrmChannel.write(heartbeat);
 			return true;
 		} catch (IOException io) {
-			// We do not see errors on send typically even if 
+			// We do not see errors on send typically even if
 			// agent is gone, so log each but do not track
-			Log.warning(Log.FAC_NETMANAGER, 
+			Log.warning(Log.FAC_NETMANAGER,
 					"NetworkChannel {0}: Error sending heartbeat packet: {1}", _channelId, io.getMessage());
 			try {
 				close(true);
