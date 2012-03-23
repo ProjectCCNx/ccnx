8fc8f2a76c263d02db6f8c5278a9f8944d94d3fb
Paul Rasmussen <rasmusse@parc.com>
2012-03-23 13:09:35 -0700

    refs #100671 Show what happened when netmanager run loop aborts due to an Error. Also exit in this case.

--- javasrc/src/org/ccnx/ccn/impl/support/Log.java
+++ javasrc/src/org/ccnx/ccn/impl/support/Log.java
@@ -1,7 +1,7 @@
 /*
  * Part of the CCNx Java Library.
  *
- * Copyright (C) 2008-2011 Palo Alto Research Center, Inc.
+ * Copyright (C) 2008-2012 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
@@ -694,6 +694,10 @@ public class Log {
 	}
 
 	public static void warningStackTrace(int facility, Throwable t) {
+		logStackTrace(facility, Level.WARNING, t);
+	}
+
+	public static void severeStackTrace(int facility, Throwable t) {
 		logStackTrace(facility, Level.WARNING, t);
 	}
 
