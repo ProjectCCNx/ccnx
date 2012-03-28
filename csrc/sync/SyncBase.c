8a2f0373fdb250aba0e6048e34d5c84c04460599
Michael Plass <plass@parc.com>
2012-03-28 15:59:06 -0700

    Refs #100698  Update copyright dates

--- csrc/sync/SyncBase.c
+++ csrc/sync/SyncBase.c
@@ -1,9 +1,10 @@
 /**
  * @file sync/SyncBase.c
- * 
+ *  
  * Part of CCNx Sync.
- *
- * Copyright (C) 2011 Palo Alto Research Center, Inc.
+ */
+/*
+ * Copyright (C) 2011-2012 Palo Alto Research Center, Inc.
  *
  * This work is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
