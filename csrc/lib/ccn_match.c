f33eb6ccfb588d92b109f494f1301f2e1555aa6e
Michael Plass <plass@parc.com>
2012-03-28 16:03:11 -0700

    Refs #100713  Update date.

--- csrc/lib/ccn_match.c
+++ csrc/lib/ccn_match.c
@@ -1,10 +1,11 @@
 /**
  * @file ccn_match.c
- * @brief Support for the match predicate between interest and content.
+ * Support for the match predicate between interest and content.
  * 
  * Part of the CCNx C Library.
- *
- * Copyright (C) 2008, 2009 Palo Alto Research Center, Inc.
+ */
+/*
+ * Copyright (C) 2008, 2009, 2012 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
