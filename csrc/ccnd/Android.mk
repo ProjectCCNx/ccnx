842d52887e4bff9eea612f3f32e43a595361e757
David J. Kordsmeier <dkords@parc.com>
2012-05-01 10:41:07 -0700

    refs #100747 Remove the duplicate in the obj list for ccnd Android.mk

--- csrc/ccnd/Android.mk
+++ csrc/ccnd/Android.mk
@@ -23,7 +23,7 @@ LOCAL_C_INCLUDES	+= $(LOCAL_PATH)/../include
 LOCAL_C_INCLUDES	+= $(LOCAL_PATH)/../../android/external/openssl-armv5/include
 
 CCNDOBJ := ccnd.o ccnd_msg.o ccnd_internal_client.o ccnd_stats.o \
-			ccnd_internal_client.o android_main.o
+			android_main.o
 CCNDSRC := $(CCNDOBJ:.o=.c)
 
 LOCAL_SRC_FILES := $(CCNDSRC)
