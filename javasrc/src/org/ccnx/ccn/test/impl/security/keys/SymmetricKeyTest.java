86cd95d46aa82acfa137b84e89a05e471401c939
Paul Rasmussen <rasmusse@parc.com>
2012-12-18 14:34:12 -0800

    refs #100157 Cleanup test a little and add new test to insure corrupt data doesn't verify

--- javasrc/src/org/ccnx/ccn/test/impl/security/keys/SymmetricKeyTest.java
+++ javasrc/src/org/ccnx/ccn/test/impl/security/keys/SymmetricKeyTest.java
@@ -1,4 +1,23 @@
+/*
+ * Part of the CCNx Java Library.
+ *
+ * Copyright (C) 2012 Palo Alto Research Center, Inc.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation. 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. You should have received
+ * a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.ccnx.ccn.test.impl.security.keys;
+
+import java.io.File;
+import java.util.Random;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -10,11 +29,14 @@ import org.ccnx.ccn.impl.security.keys.SecureKeyCache;
 import org.ccnx.ccn.impl.support.Log;
 import org.ccnx.ccn.io.content.CCNStringObject;
 import org.ccnx.ccn.protocol.ContentName;
+import org.ccnx.ccn.protocol.ContentObject;
 import org.ccnx.ccn.protocol.PublisherPublicKeyDigest;
 import org.ccnx.ccn.test.CCNTestBase;
 import org.ccnx.ccn.test.CCNTestHelper;
 import org.ccnx.ccn.test.Flosser;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SymmetricKeyTest extends CCNTestBase {
@@ -22,18 +44,24 @@ public class SymmetricKeyTest extends CCNTestBase {
 	static Flosser flosser = null;
 	
 	static CCNTestHelper testHelper = new CCNTestHelper(SymmetricKeyTest.class);
+	static KeyGenerator kg;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		CCNTestBase.setUpBeforeClass();
+		kg = KeyGenerator.getInstance("HMAC-SHA256", KeyManager.PROVIDER);
+	}
 
 	@Test
 	public void testSymmetricKeys() throws Exception {
 		Log.info(Log.FAC_TEST, "Starting testSymmetricKeys");
 		
 		flosser = new Flosser();
-		KeyGenerator kg = KeyGenerator.getInstance("HMAC-SHA256", KeyManager.PROVIDER);
 		SecretKey sk = kg.generateKey();
 		SecureKeyCache skc = putHandle.getNetworkManager().getKeyManager().getSecureKeyCache();
 		PublisherPublicKeyDigest publisher = new PublisherPublicKeyDigest(sk);
 		skc.addSecretKey(null, publisher.digest(), sk);
-		ContentName name = testHelper.getTestChildName("testSetLocator", "testString");
+		ContentName name = testHelper.getTestChildName("testSymmetricKeys", "testString");
 		CCNStringObject testString1 = new CCNStringObject(name, "A test!", 
 									SaveType.RAW, publisher, null, putHandle);
 		flosser.handleNamespace(name);
@@ -44,5 +72,23 @@ public class SymmetricKeyTest extends CCNTestBase {
 		testString1.close();
 		testString2.close();
 		flosser.stopMonitoringNamespaces();
+		
+		Log.info(Log.FAC_TEST, "Completed testSymmetricKeys");
+	}
+	
+	@Test
+	public void testCorruptContent() throws Exception {
+		Log.info(Log.FAC_TEST, "Starting testCorruptContent");
+
+		ContentName name = testHelper.getTestChildName("testCorruptContent", "testString");
+		ContentObject co = ContentObject.buildContentObject(name, "This is a test".getBytes());
+		SecretKey sk = kg.generateKey();
+		co.sign(sk);
+		Random rand = new Random();
+		int start = rand.nextInt(co.contentLength() - 8);
+		System.arraycopy(new byte[] {0xd, 0xe, 0xa, 0xd, 0xb, 0xe, 0xe, 0xf}, 0, co.content(), start, 8);
+		Assert.assertFalse(co.verify(sk));
+		
+		Log.info(Log.FAC_TEST, "Completed testCorruptContent");
 	}
 }
