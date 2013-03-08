4282b42c3482871c51fdff12388755479d79959e
Nick Briggs <briggs@parc.com>
2013-03-08 10:35:06 -0800

    Refs #100894 Fix set-but-not-used warning in sync_diff.c

--- csrc/sync/sync_diff.c
+++ csrc/sync/sync_diff.c
@@ -1160,7 +1160,6 @@ try_node_split(struct sync_update_data *ud) {
         return 0;
     struct SyncRootStruct *root = ud->root;
     int debug = root->base->debug;
-    struct ccn_charbuf *prev = NULL;
     int accLim = nodeSplitTrigger - nodeSplitTrigger/8;
     int accMin = nodeSplitTrigger/2;
     int res = 0;
@@ -1181,7 +1180,6 @@ try_node_split(struct sync_update_data *ud) {
         int nameLen = name->length + 8;
         if (nameLen > maxLen) maxLen = nameLen;
         accLen = accLen + nameLen + (maxLen - nameLen) * 2;
-        prev = name;
         if (split+1 < lim) {
             if (splitMethod & 1) {
                 // use level shift to split
