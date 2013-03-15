3d31779014293a5d484109263603e59578582f13
Michael Plass <plass@parc.com>
2013-03-15 13:01:45 -0700

    Refs #100890  Update deps.

--- csrc/ccnd/dir.mk
+++ csrc/ccnd/dir.mk
@@ -65,7 +65,8 @@ ccnd.o: ccnd.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
   ../include/ccn/ccnd.h ../include/ccn/face_mgmt.h \
-  ../include/ccn/sockcreate.h ../include/ccn/hashtb.h \
+  ../include/ccn/sockcreate.h ../include/ccn/flatname.h \
+  ../include/ccn/hashtb.h ../include/ccn/nametree.h \
   ../include/ccn/schedule.h ../include/ccn/reg_mgmt.h \
   ../include/ccn/uri.h ccnd_private.h ../include/ccn/seqwriter.h
 ccnd_msg.o: ccnd_msg.c ../include/ccn/ccn.h ../include/ccn/coding.h \
@@ -77,8 +78,9 @@ ccnd_stats.o: ccnd_stats.c ../include/ccn/ccn.h ../include/ccn/coding.h \
   ../include/ccn/charbuf.h ../include/ccn/indexbuf.h \
   ../include/ccn/ccnd.h ../include/ccn/schedule.h \
   ../include/ccn/sockaddrutil.h ../include/ccn/hashtb.h \
-  ../include/ccn/uri.h ccnd_private.h ../include/ccn/ccn_private.h \
-  ../include/ccn/reg_mgmt.h ../include/ccn/seqwriter.h
+  ../include/ccn/nametree.h ../include/ccn/uri.h ccnd_private.h \
+  ../include/ccn/ccn_private.h ../include/ccn/reg_mgmt.h \
+  ../include/ccn/seqwriter.h
 ccnd_internal_client.o: ccnd_internal_client.c ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
