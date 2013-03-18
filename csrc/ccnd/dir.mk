06537f69857d35ca2d1d0690fa0a055aaefb6113
Michael Plass <plass@parc.com>
2013-03-18 09:31:26 -0700

    Refs #100890  Deps.

--- csrc/ccnd/dir.mk
+++ csrc/ccnd/dir.mk
@@ -58,9 +58,9 @@ check test: ccnd ccndsmoketest $(SCRIPTSRC)
 # but must be updated manually.
 ###############################
 ccnd_main.o: ccnd_main.c ccnd_private.h ../include/ccn/ccn_private.h \
-  ../include/ccn/coding.h ../include/ccn/reg_mgmt.h \
-  ../include/ccn/charbuf.h ../include/ccn/schedule.h \
-  ../include/ccn/seqwriter.h
+  ../include/ccn/coding.h ../include/ccn/nametree.h \
+  ../include/ccn/reg_mgmt.h ../include/ccn/charbuf.h \
+  ../include/ccn/schedule.h ../include/ccn/seqwriter.h
 ccnd.o: ccnd.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
   ../include/ccn/coding.h ../include/ccn/charbuf.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
@@ -72,8 +72,9 @@ ccnd.o: ccnd.c ../include/ccn/bloom.h ../include/ccn/ccn.h \
 ccnd_msg.o: ccnd_msg.c ../include/ccn/ccn.h ../include/ccn/coding.h \
   ../include/ccn/charbuf.h ../include/ccn/indexbuf.h \
   ../include/ccn/ccnd.h ../include/ccn/hashtb.h ../include/ccn/uri.h \
-  ccnd_private.h ../include/ccn/ccn_private.h ../include/ccn/reg_mgmt.h \
-  ../include/ccn/schedule.h ../include/ccn/seqwriter.h
+  ccnd_private.h ../include/ccn/ccn_private.h ../include/ccn/nametree.h \
+  ../include/ccn/reg_mgmt.h ../include/ccn/schedule.h \
+  ../include/ccn/seqwriter.h
 ccnd_stats.o: ccnd_stats.c ../include/ccn/ccn.h ../include/ccn/coding.h \
   ../include/ccn/charbuf.h ../include/ccn/indexbuf.h \
   ../include/ccn/ccnd.h ../include/ccn/schedule.h \
@@ -86,7 +87,7 @@ ccnd_internal_client.o: ccnd_internal_client.c ../include/ccn/ccn.h \
   ../include/ccn/indexbuf.h ../include/ccn/ccn_private.h \
   ../include/ccn/hashtb.h ../include/ccn/keystore.h \
   ../include/ccn/schedule.h ../include/ccn/sockaddrutil.h \
-  ../include/ccn/uri.h ccnd_private.h ../include/ccn/reg_mgmt.h \
-  ../include/ccn/seqwriter.h
+  ../include/ccn/uri.h ccnd_private.h ../include/ccn/nametree.h \
+  ../include/ccn/reg_mgmt.h ../include/ccn/seqwriter.h
 ccndsmoketest.o: ccndsmoketest.c ../include/ccn/ccnd.h \
   ../include/ccn/ccn_private.h
