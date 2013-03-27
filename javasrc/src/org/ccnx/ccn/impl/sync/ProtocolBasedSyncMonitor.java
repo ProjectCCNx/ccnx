185f34bd586beba8c34a81656e208ff2d371b6b3
David J. Kordsmeier <dkords@parc.com>
2013-03-27 15:21:59 -0700

    refs #100887 Minor update to copyright date

--- javasrc/src/org/ccnx/ccn/impl/sync/ProtocolBasedSyncMonitor.java
+++ javasrc/src/org/ccnx/ccn/impl/sync/ProtocolBasedSyncMonitor.java
@@ -1,7 +1,7 @@
 /*
  * Part of the CCNx Java Library.
  *
- * Copyright (C) 2012, 2013 Palo Alto Research Center, Inc.
+ * Copyright (C) 2012-2013 Palo Alto Research Center, Inc.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2.1
