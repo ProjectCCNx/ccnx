bb2171b461dcdb2b32b4b81ba0310473dea14e39
Michael Plass <plass@parc.com>
2013-05-29 16:14:29 -0700

    Refs #100991  Add build support for OpenBSD (at least a first cut).

---
+++ csrc/conf/OpenBSD.mk
@@ -0,0 +1,20 @@
+# conf/OpenBSD.mk
+# 
+# Part of the CCNx distribution.
+#
+# Copyright (C) 2013 Palo Alto Research Center, Inc.
+#
+# This work is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+# This work is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+RESOLV_LIBS=
+SHEXT=so
+SHLIBNAME=libccn.$(SHEXT).1
+SHLIBDEPS=
+SHARED_LD_FLAGS = -shared --whole-archive -soname=$(SHLIBNAME) -lc
+PLATCFLAGS=-fPIC
+CWARNFLAGS = -Wall -Wpointer-arith -Wreturn-type -Wstrict-prototypes
