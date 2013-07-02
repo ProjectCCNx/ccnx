c7cf0af6b371653eafb4797f15d36119b82e22c8
Michael Plass <plass@parc.com>
2013-07-02 08:32:17 -0700

    Refs #101019  Sort makefile lists and switch to one per line.

--- csrc/lib/dir.mk
+++ csrc/lib/dir.mk
@@ -21,38 +21,86 @@ PROGRAMS = hashtbtest skel_decode_test \
 
 BROKEN_PROGRAMS =
 DEBRIS = ccn_verifysig _bt_* test.keystore
-CSRC = ccn_bloom.c \
-       ccn_btree.c ccn_btree_content.c ccn_btree_store.c \
-       ccn_buf_decoder.c ccn_buf_encoder.c ccn_bulkdata.c \
-       ccn_charbuf.c ccn_client.c ccn_coding.c ccn_digest.c ccn_extend_dict.c \
-       ccn_dtag_table.c ccn_face_mgmt.c ccn_flatname.c \
-       ccn_indexbuf.c ccn_interest.c ccn_keystore.c \
-       ccn_match.c ccn_nametree.c ccn_reg_mgmt.c \
-       ccn_merkle_path_asn1.c ccn_name_util.c ccn_schedule.c \
-       ccn_seqwriter.c ccn_signing.c \
-       ccn_sockcreate.c ccn_traverse.c ccn_uri.c \
-       ccn_verifysig.c ccn_versioning.c \
-       ccn_header.c \
-       ccn_fetch.c \
-       lned.c \
-       encodedecodetest.c hashtb.c hashtbtest.c \
-       signbenchtest.c skel_decode_test.c \
-       basicparsetest.c ccnbtreetest.c nametreetest.c \
-       ccn_sockaddrutil.c ccn_setup_sockaddr_un.c
+CSRC = \
+    basicparsetest.c \
+    ccn_bloom.c \
+    ccn_btree.c \
+    ccn_btree_content.c \
+    ccn_btree_store.c \
+    ccn_buf_decoder.c \
+    ccn_buf_encoder.c \
+    ccn_bulkdata.c \
+    ccn_charbuf.c \
+    ccn_client.c \
+    ccn_coding.c \
+    ccn_digest.c \
+    ccn_dtag_table.c \
+    ccn_extend_dict.c \
+    ccn_face_mgmt.c \
+    ccn_fetch.c \
+    ccn_flatname.c \
+    ccn_header.c \
+    ccn_indexbuf.c \
+    ccn_interest.c \
+    ccn_keystore.c \
+    ccn_match.c \
+    ccn_merkle_path_asn1.c \
+    ccn_name_util.c \
+    ccn_nametree.c \
+    ccn_reg_mgmt.c \
+    ccn_schedule.c \
+    ccn_seqwriter.c \
+    ccn_setup_sockaddr_un.c \
+    ccn_signing.c \
+    ccn_sockaddrutil.c \
+    ccn_sockcreate.c \
+    ccn_traverse.c \
+    ccn_uri.c \
+    ccn_verifysig.c \
+    ccn_versioning.c \
+    ccnbtreetest.c \
+    encodedecodetest.c \
+    hashtb.c \
+    hashtbtest.c \
+    lned.c \
+    nametreetest.c \
+    signbenchtest.c \
+    skel_decode_test.c
+
 LIBS = libccn.a
-LIB_OBJS = ccn_client.o ccn_charbuf.o ccn_indexbuf.o ccn_coding.o \
-       ccn_dtag_table.o \
-       ccn_face_mgmt.o ccn_flatname.o \
-       ccn_schedule.o ccn_extend_dict.o \
-       ccn_buf_decoder.o ccn_uri.o ccn_buf_encoder.o ccn_bloom.o \
-       ccn_name_util.o ccn_reg_mgmt.o ccn_digest.o \
-       ccn_interest.o ccn_keystore.o ccn_seqwriter.o ccn_signing.o \
-       ccn_sockcreate.o ccn_traverse.o \
-       ccn_match.o ccn_nametree.o hashtb.o ccn_merkle_path_asn1.o \
-       ccn_sockaddrutil.o ccn_setup_sockaddr_un.o \
-       ccn_bulkdata.o ccn_versioning.o ccn_header.o ccn_fetch.o \
-       ccn_btree.o ccn_btree_content.o ccn_btree_store.o \
-       lned.o
+LIB_OBJS = \
+    ccn_bloom.o \
+    ccn_btree.o \
+    ccn_btree_content.o \
+    ccn_btree_store.o \
+    ccn_buf_decoder.o \
+    ccn_buf_encoder.o \
+    ccn_bulkdata.o \
+    ccn_digest.o \
+    ccn_dtag_table.o \
+    ccn_extend_dict.o \
+    ccn_face_mgmt.o \
+    ccn_fetch.o \
+    ccn_flatname.o \
+    ccn_header.o \
+    ccn_interest.o \
+    ccn_keystore.o \
+    ccn_match.o \
+    ccn_merkle_path_asn1.o \
+    ccn_name_util.o \
+    ccn_nametree.o \
+    ccn_reg_mgmt.o \
+    ccn_schedule.o \
+    ccn_seqwriter.o \
+    ccn_setup_sockaddr_un.o \
+    ccn_signing.o \
+    ccn_sockaddrutil.o \
+    ccn_sockcreate.o \
+    ccn_traverse.o \
+    ccn_uri.o \
+    ccn_versioning.o \
+    hashtb.o \
+    lned.o
 
 default all: dtag_check lib $(PROGRAMS)
 # Don't try to build shared libs right now.
