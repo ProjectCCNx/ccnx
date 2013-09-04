04e81deb0333cabfe0ed6a2ed8e2fd708aa41713
Nick Briggs <briggs@parc.com>
2013-09-04 14:03:24 -0700

    Refs #100983 Clean up load-sharing strategy, incorporate new strategy ops.

--- csrc/ccnd/ccnd_strategy2.c
+++ csrc/ccnd/ccnd_strategy2.c
@@ -17,185 +17,197 @@
  * Boston, MA 02110-1301, USA.
  */
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "ccnd_strategy.h"
 
-#define MINE 0x65e272 // hint: openssl rand -hex 3
-
-struct strategy_state {
-    unsigned magic;              /**< MINE to mark our stuff */
-    unsigned src;                /**< faceid of recent content source */
-    unsigned osrc;               /**< and of older matching content */
-    unsigned usec;               /**< response-time prediction */
+struct face_state_item {
+    unsigned faceid;           /**< the face id this entry describes */
+    unsigned pending;          /**< pending interest count */
+    unsigned timedout;         /**< has this face timed out recently */
+    unsigned histindex;        /**< index in overall history structure of temporary item */
+    struct pit_face_item *pfi; /**< temporary pointer to the item, set to NULL at exit */
 };
 
-CCN_STATESIZECHECK(X_strategy_state, struct strategy_state);
+#define N_FACESTATE 32
 
-static void
-adjust_predicted_response(struct ccnd_handle *h,
-                          struct strategy_state *s, unsigned faceid);
+struct face_state {
+    unsigned n;
+    struct face_state_item *items;
+};
+
+int
+compare_item(const void *a, const void *b)
+{
+    struct face_state_item *a_item = (struct face_state_item *)a;
+    struct face_state_item *b_item = (struct face_state_item *)b;
+    unsigned a_down, b_down, a_pending, b_pending;
+    
+    a_down = (a_item->pfi->pfi_flags & CCND_PFI_DNSTREAM) != 0;
+    b_down = (b_item->pfi->pfi_flags & CCND_PFI_DNSTREAM) != 0;
+    a_pending = a_item->pending * (a_item->timedout ? 2 : 1);
+    b_pending = b_item->pending * (b_item->timedout ? 2 : 1);
+    
+    /* sort any downstream faces to the end */
+    if (a_down && b_down) return 0;
+    if (a_down) return (1);
+    if (b_down) return (-1);
+    
+    /* sort upstream faces by their pending count */
+    if (a_pending < b_pending) return (-1);
+    if (a_pending > b_pending) return (1);
+    return (0);
+    
+}
 
 /**
- * This implements the default strategy.
+ * This implements a distribution by performance strategy.
  *
- * Eventually there will be a way to have other strategies.
+ * The number of pending interests is a proxy for the performance of the face,
+ * an interest is sent on the face with the minimum pending, or randomly to
+ * one selected from those with the minimum.
  */
+
 void
-strategy2_callout(struct ccnd_handle *h,
-                  struct strategy_instance *instance,
-                  struct ccn_strategy *strategy,
-                  enum ccn_strategy_op op,
-                  unsigned faceid)
+ccnd_loadsharing_strategy_impl(struct ccnd_handle *h,
+                               struct strategy_instance *instance,
+                               struct ccn_strategy *strategy,
+                               enum ccn_strategy_op op,
+                               unsigned faceid)
 {
     struct pit_face_item *x = NULL;
     struct pit_face_item *p = NULL;
-    struct strategy_state *npe = NULL;
-    struct strategy_state *parent = NULL;
-    struct nameprefix_state *sst[2] = {NULL};
-    struct strategy_state dummy = { MINE, CCN_NOFACEID, CCN_NOFACEID, 50000 };
-    unsigned best = CCN_NOFACEID;
-    unsigned randlow, randrange;
-    unsigned nleft;
-    unsigned amt;
-    int usec;
+    struct face_state *face_state = (struct face_state *)instance->data;
+    unsigned i;
+    unsigned best;
+    struct face_state_item t_items[N_FACESTATE];
+    unsigned n_items = 0;
     
-    /* We will want access to the state for our prefix and its parent */
-    strategy_getstate(h, strategy, sst, 2);
-    
-    /* First get or initialize the parent nameprefix state */
-    if (sst[1] == NULL)
-        parent = &dummy;
-    else if (sst[1]->s[0] == CCN_UNINIT) {
-        parent = (struct strategy_state *)sst[1]->s;
-        *parent = dummy;
-    }
-    else if ((sst[1]->s[0] & CCN_MAGIC_MASK) == MINE)
-        parent = (struct strategy_state *)sst[1]->s;
-    
-    /* Now get the state for the longer prefix */
-    npe = (struct strategy_state *)sst[0]->s; /* This one should not be NULL */
-    if ((npe->magic & CCN_AGED) != 0) {
-        if ((npe->magic & CCN_MAGIC_MASK) != MINE)
-            *npe = *parent;
-        else
-            npe->magic = MINE;
-    }
-    if (npe->magic != MINE)
-        npe = &dummy; /* do not walk on somebody else's state */
+    memset(t_items, 0, sizeof(t_items));
     
     switch (op) {
         case CCNST_NOP:
             break;
-        case CCNST_FIRST:
-            best = npe->src;
-            if (best == CCN_NOFACEID)
-                best = npe->src = npe->osrc;
+        case CCNST_INIT:
+            /* Allocate strategy per registration point private data for
+             * per-face pending interest count undifferentiated by (interest) prefix.
+             */
+            if (face_state != NULL) {
+                free(face_state->items);
+                free(face_state);
+            }
+            face_state = calloc(1, sizeof(*face_state));
+            face_state->items = calloc(N_FACESTATE, sizeof(*face_state->items));
+            instance->data = face_state;
+            break;
+        case CCNST_FIRST:  /* newly created interest entry */
+            /* clear any default timing information */
+            for (x = strategy->pfl; x != NULL; x = x->next)
+                x->expiry = 0;
             /* Find our downstream; right now there should be just one. */
             for (x = strategy->pfl; x != NULL; x = x->next)
                 if ((x->pfi_flags & CCND_PFI_DNSTREAM) != 0)
                     break;
             if (x == NULL || (x->pfi_flags & CCND_PFI_PENDING) == 0)
                 return;
-            if (best == CCN_NOFACEID) {
-                randlow = 4000;
-                randrange = 75000;
-            }
-            else {
-                randlow = npe->usec;
-                if (randlow < 2000)
-                    randlow = 100 + ccnd_random(h) % 4096U;
-                randrange = (randlow + 1) / 2;
-            }
-            nleft = 0;
+            
+            /* Ensure that we have a face state entry for every face in the pfl
+             */
             for (p = strategy->pfl; p!= NULL; p = p->next) {
-                if ((p->pfi_flags & CCND_PFI_UPSTREAM) != 0) {
-                    if (p->faceid == best) {
-                        /* we may have already sent in case of TAP */
-                        if ((p->pfi_flags & CCND_PFI_UPENDING) == 0)
-                            p = send_interest(h, strategy->ie, x, p);
-                        strategy_settimer(h, strategy->ie, npe->usec, CCNST_TIMER);
+                for (i = 0; i < face_state->n; i++) {
+                    if (face_state->items[i].faceid == p->faceid) {
+                        fprintf(stderr, "strategy2: found face %u at index %u\n", p->faceid, i);
+                        if (p != x && ((p->pfi_flags & CCND_PFI_UPENDING) == 0)) {
+                            t_items[n_items] = face_state->items[i];
+                            t_items[n_items].pfi = p;
+                            t_items[n_items].histindex = i;
+                            n_items++;
+                        }
+                        break;
                     }
-                    else if ((p->pfi_flags & CCND_PFI_UPENDING) != 0)
-                        /* TAP interest has already been sent */;
-                    else if (p->faceid == npe->osrc)
-                        pfi_set_expiry_from_micros(h, strategy->ie, p, randlow);
-                    else {
-                        /* Want to preserve the order of the rest */
-                        nleft++;
-                        p->pfi_flags |= CCND_PFI_SENDUPST;
+                }
+                if (i == face_state->n) {
+                    /* there was no entry for this face, make one. */
+                    if (i < N_FACESTATE) {
+                        fprintf(stderr, "strategy2: adding face %u\n", p->faceid);
+                        face_state->items[i].faceid = p->faceid;
+                        face_state->items[i].pending = 0;
+                        face_state->n++;
+                        if (p != x && ((p->pfi_flags & CCND_PFI_UPENDING) == 0)) {
+                            t_items[n_items] = face_state->items[i];
+                            t_items[n_items].pfi = p;
+                            t_items[n_items].histindex = i;
+                            n_items++;
+                        }
                     }
                 }
             }
-            if (nleft > 0) {
-                /* Send remainder in order, with randomized timing */
-                amt = (2 * randrange + nleft - 1) / nleft;
-                if (amt == 0) amt = 1; /* paranoia - should never happen */
-                usec = randlow;
-                for (p = strategy->pfl; p!= NULL; p = p->next) {
-                    if ((p->pfi_flags & CCND_PFI_SENDUPST) != 0) {
-                        pfi_set_expiry_from_micros(h, strategy->ie, p, usec);
-                        usec += ccnd_random(h) % amt;
-                    }
-                }
+            /* no eligible faces, so nothing to do */
+            if (n_items == 0)
+                return;
+            
+            /* Sort the faces by number of items pending, lowest first
+             */
+            qsort(t_items, n_items, sizeof(t_items[0]), &compare_item);
+            
+            for (i = 0; i < n_items; i++) {
+                if (t_items[i].pending > t_items[0].pending) break;
             }
+            best = ccnd_random(h) % i;
+            fprintf(stderr, "strategy2: i %u best %u\n", i, best);
+            face_state->items[t_items[best].histindex].pending++;
+            fprintf(stderr, "strategy2: face %u selected, pending %u\n",
+                    t_items[best].pfi->faceid, t_items[best].pending);
+            send_interest(h, strategy->ie, x, t_items[best].pfi);
+            break;
+        case CCNST_NEWUP:
+            break;
+        case CCNST_NEWDN:
+            break;
+        case CCNST_EXPUP:
+            /*
+             * Someone has not responded in time.
+             * We decrement the pending count and flag the face has having had
+             * a timeout so that the face selection can penalize non-responding
+             * faces.
+             */
+            for (i = 0; i < face_state->n; i++)
+                if (face_state->items[i].faceid == faceid) {
+                    fprintf(stderr, "strategy2: timed out face %u at index %u\n", faceid, i);
+                    if (face_state->items[i].pending > 0) {
+                        face_state->items[i].pending--;
+                    }
+                    face_state->items[i].timedout = 1;
+                    break;
+                }
+            break;
+        case CCNST_EXPDN:
+            break;
+        case CCNST_REFRESH:
             break;
         case CCNST_TIMER:
-            /*
-             * Our best choice has not responded in time.
-             * Increase the predicted response.
-             */
-            adjust_predicted_response(h, npe, CCN_NOFACEID);
-            adjust_predicted_response(h, parent, CCN_NOFACEID);
             break;
         case CCNST_SATISFIED:
-            /* Keep a little history about where matching content comes from. */
-            adjust_predicted_response(h, npe, faceid);
-            adjust_predicted_response(h, parent, faceid);
+            /* Decrement the pending count on the face (faceid) that responded */
+            for (i = 0; i < face_state->n; i++)
+                if (face_state->items[i].faceid == faceid) {
+                    face_state->items[i].timedout = 0;
+                    if (face_state->items[i].pending > 0)
+                        face_state->items[i].pending--;
+                    break;
+                }
+            fprintf(stderr, "strategy2: face %u response\n", faceid);
             break;
-        case CCNST_TIMEOUT:
+        case CCNST_TIMEOUT: // all downstreams timed out, PIT entry will go away
             /* Interest has not been satisfied or refreshed */
+            fprintf(stderr, "strategy2: face %u TIMEOUT\n", faceid);
             break;
-        case CCNST_INIT:
-            break; /* No strategy private data needed */
         case CCNST_FINALIZE:
-            break; /* Nothing to clean up */
-    }
-}
-
-// XXX - import this late so we don't pollute too much.
-#include "ccnd_private.h"
-
-/**
- * Adjust the predicted response associated with a name prefix entry.
- *
- * It is decreased by a small fraction if we get content within our
- * previous predicted value, and increased by a larger fraction if not.
- *
- * faceid is CCN_NOFACEID if no content arrived, or else tells the
- * arrival face.
- */
-static void
-adjust_predicted_response(struct ccnd_handle *h,
-                          struct strategy_state *s, unsigned faceid)
-{
-    unsigned t = s->usec;
-    if (faceid == CCN_NOFACEID) {
-        t = t + (t >> 3); /* no content arrived */
-        if (t > h->predicted_response_limit)
-            t = h->predicted_response_limit;
-    }
-    else if (faceid == s->src) {
-        t = t - (t >> 7); /* content arrived on expected face */
-        if (t < 127)
-            t = 127;
-    }
-    s->usec = t;
-    if (faceid == CCN_NOFACEID)
-        return;
-    /* content arrived, so keep track of the arrival face */
-    if (s->src == CCN_NOFACEID)
-        s->src = faceid;
-    else if (s->src != faceid) {
-        s->osrc = s->src;
-        s->src = faceid;
+            /* Free the strategy per registration point private data */
+            free(face_state->items);
+            free(face_state);
+            instance->data = NULL;
+            break;
     }
 }
