f228f626a989af85359da3a67fbfd6fb6700a163
Michael Plass <plass@parc.com>
2013-10-10 15:50:37 -0700

    Refs #100983  Fix ccnd build to construct the object list for android when make depend is used.

---
+++ csrc/ccnd/android_obj.mk
@@ -0,0 +1,11 @@
+CCNDOBJ := \
+    android_main.o \
+    ccnd.o \
+    ccnd_internal_client.o \
+    ccnd_msg.o \
+    ccnd_stats.o \
+    ccnd_strategy0.o \
+    ccnd_strategy1.o \
+    ccnd_strategy2.o \
+    ccnd_stregistry.o \
+
--- csrc/ccnd/dir.mk
+++ csrc/ccnd/dir.mk
@@ -33,11 +33,11 @@ all: default $(BROKEN_PROGRAMS)
 
 $(PROGRAMS): $(CCNLIBDIR)/libccn.a
 
-CCND_OBJ = ccnd_main.o ccnd.o ccnd_msg.o ccnd_stats.o ccnd_internal_client.o ccnd_stregistry.o ccnd_strategy0.o \
+CCND_OBJ = ccnd.o ccnd_msg.o ccnd_stats.o ccnd_internal_client.o ccnd_stregistry.o ccnd_strategy0.o \
 	ccnd_strategy1.o ccnd_strategy2.o
 
-ccnd: $(CCND_OBJ) ccnd_built.sh
-	$(CC) $(CFLAGS) -o $@ $(CCND_OBJ) $(LDLIBS) $(OPENSSL_LIBS) -lcrypto
+ccnd: ccnd_main.o $(CCND_OBJ) ccnd_built.sh
+	$(CC) $(CFLAGS) -o $@ ccnd_main.o $(CCND_OBJ) $(LDLIBS) $(OPENSSL_LIBS) -lcrypto
 	sh ./ccnd_built.sh
 
 ccnd_built.sh:
@@ -49,7 +49,14 @@ ccndsmoketest: ccndsmoketest.o
 ccnd_stregistry.h: gen_stregistry.sh $(CSRC)
 	sh gen_stregistry.sh $(CSRC)
 
-depend: ccnd_stregistry.h
+depend: ccnd_stregistry.h android_obj.mk
+android_obj.mk: _always
+	echo 'CCNDOBJ := \' > templist
+	for i in android_main.o $(CCND_OBJ); \
+		do echo "    $$i" '\'; done | sort -u >> templist
+	echo >> templist
+	diff -b templist android_obj.mk || mv templist android_obj.mk
+	$(RM) templist
 
 clean:
 	rm -f *.o *.a $(PROGRAMS) $(BROKEN_PROGRAMS) depend
